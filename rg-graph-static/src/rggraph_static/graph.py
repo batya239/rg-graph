558f3c24c5d6d14ee8789ff37785f8a605e6f145
Mikhail Kompaniets <mkompan@gmail.com>
2010-02-04 02:11:26 +0300

    implemented generation of Graph.Nodes test.py -> example.py

--- rg-graph-static/src/rggraph_static/graph.py
+++ rg-graph-static/src/rggraph_static/graph.py
@@ -7,14 +7,14 @@ class Line:
     """ Class represents information about Line of a graph
         idx, type, Momenta, In, Out 
     """
-    def __init__(self,Ltype,LIn, LOut, LMomenta):
-         self.Type=Ltype
-         self.In=LIn
-         self.Out=LOut
-         self.Momenta=LMomenta
+    def __init__(self, Ltype, LIn, LOut, LMomenta):
+         self.Type = Ltype
+         self.In = LIn
+         self.Out = LOut
+         self.Momenta = LMomenta
          
     def Nodes(self):
-         return (self.In,self.Out)
+         return (self.In, self.Out)
      
 
 
@@ -23,10 +23,11 @@ class Node:
           idx, type, Lines
 #### ??          getFactor()
      """
-     def __init__(self,G,**kwargs):
-          """  в кваргз можно было бы указать например что вершина продиференцированна или тип вершины.
+     def __init__(self, **kwargs):
+          """  в кваргз можно было бы указать например что вершина продифференцированна или тип вершины.
           """
-          self.Lines=[]
+          self.Lines=kwargs["Lines"]
+          self.Type=kwargs["Type"]
 
 
 
@@ -38,49 +39,49 @@ class Graph:
          subgraphs - list of Graph objects
      """
      def __init__(self, vModel):
-          self.Lines=dict()
-          self.Nodes=dict()
-          self.adjList=dict()
-          self.subgraphs=list()
-          self.model=vModel
+          self.Lines = dict()
+          self.Nodes = dict()
+          self.adjList = dict()
+          self.subgraphs = list()
+          self.model = vModel
         
-     def addLine(self,Line):
-          self.Lines[idx]=Line
+     def addLine(self, idx, Line):
+          self.Lines[idx] = Line
         
     
-     def defineNodes(self,dictNodeType):
-          
-           def NodeLines2Type(NodeLines):
-               res=[]
-                 
-               for idx in NodeLines:
-                   
-                   res.append(NodeLines[1])
-               return res
-            
-                     
-           tmpNodeLines=dict()
+     def defineNodes(self, dictNodeType):
+                               
+           tmpNodeLines = dict()
 # пробегаем по всем линиям для каждой вершины строим множество линий входящих/исходящих в нее
 # вместе с типами этих линий  (для определения типа вершины)         
            for idxL in self.Lines:
                for idxN in self.Lines[idxL].Nodes():
-                   if idxN in tmpNodesLines:
-                       tmpNodeLines[idxN]=tmpNodeLines[idxN]|set([(idxL,self.Lines[idxL].Type),])
+                   if idxN in tmpNodeLines:
+                       tmpNodeLines[idxN] = tmpNodeLines[idxN] | set([(idxL,self.Lines[idxL].Type),])
                    else:
-                       tmpNodeLines[idxN]=set([(idxL,self.Lines[idxL].Type),])
+                       tmpNodeLines[idxN] = set([(idxL,self.Lines[idxL].Type),])
                           
-# определяем тип вершины.
+
            for idxN in tmpNodeLines:
+               # определяем тип вершины.
+               (tmpLines,tmpLineTypes) = zip(*tmpNodeLines[idxN])
+               tmplstLineTypes=list(tmpLineTypes)
+               tmplstLineTypes.sort()
                if idxN in dictNodeType:
-                   tmpType=dictNodeType[idxN]
-                   if tmpType<>0:
-                       if self.model.NodeTypes[tmpType].sort() <> NodeLines2Type(tmpNodeLines[idxN]).sort(): 
+                   tmpType = dictNodeType[idxN]
+                   if tmpType <> 0:
+                       tmpNodeTypes=list(self.model.NodeTypes[idxT]["Lines"])
+                       tmpNodeTypes.sort()
+                       if tmpNodeTypes == tmplstLineTypes:
                            raise Exception, "invalid node type in dictNodeType"
                else:
-                   tmpType=-1
+                   tmpType = -1
                    for idxT in self.model.NodeTypes:
-                       if self.model.NodeTypes[idxT].sort() == NodeLines2Type(tmpNodeLines[idxN]).sort():
-                           tmpType=idxT
+                       tmpNodeTypes=list(self.model.NodeTypes[idxT]["Lines"])
+                       tmpNodeTypes.sort()
+                       if tmpNodeTypes == tmplstLineTypes:
+                           tmpType = idxT
                            break
-                   if tmpType<0: raise "no such node in model (node=%s)" %idxN
-               
+                   if tmpType < 0: raise "no such node in model (node=%s)" %idxN
+                   
+               self.Nodes[idxN]=Node(Type=tmpType,Lines=tmpLines)               
--- rg-graph-static/src/rggraph_static/model.py
+++ rg-graph-static/src/rggraph_static/model.py
@@ -11,14 +11,14 @@ class Model:
     """
     def __init__(self,name):
         self.Name=name
-        self.vLineTypes=dict()
-        self.vNodeTypes=dict()
+        self.LineTypes=dict()
+        self.NodeTypes=dict()
     def addLineType(self,LineIdx,**kwargs):
         """ propagator,directed
             propagator="1/(p*p+tau)", directed=0 (simple massive line)
             only for static!!! для динамики нужно оперировать полями
         """
-        self.vLineTypes[LineIdx]=kwargs
+        self.LineTypes[LineIdx]=kwargs
     def addNodeType(self,NodeIdx,**kwargs):
         """ Lines(types),Factor
             Lines=[1,1,1], Factor=1 (simple phi3 vertex)
@@ -26,13 +26,13 @@ class Model:
             Lines=[], Factor=1 (External node?)
 
         """
-        self.vNodeTypes[NodeIdx]=kwargs
+        self.NodeTypes[NodeIdx]=kwargs
     def __str__(self):
         res="Model Name = %s\n\nLine types:\n" %self.Name
-        for idxL in self.vLineTypes:
-            res=res+"\ttype %s : %s\n" %(idxL,self.vLineTypes[idxL])
+        for idxL in self.LineTypes:
+            res=res+"\ttype %s : %s\n" %(idxL,self.LineTypes[idxL])
         res=res+" \nNode types:\n"
-        for idxN in self.vNodeTypes:
-            res=res+"\ttype %s : %s\n" %(idxN,self.vNodeTypes[idxN])
+        for idxN in self.NodeTypes:
+            res=res+"\ttype %s : %s\n" %(idxN,self.NodeTypes[idxN])
         return res
         
