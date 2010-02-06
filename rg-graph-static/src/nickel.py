2639ee49ac0290274365bf0d89892ed0a1433326
Sergey Novikov <dr.snov@gmail.com>
2010-02-06 13:31:08 +0100

    Added Nickel-like canonicalization draft.

---
+++ rg-graph-static/src/nickel.py
@@ -0,0 +1,95 @@
+﻿#!/usr/bin/python
+
+class Nickel(object):
+  """Class to generate graph notations by Nickel.
+  """
+  def __init__(self, graph):
+    self.graph = graph
+    self.end_mapped = max(sum(graph, [])) + 1
+
+  def IsExternal(self, vertex):
+    return vertex < 0
+
+  def IsInternal(self, vertex):
+    return vertex >= 0 and vertex < self.end_mapped
+
+  def IsMapped(self, vertex):
+    return vertex < self.end_mapped
+
+  def SortEdges(self, g):
+    for e in g:
+      e.sort(key=lambda v: v if not self.IsExternal(v) else v + 1000)
+    g.sort()
+
+  def MaxInternal(self, g):
+    return max(sum(g, []), key=lambda v: v if self.IsMapped(v) else v - 1000)
+
+  def GetList(self):
+    """Generates list signature of the diagram"""
+    maxv = self.MaxInternal(self.graph)
+    nick = [[] for i in range(max(1, maxv))]
+    for e in self.graph:
+      se = sorted(e, key=lambda v: v if not self.IsExternal(v) else v + 1000)
+      nick[se[0]].append(se[1])
+    for vv in nick:
+      vv.sort()
+    return nick
+
+
+class Step(object):
+  def __init__(self, edges, nickel_list, node_map, curr_node, free_node):
+    self.edges = edges
+    self.nickel_list = nickel_list
+    self.node_map = node_map
+    self.curr_node = curr_node
+    self.free_node = free_node
+
+  def Expand(self):
+    nodes = AdjacentNodes(self.curr_node, self.edges)
+    edge_rest = [e for e in self.edges if self.curr_node not in e]
+    new_nodes = [n for n in nodes if n > self.free_node]
+    new_nodes = list(set(new_nodes))
+    free_nodes = range(self.free_node, self.free_node + len(new_nodes))
+    for perm in Permutations(free_nodes):
+      node_map = dict(zip(new_nodes, perm))
+      def ex(n):
+        return node_map.get(n, n)
+
+      expanded_nodes = [ex(n) for n in nodes]
+      expanded_nodes.sort()
+      edges = [[ex(n), ex(m)] for [n, m] in edge_rest]
+      node_map.update(self.node_map)
+      yield Step(edges, self.nickel_list + [expanded_nodes],
+                 node_map, self.curr_node + 1,
+                 self.free_node + len(new_nodes))
+
+
+def AdjacentNodes(node, edges):
+  nodes = []
+  for e in edges:
+    if e[0] == node:
+      nodes.append(e[1])
+    elif e[1] == node:
+      nodes.append(e[0])
+  return nodes
+
+
+def Combinations(seq, n):
+  """Generator of all the n-element combinations of the given sequence.
+  """
+  if n == 0:
+    yield seq[0:0]
+  else:
+    for i in range(len(seq)):
+      for tail in Combinations(seq[:i] + seq[i+1:], n - 1):
+        yield seq[i:i+1] + tail
+
+
+def Permutations(seq):
+  """Generator of all the permutations of the given sequence.
+  """
+  return Combinations(seq, len(seq))
+
+
+if __name__ == "__main__":
+  pass
---
+++ rg-graph-static/src/nickel_test.py
@@ -0,0 +1,59 @@
+﻿#!/usr/bin/python
+
+import nickel
+import unittest
+
+class TestNickel(unittest.TestCase):
+  def testGetList(self):
+    g = [[0, -1], [-1, 0]]
+    self.assertEqual(nickel.Nickel(g).GetList(), [[-1, -1]])
+    g.extend([[2, 1], [1, 0]])
+    self.assertEqual(nickel.Nickel(g).GetList(), [[-1, -1, 1],[2]])
+
+
+class TestStep(unittest.TestCase):
+  def compareSteps(self, l, r):
+    self.assertEqual(l.edges, r.edges)
+    self.assertEqual(l.nickel_list, r.nickel_list)
+    self.assertEqual(l.node_map, r.node_map)
+    self.assertEqual(l.curr_node, r.curr_node)
+    self.assertEqual(l.free_node, r.free_node)
+
+  def testExpand(self):
+    ss = nickel.Step([[0,10]], [], {}, 0, 1)
+    l = list(ss.Expand())
+    self.assertEqual(len(l), 1)
+    self.compareSteps(l[0], nickel.Step([], [[1]], {10: 1}, 1, 2))
+
+
+class TestUtil(unittest.TestCase):
+  def testAdjacentNodes(self):
+    self.assertEqual(nickel.AdjacentNodes(1, [[1, 0], [0, 2], [2,1]]),
+                     [0, 2])
+
+  def testCombinations(self):
+    xcomb = nickel.Combinations
+    self.assertEqual(list(xcomb([1, 2, 3], 4)), [])
+    self.assertEqual(list(xcomb(('1', 2), 1)), [('1',), (2,)])
+    self.assertEqual(list(xcomb([1, 2, 3], 2)),
+                     [[1, 2], [1, 3],
+                      [2, 1], [2, 3],
+                      [3, 1], [3, 2]])
+
+  def testPermutations(self):
+    xperm = nickel.Permutations
+    self.assertEqual(list(xperm([])), [[]])
+    self.assertEqual(list(xperm(('1',))), [('1',)])
+    self.assertEqual(list(xperm('ab')), ['ab', 'ba'])
+    perm3 = [[1, 2, 3], [1, 3, 2],
+             [2, 1, 3], [2, 3, 1],
+             [3, 1, 2], [3, 2, 1]]
+    self.assertEqual(list(xperm([1, 2, 3])), perm3)
+    for p in xperm([1, 2, 3]):
+      self.assertEqual(p, perm3.pop(0))
+
+
+unittest.main()  # Calling from the command line invokes all tests.
+
+#if __name__ == "__main__":
+#  unittest.main()
