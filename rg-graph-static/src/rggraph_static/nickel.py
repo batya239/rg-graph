65db083fd82e5654bd9356440c16902baf123a65
Sergey Novikov <dr.snov@gmail.com>
2010-02-11 23:18:34 +0100

    2 space -> 4 space

--- rg-graph-static/src/rggraph_static/nickel.py
+++ rg-graph-static/src/rggraph_static/nickel.py
@@ -1,238 +1,239 @@
 ﻿#!/usr/bin/python
 
 class Nickel(object):
-  """Class to generate Nickel-like graph notations.
+    """Class to generate Nickel-like graph notations.
 
-  Usage:
-  >>> n = nickel.Nickel(edges=[[-1, 0], [0, 1], [1, -1]])
-  >>> n.nodes
-  [[-1, 1], [-1]]
-  >>> n.string
-  'e1-e-'
-  """
-  def __init__(self, edges=None, nickel=None, string=None):
-    self.node_to_char = {-2: '-', -1: 'e'}
-    self.edges = edges
-    self.nickel = nickel
-    self.string = string
+    Usage:
+    >>> n = nickel.Nickel(edges=[[-1, 0], [0, 1], [1, -1]])
+    >>> n.nodes
+    [[-1, 1], [-1]]
+    >>> n.string
+    'e1-e-'
+    """
+    def __init__(self, edges=None, nickel=None, string=None):
+        self.node_to_char = {-2: '-', -1: 'e'}
+        self.edges = edges
+        self.nickel = nickel
+        self.string = string
 
-    if self.edges:
-      for e in self.edges:
-        e.sort()
-      self.edges.sort()
+        if self.edges:
+            for e in self.edges:
+                e.sort()
+            self.edges.sort()
 
-    if self.nickel:
-      for nn in self.nickel:
-        nn.sort()
+        if self.nickel:
+            for nn in self.nickel:
+                nn.sort()
 
-    if edges != None:
-      self.edges = edges
-      self.nickel = self.NickelFromEdges(edges)
-      self.string = self.StringFromNickel(self.nickel)
-    elif nickel != None:
-      self.edges = self.EdgesFromNickel(nickel)
-      self.nickel = nickel
-      self.string = self.StringFromNickel(nickel)
-    elif string != None:
-      self.string = string
-      self.nickel = self.NickelFromString(string)
-      self.edges = self.EdgesFromNickel(self.nickel)
+        if edges != None:
+            self.edges = edges
+            self.nickel = self.NickelFromEdges(edges)
+            self.string = self.StringFromNickel(self.nickel)
+        elif nickel != None:
+            self.edges = self.EdgesFromNickel(nickel)
+            self.nickel = nickel
+            self.string = self.StringFromNickel(nickel)
+        elif string != None:
+            self.string = string
+            self.nickel = self.NickelFromString(string)
+            self.edges = self.EdgesFromNickel(self.nickel)
 
-  def NickelFromEdges(self, edges):
-    max_node = max(sum(edges, []))
-    nickel = [[] for i in range(max(1, max_node))]
-    for e in edges:
-      [s, d] = sorted(e, key=lambda n: n if n >= 0 else 1000)
-      nickel[s].append(d)
-    for nn in nickel:
-      nn.sort()
-    return nickel
+    def NickelFromEdges(self, edges):
+        max_node = max(sum(edges, []))
+        nickel = [[] for i in range(max(1, max_node))]
+        for e in edges:
+            [s, d] = sorted(e, key=lambda n: n if n >= 0 else 1000)
+            nickel[s].append(d)
+        for nn in nickel:
+            nn.sort()
+        return nickel
 
-  def EdgesFromNickel(self, nickel):
-    edges = []
-    for n in range(len(nickel)):
-      for m in nickel[n]:
-        edges.append(sorted([n, m]))
-    edges.sort()
-    return edges
+    def EdgesFromNickel(self, nickel):
+        edges = []
+        for n in range(len(nickel)):
+            for m in nickel[n]:
+                edges.append(sorted([n, m]))
+        edges.sort()
+        return edges
 
-  def StringFromNickel(self, nickel):
-    temp = [nn + [-2] for nn in nickel]
-    temp = sum(temp, [])
-    temp = [str(self.node_to_char.get(n, n)) for n in temp]
-    return ''.join(temp)
+    def StringFromNickel(self, nickel):
+        temp = [nn + [-2] for nn in nickel]
+        temp = sum(temp, [])
+        temp = [str(self.node_to_char.get(n, n)) for n in temp]
+        return ''.join(temp)
 
-  def NickelFromString(self, string):
-    char_to_node = dict(zip(self.node_to_char.values(),
-                            self.node_to_char.keys()))
-    flat_nickel = [int(char_to_node.get(c, c)) for c in string]
-    nickel = []
-    accum = []
-    for n in flat_nickel:
-      if n != -2:
-        accum.append(n)
-      else:
-        nickel.append(accum)
+    def NickelFromString(self, string):
+        char_to_node = dict(zip(self.node_to_char.values(),
+                                self.node_to_char.keys()))
+        flat_nickel = [int(char_to_node.get(c, c)) for c in string]
+        nickel = []
         accum = []
-    return nickel
+        for n in flat_nickel:
+            if n != -2:
+                accum.append(n)
+            else:
+                nickel.append(accum)
+                accum = []
+        return nickel
 
 
 class InputError(Exception):
-  """Rased when when Canonicalization is called with wrong input.
-  """
-  pass
+    """Rased when when Canonicalization is called with wrong input.
+    """
+    pass
 
 
 class Canonicalize(object):
-  """Class to find canonical node maping and to give Nickel node list for it.
+    """Class to find canonical node maping and to give Nickel node list for it.
 
-  Negative nodes assumed to be external. Non-negative ones - internal.
-  Usage:
-    c = nickel.Canonicalize([[-1, 10], [-1, 11], [10, 11]])
-    assertEqual(c.nickel, [[-1, 1], [-1]])
-    assertEqual(c.num_symmetries, 2)
-    assertEqual(c.node_maps, [{10: 0, 11: 1}, {10: 1, 11: 0}])
-    assertTrue(c.is_valid)
-  """
-  def __init__(self, edges):
-    if not [n for n in sum(edges, []) if n < 0]:
-      raise InputError('No external (negative) nodes found in the input.')
-    if not IsConnected(edges):
-      raise InputError('Input edge list is an unconnected graph.')
+    Negative nodes assumed to be external. Non-negative ones - internal.
+    Usage:
+        c = nickel.Canonicalize([[-1, 10], [-1, 11], [10, 11]])
+        assertEqual(c.nickel, [[-1, 1], [-1]])
+        assertEqual(c.num_symmetries, 2)
+        assertEqual(c.node_maps, [{10: 0, 11: 1}, {10: 1, 11: 0}])
+        assertTrue(c.is_valid)
+    """
+    def __init__(self, edges):
+        if not [n for n in sum(edges, []) if n < 0]:
+            raise InputError('No external (negative) nodes found in the input.')
+        if not IsConnected(edges):
+            raise InputError('Input edge list is an unconnected graph.')
 
-    self.orig = edges
+        self.orig = edges
 
-    self.num_internal_nodes = 0
-    for n in set(sum(edges, [])):
-      if n >= 0:
-        self.num_internal_nodes += 1
+        self.num_internal_nodes = 0
+        for n in set(sum(edges, [])):
+            if n >= 0:
+                self.num_internal_nodes += 1
 
-    # Shift original nodes to free space for canonical numbers.
-    self.offset = max(100, self.num_internal_nodes)
-    def shift(n):
-      return n + self.offset if n >= 0 else -1
-    self.edges = [[shift(n), shift(m)] for [n, m] in edges]
+        # Shift original nodes to free space for canonical numbers.
+        self.offset = max(100, self.num_internal_nodes)
+        def shift(n):
+            return n + self.offset if n >= 0 else -1
+        self.edges = [[shift(n), shift(m)] for [n, m] in edges]
 
-    # Do the work.
-    self.curr_states = self.InitStates(self.edges)
-    for _ in range(self.num_internal_nodes):
-      self.curr_states = self.DoExpand(self.curr_states)
+        # Do the work.
+        self.curr_states = self.InitStates(self.edges)
+        for _ in range(self.num_internal_nodes):
+            self.curr_states = self.DoExpand(self.curr_states)
 
-    # Collect results.
-    nickels = [s.nickel_list for s in self.curr_states]
-    self.num_symmetries = len(nickels)
-    self.nickel = min(nickels)
-    is_valid = self.nickel == max(nickels)
-    self.is_valid = is_valid and (len(sum(self.nickel, [])) == len(self.edges))
-    # Shift back original nodes.
-    self.node_maps = []
-    for state in self.curr_states:
-      curr_node_map = {}
-      for key, value in state.node_map.items():
-        curr_node_map[key - self.offset] = value
-      self.node_maps.append(curr_node_map)
+        # Collect results.
+        nickels = [s.nickel_list for s in self.curr_states]
+        self.num_symmetries = len(nickels)
+        self.nickel = min(nickels)
+        is_valid = self.nickel == max(nickels)
+        self.is_valid = is_valid and (len(sum(self.nickel, [])) ==
+                                      len(self.edges))
+        # Shift back original nodes.
+        self.node_maps = []
+        for state in self.curr_states:
+            curr_node_map = {}
+            for key, value in state.node_map.items():
+                curr_node_map[key - self.offset] = value
+            self.node_maps.append(curr_node_map)
 
-  def __str__(self):
-      return Nickel(nickel=self.nickel).string
+    def __str__(self):
+        return Nickel(nickel=self.nickel).string
 
-  def InitStates(self, edges):
-    """Creates all possible initial states for node 0."""
-    boundary_nodes = list(set(AdjacentNodes(-1, edges)))
-    states = []
-    for bound in boundary_nodes:
-      states.append(Expander(MapNodes2({bound: 0}, self.edges),
-                             [], {bound: 0}, 0, 1))
-    return states
+    def InitStates(self, edges):
+        """Creates all possible initial states for node 0."""
+        boundary_nodes = list(set(AdjacentNodes(-1, edges)))
+        states = []
+        for bound in boundary_nodes:
+            states.append(Expander(MapNodes2({bound: 0}, self.edges),
+                                                         [], {bound: 0}, 0, 1))
+        return states
 
-  def DoExpand(self, curr_states):
-    states = [list(s.Expand()) for s in curr_states]
-    states = sum(states, [])
-    minimum = min(states)
-    return [s for s in states if s == minimum]
+    def DoExpand(self, curr_states):
+        states = [list(s.Expand()) for s in curr_states]
+        states = sum(states, [])
+        minimum = min(states)
+        return [s for s in states if s == minimum]
 
 
 class Expander(object):
-  """A helper class for Canonicalize.
+    """A helper class for Canonicalize.
 
-  Eats edges adjacent to already canonicalized nodes and canonicalizes them.
-  """
-  def __init__(self, edges, nickel_list, node_map, curr_node, free_node):
-    self.edges = edges
-    self.nickel_list = nickel_list  # Nested lists must be sorted.
-    self.node_map = node_map
-    self.curr_node = curr_node
-    self.free_node = free_node
+    Eats edges adjacent to already canonicalized nodes and canonicalizes them.
+    """
+    def __init__(self, edges, nickel_list, node_map, curr_node, free_node):
+        self.edges = edges
+        self.nickel_list = nickel_list    # Nested lists must be sorted.
+        self.node_map = node_map
+        self.curr_node = curr_node
+        self.free_node = free_node
 
-  def __cmp__(self, other):
-    # Shorten the long list to not let unexpanded one win.
-    min_len = min(len(self.nickel_list), len(other.nickel_list))
-    return cmp(self.nickel_list[:min_len], other.nickel_list[:min_len])
+    def __cmp__(self, other):
+        # Shorten the long list to not let unexpanded one win.
+        min_len = min(len(self.nickel_list), len(other.nickel_list))
+        return cmp(self.nickel_list[:min_len], other.nickel_list[:min_len])
 
-  def Expand(self):
-    nodes = AdjacentNodes(self.curr_node, self.edges)
-    edge_rest = [e for e in self.edges if self.curr_node not in e]
-    new_nodes = [n for n in nodes if n > self.free_node]
-    new_nodes = list(set(new_nodes))
-    free_nodes = range(self.free_node, self.free_node + len(new_nodes))
-    for perm in Permutations(free_nodes):
-      node_map = dict(zip(new_nodes, perm))
-      expanded_nodes = MapNodes1(node_map, nodes)
-      expanded_nodes.sort()
-      edges = MapNodes2(node_map, edge_rest)
-      node_map.update(self.node_map)
-      yield Expander(edges, self.nickel_list + [expanded_nodes],
-                     node_map, self.curr_node + 1,
-                     self.free_node + len(new_nodes))
+    def Expand(self):
+        nodes = AdjacentNodes(self.curr_node, self.edges)
+        edge_rest = [e for e in self.edges if self.curr_node not in e]
+        new_nodes = [n for n in nodes if n > self.free_node]
+        new_nodes = list(set(new_nodes))
+        free_nodes = range(self.free_node, self.free_node + len(new_nodes))
+        for perm in Permutations(free_nodes):
+            node_map = dict(zip(new_nodes, perm))
+            expanded_nodes = MapNodes1(node_map, nodes)
+            expanded_nodes.sort()
+            edges = MapNodes2(node_map, edge_rest)
+            node_map.update(self.node_map)
+            yield Expander(edges, self.nickel_list + [expanded_nodes],
+                           node_map, self.curr_node + 1,
+                           self.free_node + len(new_nodes))
 
 
 def AdjacentNodes(node, edges):
-  nodes = []
-  for e in edges:
-    if e[0] == node:
-      nodes.append(e[1])
-    elif e[1] == node:
-      nodes.append(e[0])
-  return nodes
+    nodes = []
+    for e in edges:
+        if e[0] == node:
+            nodes.append(e[1])
+        elif e[1] == node:
+            nodes.append(e[0])
+    return nodes
 
 
 def IsConnected(edges):
-  if not edges:
-    return False
-  else:
-    old_len = 0
-    visited = set(edges[0])
-    while old_len < len(visited):
-      old_len = len(visited)
-      for [s, d] in edges:
-        if s in visited or d in visited:
-          visited.update([s, d])
-    return visited == set(sum(edges, []))
+    if not edges:
+        return False
+    else:
+        old_len = 0
+        visited = set(edges[0])
+        while old_len < len(visited):
+            old_len = len(visited)
+            for [s, d] in edges:
+                if s in visited or d in visited:
+                    visited.update([s, d])
+        return visited == set(sum(edges, []))
 
 
 def Combinations(seq, n):
-  """Generator of all the n-element combinations of the given sequence.
-  """
-  if n == 0:
-    yield seq[0:0]
-  else:
-    for i in range(len(seq)):
-      for tail in Combinations(seq[:i] + seq[i+1:], n - 1):
-        yield seq[i:i+1] + tail
+    """Generator of all the n-element combinations of the given sequence.
+    """
+    if n == 0:
+        yield seq[0:0]
+    else:
+        for i in range(len(seq)):
+            for tail in Combinations(seq[:i] + seq[i+1:], n - 1):
+                yield seq[i:i+1] + tail
 
 
 def Permutations(seq):
-  """Generator of all the permutations of the given sequence.
-  """
-  return Combinations(seq, len(seq))
+    """Generator of all the permutations of the given sequence.
+    """
+    return Combinations(seq, len(seq))
 
 
 def MapNodes1(dict, list_of_nodes):
-  return [dict.get(n, n) for n in list_of_nodes]
+    return [dict.get(n, n) for n in list_of_nodes]
 
 
 def MapNodes2(dict, list_of_lists):
-  return [MapNodes1(dict, x) for x in list_of_lists]
+    return [MapNodes1(dict, x) for x in list_of_lists]
 
 
 if __name__ == "__main__":
-  pass
+    pass
--- rg-graph-static/src/rggraph_static/nickel_test.py
+++ rg-graph-static/src/rggraph_static/nickel_test.py
@@ -4,167 +4,165 @@ import nickel
 import unittest
 
 class TestNickel(unittest.TestCase):
-  def testNickelFromEdges(self):
-    e = nickel.Nickel(edges=[[0, -1], [-1, 0]])
-    self.assertEqual(e.nickel, [[-1, -1]])
-    self.assertEqual(e.string, 'ee-')
+    def testNickelFromEdges(self):
+        e = nickel.Nickel(edges=[[0, -1], [-1, 0]])
+        self.assertEqual(e.nickel, [[-1, -1]])
+        self.assertEqual(e.string, 'ee-')
 
-    s = nickel.Nickel(string='ee-')
-    self.assertEqual(s.nickel, [[-1, -1]])
-    self.assertEqual(s.edges, e.edges)
+        s = nickel.Nickel(string='ee-')
+        self.assertEqual(s.nickel, [[-1, -1]])
+        self.assertEqual(s.edges, e.edges)
 
-    ee = nickel.Nickel(edges=[[0, -1], [-1, 0], [2, 1], [1, 0]])
-    self.assertEqual(ee.nickel, [[-1, -1, 1], [2]])
-    self.assertEqual(ee.string, 'ee1-2-')
+        ee = nickel.Nickel(edges=[[0, -1], [-1, 0], [2, 1], [1, 0]])
+        self.assertEqual(ee.nickel, [[-1, -1, 1], [2]])
+        self.assertEqual(ee.string, 'ee1-2-')
 
-    ss = nickel.Nickel(string='ee1-2-')
-    self.assertEqual(ss.nickel, [[-1, -1, 1], [2]])
-    self.assertEqual(ss.edges, ee.edges)
+        ss = nickel.Nickel(string='ee1-2-')
+        self.assertEqual(ss.nickel, [[-1, -1, 1], [2]])
+        self.assertEqual(ss.edges, ee.edges)
 
 
 class TestCanonicalize(unittest.TestCase):
-  def testInit(self):
-    c = nickel.Canonicalize([[-1, 0]])
-    init = c.InitStates([[-1, 10]])
-    self.assertEqual(len(init), 1)
-    self.assertEqual(len(c.InitStates([[-1, 10], [10, 11], [11, -1]])), 2)
+    def testInit(self):
+        c = nickel.Canonicalize([[-1, 0]])
+        init = c.InitStates([[-1, 10]])
+        self.assertEqual(len(init), 1)
+        self.assertEqual(len(c.InitStates([[-1, 10], [10, 11], [11, -1]])), 2)
 
-  def testRaise(self):
-    self.assertRaises(nickel.InputError, nickel.Canonicalize, [[0, 1]])
-    self.assertRaises(nickel.InputError, nickel.Canonicalize, [[0, 1], [2, 3]])
+    def testRaise(self):
+        self.assertRaises(nickel.InputError, nickel.Canonicalize, [[0, 1]])
+        self.assertRaises(nickel.InputError, nickel.Canonicalize, [[0, 1], [2, 3]])
 
-  def testCanon(self):
-    c = nickel.Canonicalize([[-1, 0]])
-    self.assertEqual(c.num_symmetries, 1)
-    self.assertEqual(c.nickel, [[-1]])
-    self.assertTrue(c.is_valid)
+    def testCanon(self):
+        c = nickel.Canonicalize([[-1, 0]])
+        self.assertEqual(c.num_symmetries, 1)
+        self.assertEqual(c.nickel, [[-1]])
+        self.assertTrue(c.is_valid)
 
-  def testCanon1(self):
-    c = nickel.Canonicalize([[-1, 0], [-1, 0]])
-    self.assertEqual(c.num_symmetries, 1)
-    self.assertEqual(c.nickel, [[-1, -1]])
-    self.assertTrue(c.is_valid)
+    def testCanon1(self):
+        c = nickel.Canonicalize([[-1, 0], [-1, 0]])
+        self.assertEqual(c.num_symmetries, 1)
+        self.assertEqual(c.nickel, [[-1, -1]])
+        self.assertTrue(c.is_valid)
 
-  def testCanon2(self):
-    c = nickel.Canonicalize([[-1, 10], [-1, 11], [10, 11]])
-    self.assertEqual(c.nickel, [[-1, 1], [-1]])
-    self.assertEqual(c.num_symmetries, 2)
-    self.assertEqual(c.node_maps, [{10: 0, 11: 1}, {10: 1, 11: 0}])
-    self.assertTrue(c.is_valid)
+    def testCanon2(self):
+        c = nickel.Canonicalize([[-1, 10], [-1, 11], [10, 11]])
+        self.assertEqual(c.nickel, [[-1, 1], [-1]])
+        self.assertEqual(c.num_symmetries, 2)
+        self.assertEqual(c.node_maps, [{10: 0, 11: 1}, {10: 1, 11: 0}])
+        self.assertTrue(c.is_valid)
 
-  def testCanon3(self):
-    c = nickel.Canonicalize([[-1, 11], [-1, 11], [-1, 10], [10, 11]])
-    self.assertEqual(c.num_symmetries, 1)
-    self.assertEqual(c.nickel, [[-1, -1, 1], [-1]])
-    self.assertTrue(c.is_valid)
+    def testCanon3(self):
+        c = nickel.Canonicalize([[-1, 11], [-1, 11], [-1, 10], [10, 11]])
+        self.assertEqual(c.num_symmetries, 1)
+        self.assertEqual(c.nickel, [[-1, -1, 1], [-1]])
+        self.assertTrue(c.is_valid)
 
-  def testCanon4(self):
-    c = nickel.Canonicalize([[-1, 0], [0, 1], [0, 2], [1, 2], [1, 3], [2, 3],
-                            [3, -1]])
-    self.assertEqual(c.num_symmetries, 4)
-    self.assertEqual(c.nickel, [[-1, 1, 2], [2, 3], [3], [-1]])
-    self.assertTrue(c.is_valid)
+    def testCanon4(self):
+        c = nickel.Canonicalize([[-1, 0], [0, 1], [0, 2], [1, 2], [1, 3], [2, 3],
+                                                        [3, -1]])
+        self.assertEqual(c.num_symmetries, 4)
+        self.assertEqual(c.nickel, [[-1, 1, 2], [2, 3], [3], [-1]])
+        self.assertTrue(c.is_valid)
 
-  def testCanon5(self):
-    c = nickel.Canonicalize([[-1, 3], [-1, 4], [-1, 5], [3, 4], [3, 5], [4, 6],
-                             [5, 7], [6, 8], [6, 8], [7, 9], [7, 9], [8, 10],
-                             [9, 11], [10, 11], [10, 11]])
-    self.assertEqual(c.num_symmetries, 2)
-    self.assertEqual(c.nickel, [[-1, 1, 2], [-1, 3], [-1, 4], [5, 5], [6, 6],
-                                [7], [8], [8, 8], []])
-    self.assertTrue(c.is_valid)
+    def testCanon5(self):
+        c = nickel.Canonicalize([[-1, 3], [-1, 4], [-1, 5], [3, 4], [3, 5],
+                                 [4, 6], [5, 7], [6, 8], [6, 8], [7, 9], [7, 9],
+                                 [8, 10], [9, 11], [10, 11], [10, 11]])
+        self.assertEqual(c.num_symmetries, 2)
+        self.assertEqual(c.nickel, [[-1, 1, 2], [-1, 3], [-1, 4], [5, 5],
+                                    [6, 6], [7], [8], [8, 8], []])
+        self.assertTrue(c.is_valid)
 
-  def testCanon6(self):
-    c = nickel.Canonicalize([[-1, 3], [-1, 4], [-1, 7], [3, 4], [3, 5], [4, 6],
-                             [5, 6], [5, 7], [6, 8], [7, 9], [8, 9], [8, 10],
-                             [9, 11], [10, 11], [10, 11]])
-    self.assertEqual(c.num_symmetries, 1)
-    self.assertEqual(c.nickel, [[-1, 1, 2], [-1, 3], [3, 4], [5], [-1, 6],
-                                [6, 7], [8], [8, 8], []])
-    self.assertTrue(c.is_valid)
+    def testCanon6(self):
+        c = nickel.Canonicalize([[-1, 3], [-1, 4], [-1, 7], [3, 4], [3, 5],
+                                 [4, 6], [5, 6], [5, 7], [6, 8], [7, 9], [8, 9],
+                                 [8, 10], [9, 11], [10, 11], [10, 11]])
+        self.assertEqual(c.num_symmetries, 1)
+        self.assertEqual(c.nickel, [[-1, 1, 2], [-1, 3], [3, 4], [5], [-1, 6],
+                                    [6, 7], [8], [8, 8], []])
+        self.assertTrue(c.is_valid)
 
-  def testCanon7(self):
-    c = nickel.Canonicalize([[-1, 2], [-1, 3], [2, 3], [2, 4], [3, 5], [4, 6],
-                             [4, 7], [5, 6], [5, 7], [6, 8], [7, 9], [8, 9],
-                             [8, 9]])
-    self.assertEqual(c.num_symmetries, 4)
-    self.assertEqual(c.nickel, [[-1, 1, 2], [-1, 3], [4, 5], [4, 5], [6], [7],
-                                [7, 7], []])
-    self.assertTrue(c.is_valid)
+    def testCanon7(self):
+        c = nickel.Canonicalize([[-1, 2], [-1, 3], [2, 3], [2, 4], [3, 5],
+                                 [4, 6], [4, 7], [5, 6], [5, 7], [6, 8], [7, 9],
+                                 [8, 9], [8, 9]])
+        self.assertEqual(c.num_symmetries, 4)
+        self.assertEqual(c.nickel, [[-1, 1, 2], [-1, 3], [4, 5], [4, 5], [6],
+                                    [7], [7, 7], []])
+        self.assertTrue(c.is_valid)
 
 
 class TestExpander(unittest.TestCase):
-  def compareExpanders(self, l, r):
-    self.assertEqual(l.curr_node, r.curr_node)
-    self.assertEqual(l.free_node, r.free_node)
-    self.assertEqual(l.edges, r.edges)
-    self.assertEqual(l.nickel_list, r.nickel_list)
-    self.assertEqual(l.node_map, r.node_map)
+    def compareExpanders(self, l, r):
+        self.assertEqual(l.curr_node, r.curr_node)
+        self.assertEqual(l.free_node, r.free_node)
+        self.assertEqual(l.edges, r.edges)
+        self.assertEqual(l.nickel_list, r.nickel_list)
+        self.assertEqual(l.node_map, r.node_map)
 
-  def testExpand(self):
-    input = nickel.Expander([[-1, 0]], [], {}, 0, 1)
-    output = nickel.Expander([], [[-1]], {}, 1, 1)
-    l = list(input.Expand())
-    self.assertEqual(len(l), 1)
-    self.compareExpanders(l[0], output)
+    def testExpand(self):
+        input = nickel.Expander([[-1, 0]], [], {}, 0, 1)
+        output = nickel.Expander([], [[-1]], {}, 1, 1)
+        l = list(input.Expand())
+        self.assertEqual(len(l), 1)
+        self.compareExpanders(l[0], output)
 
-  def testExpand2(self):
-    input = nickel.Expander([[-1, 1], [1, 2], [1, 13], [1, 14]], [[1, 2]],
-                            {10: 0, 11: 1, 12: 2}, 1, 3)
-    output = nickel.Expander([], [[1, 2], [-1, 2, 3, 4]],
-                             {10: 0, 11: 1, 12: 2, 13: 3, 14: 4}, 2, 5)
-    l = list(input.Expand())
-    self.assertEqual(len(l), 2)
-    self.compareExpanders(l[0], output)
+    def testExpand2(self):
+        input = nickel.Expander([[-1, 1], [1, 2], [1, 13], [1, 14]], [[1, 2]],
+                                {10: 0, 11: 1, 12: 2}, 1, 3)
+        output = nickel.Expander([], [[1, 2], [-1, 2, 3, 4]],
+                                 {10: 0, 11: 1, 12: 2, 13: 3, 14: 4}, 2, 5)
+        l = list(input.Expand())
+        self.assertEqual(len(l), 2)
+        self.compareExpanders(l[0], output)
 
-  def testStopExpand(self):
-    output = nickel.Expander([], [[-1, 1]], {10: 1}, 1, 2)
-    l = list(output.Expand())
-    self.assertEqual(l[0].nickel_list, [[-1, 1], []])
+    def testStopExpand(self):
+        output = nickel.Expander([], [[-1, 1]], {10: 1}, 1, 2)
+        l = list(output.Expand())
+        self.assertEqual(l[0].nickel_list, [[-1, 1], []])
 
-  def testCmp(self):
-    input = nickel.Expander([[-1, 0], [0, 10]], [], {}, 0, 1)
-    output = nickel.Expander([], [[-1, 1]], {10: 1}, 1, 2)
-    self.assertEqual(input, output)
+    def testCmp(self):
+        input = nickel.Expander([[-1, 0], [0, 10]], [], {}, 0, 1)
+        output = nickel.Expander([], [[-1, 1]], {10: 1}, 1, 2)
+        self.assertEqual(input, output)
 
 
 
 class TestUtil(unittest.TestCase):
-  def testAdjacentNodes(self):
-    self.assertEqual(nickel.AdjacentNodes(1, [[1, 0], [0, 2], [2,1]]),
-                     [0, 2])
-    self.assertEqual(nickel.AdjacentNodes(0, [[-1, 0]]), [-1])
+    def testAdjacentNodes(self):
+        self.assertEqual(nickel.AdjacentNodes(1, [[1, 0], [0, 2], [2,1]]),
+                         [0, 2])
+        self.assertEqual(nickel.AdjacentNodes(0, [[-1, 0]]), [-1])
 
-  def testCombinations(self):
-    xcomb = nickel.Combinations
-    self.assertEqual(list(xcomb([1, 2, 3], 4)), [])
-    self.assertEqual(list(xcomb(('1', 2), 1)), [('1',), (2,)])
-    self.assertEqual(list(xcomb([1, 2, 3], 2)),
-                     [[1, 2], [1, 3],
-                      [2, 1], [2, 3],
-                      [3, 1], [3, 2]])
+    def testCombinations(self):
+        xcomb = nickel.Combinations
+        self.assertEqual(list(xcomb([1, 2, 3], 4)), [])
+        self.assertEqual(list(xcomb(('1', 2), 1)), [('1',), (2,)])
+        self.assertEqual(list(xcomb([1, 2, 3], 2)),
+                         [[1, 2], [1, 3], [2, 1], [2, 3], [3, 1], [3, 2]])
 
-  def testPermutations(self):
-    xperm = nickel.Permutations
-    self.assertEqual(list(xperm([])), [[]])
-    self.assertEqual(list(xperm(('1',))), [('1',)])
-    self.assertEqual(list(xperm('ab')), ['ab', 'ba'])
-    perm3 = [[1, 2, 3], [1, 3, 2],
-             [2, 1, 3], [2, 3, 1],
-             [3, 1, 2], [3, 2, 1]]
-    self.assertEqual(list(xperm([1, 2, 3])), perm3)
-    for p in xperm([1, 2, 3]):
-      self.assertEqual(p, perm3.pop(0))
+    def testPermutations(self):
+        xperm = nickel.Permutations
+        self.assertEqual(list(xperm([])), [[]])
+        self.assertEqual(list(xperm(('1',))), [('1',)])
+        self.assertEqual(list(xperm('ab')), ['ab', 'ba'])
+        perm3 = [[1, 2, 3], [1, 3, 2],
+                 [2, 1, 3], [2, 3, 1],
+                 [3, 1, 2], [3, 2, 1]]
+        self.assertEqual(list(xperm([1, 2, 3])), perm3)
+        for p in xperm([1, 2, 3]):
+            self.assertEqual(p, perm3.pop(0))
 
-  def testIsConnected(self):
-    conn = nickel.IsConnected
-    self.assertFalse(conn([]))
-    self.assertTrue(conn([[-1, 2]]))
-    self.assertTrue(conn([[-1, 0], [0, 1]]))
-    self.assertFalse(conn([[-1, 0], [2, 1]]))
+    def testIsConnected(self):
+        conn = nickel.IsConnected
+        self.assertFalse(conn([]))
+        self.assertTrue(conn([[-1, 2]]))
+        self.assertTrue(conn([[-1, 0], [0, 1]]))
+        self.assertFalse(conn([[-1, 0], [2, 1]]))
 
 
 unittest.main()  # Calling from the command line invokes all tests.
 
 #if __name__ == "__main__":
-#  unittest.main()
+#    unittest.main()
