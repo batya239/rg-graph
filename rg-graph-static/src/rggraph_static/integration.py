0b1d2b9c54c06fdc220859c1b89469f2ecb20991
Mikhail Kompaniets <mkompan@gmail.com>
2010-02-26 19:25:13 +0300

    calculate integrals without ginac.normal()

--- rg-graph-static/src/rggraph_static/integration.py
+++ rg-graph-static/src/rggraph_static/integration.py
@@ -85,6 +85,131 @@ def AvarageByExtDirection(s_expr,str_ext_cos_atoms,degree):
 #            print t_expr
                 res = res + t_expr
         return res
+    
+
+def PrepareFactorizedNoSimplify(fact_expr, space_dim):
+# TODO: p=1 m=1 Надо делать в каком-то другом месте, до того как выражение попадает сюда
+# TODO: усреднение по направлениям p (eps)
+# TODO: детерминанант по модулям (eps)
+# TODO: инверсия по модулям (0-inf) -> (0 - 1)
+# TODO: замена косинусов и детерминант по углам
+# TODO: разложение в ряд по eps
+# TODO: вывод.    
+
+    def g_cos(idx1, idx2, vars_dict):
+        return vars_dict["ct_%s_%s" %(idx1,idx2)]
+    def g_sin(idx1, idx2, vars_dict):
+        return (1-g_cos(idx1,idx2,vars_dict)**2)**0.5 
+    utils.print_time("Prepare: start")
+#    expr = k_op
+    str_atom_set = ginac.GetVarsAsStr(fact_expr.factor * fact_expr.other)
+    (ext_moment_atoms, ext_cos_atoms, int_moment_atoms, 
+                       int_cos_atoms, other_atoms) = SplitAtoms(str_atom_set)
+    if len(ext_moment_atoms | other_atoms)>0:
+        raise  NotImplementedError, "Don't know what to do with following atoms: %s" %(ext_moment_atoms|other_atoms)
+    expr_o = AvarageByExtDirection(fact_expr.other, ext_cos_atoms, 2)
+#    print "\nExtDir:\n"
+#    pretty_print(expr)
+
+# TODO: search for int_cos_atoms again it changed after avaraging 
+    (ext_moment_atoms, ext_cos_atoms, int_moment_atoms, 
+                       int_cos_atoms, other_atoms) = SplitAtoms(ginac.GetVarsAsStr(fact_expr.factor * expr_o))
+    (g_expr_f,g_expr_o, g_vars) = ginac.sympy2swiginacFactorized(fact_expr.factor,expr_o)
+#    print g_vars
+#    print int_cos_atoms
+
+# доопределяем недостающие переменные самое важное d и e, остальное на самом деле вроде не нужно.
+    for idx in ["d", "e"]+list(int_moment_atoms)+list(int_cos_atoms):
+        if  idx not in g_vars:
+            g_vars[idx] = swiginac.symbol(idx)
+    utils.print_time("Prepare:before normal")        
+# общий знаменатель
+#    g_expr = g_expr_f * swiginac.normal(g_expr_o)
+    g_expr = g_expr_f * g_expr_o
+    utils.print_time("Prepare:after normal ")
+    
+    d = g_vars["d"]
+# детерминанты по импульсам
+    for int_moment in list(int_moment_atoms):
+        g_expr = g_expr * g_vars[int_moment] ** ( d - 1)
+        
+#Инверсия
+    inv_moments = set([])
+    for idx in list(int_moment_atoms):
+        reg = regex.match("^q(\d+)$",idx)
+        if reg:
+            t_inv = "y%s" %reg.groups()[0]
+            inv_moments = inv_moments | set([t_inv,])
+            g_vars[t_inv] = swiginac.symbol(t_inv)
+            g_expr = g_expr.subs(g_vars[idx] == (1 - g_vars[t_inv]) / g_vars[t_inv]) / g_vars[t_inv] / g_vars[t_inv] 
+        else:
+            raise ValueError, "Unknown internal momenta %s" %idx
+    utils.print_time("Prepare: inversion")
+# TODO: это приведение к общ. знаменателю может занимать много времени 
+# общий знаменатель
+#    g_expr = swiginac.normal(g_expr)
+    
+        
+#замена на полярные углы
+
+#    n>=1
+#q0 x qn = ct_0_n
+#(1-ct_0_n**2)**((d-3)/2)
+#
+#    n>=2
+#q1 x qn =  ct_0_1*ct_0_n+ ( 1 - ct_0_1**2)**(0.5)*(1-ct_0_n**2)**(0.5)*ct_1_n
+#q1 x qn =  ct_0_1*ct_0_n+ sin_0_1*sin_0_n*ct_1_n    
+#(1-ct_1_n**2)**((d-4)/2)
+#
+#    n>=3
+#q2 x qn = ct_0_2*ct_0_n+(1-ct_0_2**2)**0.5*(1-ct_0_n**2)**0.5*(ct_1_2*ct_1_n+(1-ct_1_2**2)**0.5*(1-ct_1_n*2)*0.5)*ct_2_n)
+#q2 x qn = ct_0_2*ct_0_n+sin_0_2*sin_0_n*(ct_1_2*ct_1_n+sin_1_2*sin_1_n*ct_2_n)
+#(1-ct_2_n**2)**((d-5)/2)
+
+#define polar cosines in swiginac
+    polar_cos = set([])
+    int_moment_atoms_list = list(int_moment_atoms)
+    for idx1 in range(len(int_moment_atoms_list)):
+        for idx2 in range(idx1+1,len(int_moment_atoms_list)):
+            t_polar_cos = "ct_%s_%s" % (idx1,idx2)
+            g_vars[t_polar_cos] = swiginac.symbol(t_polar_cos)
+            polar_cos = polar_cos | set([t_polar_cos,])
+    
+    for atom in int_cos_atoms:
+        reg = regex.match("(q\d+)x(q\d+)",atom)
+        if reg:
+            idx1 = int_moment_atoms_list.index(reg.groups()[0])
+            idx2 = int_moment_atoms_list.index(reg.groups()[1])
+            if idx1 > idx2:
+                idx1, idx2 = idx2, idx1
+            if space_dim - idx1 -3 < 0:
+                raise NotImplementedError, " number of independent moments more then d-2 . d = %s" %space_dim
+             
+            if idx1 == 0 :
+                det = g_sin(idx1, idx2, g_vars)  ** (d - 3) 
+                subst = g_cos(idx1, idx2, g_vars)
+            elif idx1 == 1 :
+                det = g_sin(idx1, idx2, g_vars) ** (d - 4)
+                subst = (g_cos(0, idx1, g_vars) * g_cos(0, idx2, g_vars) +
+                         g_sin(0, idx1, g_vars) * g_sin(0, idx2, g_vars) * 
+                         g_cos(idx1, idx2, g_vars))
+            elif idx1 == 2 :
+                det = g_sin(idx1, idx2, g_vars) ** (d - 5)
+                subst = (g_cos(0, idx1, g_vars) * g_cos(0, idx2, g_vars) +
+                         g_sin(0, idx1, g_vars) * g_sin(0, idx2, g_vars) *
+                         (g_cos(1, idx1, g_vars) * g_cos(1, idx2, g_vars) +
+                         g_sin(1, idx1, g_vars) * g_sin(1, idx2, g_vars) * 
+                         g_cos(idx1, idx2, g_vars)))
+            else:
+                raise NotImplementedError, " number of independent moments more then d-2 . d = %s" %space_dim
+#            print "\n\n subst = %s\n det = %s\n\n" %(subst, det)
+            g_expr = g_expr.subs(g_vars[atom] == subst) * det
+        else:
+            raise ValueError,  "Unknown scalar product of internal moments  %s " %atom
+    utils.print_time("Prepare: end")  
+
+    
+    return (g_expr,g_vars)    
                     
 
 def PrepareFactorized(fact_expr, space_dim):
@@ -515,7 +640,7 @@ def CompileMCCode(prog_name):
     print "Compiling %s ... " %prog_name,
     sys.stdout.flush()
     process = subprocess.Popen(["gcc", code_name, "-lm", "-lpthread", 
-                                "-lpvegas", "-o", prog_name], shell=False, 
+                                "-lpvegas", "-O0", "-o", prog_name], shell=False, 
                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     exit_code = process.wait()
     (std_out, std_err) = process.communicate()
