9e750a79568164b2110b0eb45388bcd5c89b4740
Mikhail Kompaniets <mkompan@gmail.com>
2010-03-14 17:25:42 +0300

    some test integrals

--- rg-graph-static/src/custom.py
+++ rg-graph-static/src/custom.py
@@ -16,7 +16,7 @@ print phi3.name
 
 base_name = "fns_custom"
 TARGET = 4
-NLOOPS = 3
+NLOOPS = 4
 print "NLOOPS = " , NLOOPS
 n_epsilon_series = TARGET - NLOOPS
 NPOINTS = 10000
@@ -38,27 +38,27 @@ var('q1 q2 q3 q4 q1xq2 q1xq3 q1xq4 q2xq3 q2xq4 q3xq4 a b')
 ### 4loop
 #
 
-tmp =(1/(a*a*q1*q1+q2*q2+2*q1xq2*a+1)/(a*a*q1*q1+q3*q3+2*q1xq3*a+1)/
-      (q2*q2+q4*q4+2*q2xq4+1)/(q3*q3+q4*q4+2*q3xq4+1)*
-         (1/(a**2*q1**2+q2**2+q3**2+q4**2+2*q1xq2*a+
-             2*q1xq3*a+2*q1xq4*a+2*q2xq3+2*q2xq4+2*q3xq4+1)
-         ).diff(a)+
-         (1/(a*a*q1*q1+q2*q2+2*q1xq2*a+1)).diff(a)*
-             (1/(a**2*b**2*q1**2+b**2*q2**2+q3**2+q4**2+2*q1xq2*a*b*b+
-             2*q1xq3*a*b+2*q1xq4*a*b+2*q2xq3*b+2*q2xq4*b+2*q3xq4+1)/
-             (a*a*b*b*q1*q1+q3*q3+2*q1xq3*a*b+1)/(b*b*q2*q2+q4*q4+2*q2xq4*b+1)/
-             (q3*q3+q4*q4+2*q3xq4+1)
-              ).diff(b)+
-         (1/(a*a*q1*q1+q3*q3+2*q1xq3*a+1)).diff(a)*
-             (1/(a**2*q1**2*b*b+q2**2+q3**2*b**2+q4**2+2*q1xq2*a*b+
-             2*q1xq3*a*b*b+2*q1xq4*a*b+2*q2xq3*b+2*q2xq4+2*q3xq4*b+1)/
-             (a*a*b*b*q1*q1+q2*q2+2*q1xq2*a*b+1)/(b*b*q3*q3+q4*q4+2*q3xq4*b+1)/
-             (q2*q2+q4*q4+2*q2xq4+1)
-              ).diff(b)
-
-      ).diff(a).diff(a) 
-
-k2term = rggrf.roperation.Factorized((1-a)*(1-a)/(q1**2+1)**5/(q2**2+1)/(q3**2+1)/(q4**2+1),tmp) 
+#tmp =(1/(a*a*q1*q1+q2*q2+2*q1xq2*a+1)/(a*a*q1*q1+q3*q3+2*q1xq3*a+1)/
+#      (q2*q2+q4*q4+2*q2xq4+1)/(q3*q3+q4*q4+2*q3xq4+1)*
+#         (1/(a**2*q1**2+q2**2+q3**2+q4**2+2*q1xq2*a+
+#             2*q1xq3*a+2*q1xq4*a+2*q2xq3+2*q2xq4+2*q3xq4+1)
+#         ).diff(a)+
+#         (1/(a*a*q1*q1+q2*q2+2*q1xq2*a+1)).diff(a)*
+#             (1/(a**2*b**2*q1**2+b**2*q2**2+q3**2+q4**2+2*q1xq2*a*b*b+
+#             2*q1xq3*a*b+2*q1xq4*a*b+2*q2xq3*b+2*q2xq4*b+2*q3xq4+1)/
+#             (a*a*b*b*q1*q1+q3*q3+2*q1xq3*a*b+1)/(b*b*q2*q2+q4*q4+2*q2xq4*b+1)/
+#             (q3*q3+q4*q4+2*q3xq4+1)
+#              ).diff(b)+
+#         (1/(a*a*q1*q1+q3*q3+2*q1xq3*a+1)).diff(a)*
+#             (1/(a**2*q1**2*b*b+q2**2+q3**2*b**2+q4**2+2*q1xq2*a*b+
+#             2*q1xq3*a*b*b+2*q1xq4*a*b+2*q2xq3*b+2*q2xq4+2*q3xq4*b+1)/
+#             (a*a*b*b*q1*q1+q2*q2+2*q1xq2*a*b+1)/(b*b*q3*q3+q4*q4+2*q3xq4*b+1)/
+#             (q2*q2+q4*q4+2*q2xq4+1)
+#              ).diff(b)
+#
+#      ).diff(a).diff(a) 
+#
+#k2term = rggrf.roperation.Factorized((1-a)*(1-a)/(q1**2+1)**5/(q2**2+1)/(q3**2+1)/(q4**2+1),tmp) 
 ###k2term = rggrf.roperation.Factorized((1-a)*(1-a)/(q1**2+1)**6/(q2**2+1)**2/(q3**2+1)**2/(q4**2+1)**2,tmp)
 
 #tmp =(1/(a*a*q1*q1+q2*q2+2*q1xq2*a+1)/(a*a*q1*q1+q3*q3+2*q1xq3*a+1)/
@@ -76,11 +76,61 @@ k2term = rggrf.roperation.Factorized((1-a)*(1-a)/(q1**2+1)**5/(q2**2+1)/(q3**2+1
 
 #k2term = rggrf.roperation.Factorized((1-a)*(1-a)/(q1**2+1)**5/(q2**2+1)/(q3**2+1)/(q4**2+1),tmp)
 
+#tmp = 1/(
+#         (q1*q1+1)**3*(q2**2+1)**3*(q3*q3+1)*(q1**2+q2**2/2+1+q1xq2)*(q3**2+q2**2/2+1-q2xq3)**2
+#         ) * (
+#              (1/(q3**2+1)+1/((q3**2+q2**2/2+1-q2xq3)**2))*
+#              ( 
+#               ((-q2**2/2-q1xq2)*(3+3*q1**2+q2**2+2*q1xq2))/(2*(q1**2+q2**2/2+1+q1xq2)) +
+#               (3*(q2**4/4+q1**2*q2**2/4+q2**2*q1xq2+q1**2*q2**2*q1xq2-q2**2*q1xq2**2/2-
+#                 q1xq2**3) )/(2*(q1**2+1)*(q2**2+1)) 
+#               ) + 1/(q1**2+1)*(
+#                                q2**2/4+q1**2*q2**4/4+q2**2*q1xq2+q1**2*q2**2*q1xq2-q2**2*q1xq2**2/2 -q1xq2**3
+#                                )*(
+#                                   1/((q3**2+1)**2)+3/(2*(q2**2/2+q3**2+1-q1xq2)**2) 
+#                                   + 3/(2*(q3**2+1)*(q2**2/2+q3**2+1-q2xq3)) 
+#                                   ) 
+#              )
+
+tmp = 1/(
+         (q1*q1+1)**3*(q2**2+1)**3*(q3*q3+1)*(q1**2+q2**2/2+1+q1xq2)*(q3**2+q2**2/2+1-q2xq3)**2
+         ) * (
+              (1/(q3**2+1)+1/((q3**2+q2**2/2+1-q2xq3)**2))*
+              ( 
+               ((-q2**2/2-q1xq2)*(3+3*q1**2+q2**2+2*q1xq2))/(2*(q1**2+q2**2/2+1+q1xq2)) +
+               (3*(q2**4/4+q1**2*q2**2/4+q2**2*q1xq2+q1**2*q2**2*q1xq2-q2**2*q1xq2**2/2-
+                 q1xq2**3) )/(2*(q1**2+1)*(q2**2+1)) 
+               )  
+              )
+         
+tmp = 1/(
+         (q1*q1+1)**3*(q2**2+1)**3*(q3*q3+1)*(q1**2+q2**2/2+1+q1xq2)*(q3**2+q2**2/2+1-q2xq3)**2
+         ) * (
+               1/(q1**2+1)*(
+                                q2**2/4+q1**2*q2**4/4+q2**2*q1xq2+q1**2*q2**2*q1xq2-q2**2*q1xq2**2/2 -q1xq2**3
+                                )*(
+                                   1/((q3**2+1)**2)+3/(2*(q2**2/2+q3**2+1-q1xq2)**2) 
+                                   + 3/(2*(q3**2+1)*(q2**2/2+q3**2+1-q2xq3)) 
+                                   ) 
+             )
+         
+tmp = (
+       (-q1*q1/2-q1xq2)*(3+3*q2**2+q1**2+2*q1xq2)
+       )/(
+          2*(q1**2+1)**3*(q2**2+1)**3*(q3**2+1)*
+          (q2**2+q1**2/2+1+q1xq2)**2*(q3**2+q1**2/2+1-q1xq3)**2
+          )*(
+             1/(q3**2+1)+1/(q3**2+q1**2/2+1-q1xq3)
+             )
+tmp = 1/((q1**2+q2**2+1)**3*(q2**2+1)**4*(q3**2+1)**4)
+            
+k2term = rggrf.roperation.Factorized(1,tmp)
+
 s_prep =   ExpandScalarProdsAndPrepareFactorized(k2term)
 
 #print "---------dm_%s_p%s --------- " %(idxL,idxK2)
 sys.stdout.flush()
-prepared_eqs.append(rggrf.integration.PrepareFactorizedNoSimplifyStrVars(s_prep, SPACE_DIM, ignore_unknown=True))
+prepared_eqs.append(rggrf.integration.PrepareFactorizedStrVars(s_prep, SPACE_DIM, ignore_unknown=True, simplify = False))
    
 prog_names = rggrf.integration.GenerateMCCodeForGraphStrVars(base_name, prepared_eqs,SPACE_DIM, n_epsilon_series, NPOINTS, NTHREADS) 
 
