e411d06d3176f830932a020800b97fdba8cd30d7
Mikhail Kompaniets <mkompan@gmail.com>
2010-03-15 23:35:33 +0300

    delta and green

--- rg-graph-static/src/delta.py
+++ rg-graph-static/src/delta.py
@@ -6,26 +6,44 @@ import rggraph_static as rggrf
 
 
 from phi3 import *
+    
+if "-target" in sys.argv:
+    target = int(sys.argv[sys.argv.index('-target')+1])
+else:
+    target = phi3.target
 
+if "-debug" in sys.argv:
+    debug = True
+else:
+    debug = False
 
-#print phi3.name
+if "-graph" in sys.argv:
+    g_list = [sys.argv[sys.argv.index('-graph')+1],]
+else:
+    g_list = phi3.GraphList()
+
+    
 eps = sympy.var('eps')
 
-G = rggrf.Graph(phi3)
-G.Load()
-G.DefineNodes({})
-G.GenerateNickel()
-print G.sym_coeff
-G.LoadResults('eps')
-G.FindSubgraphs()
-D = rggrf.roperation.Delta(G)
-G.__dict__["delta_gamma"] = rggrf.utils.SimpleSeries(D.Calculate('eps'),eps,0,phi3.target-G.NLoops())
-#print G.__dict__["r1_dot_gamma"]
-#print G.__dict__["delta_gamma"]
+#print phi3.namec
+for n in range(1,target+1):
+    for file in g_list:
+         
+        G = rggrf.Graph(phi3)
+        G.Load(str_nickel=file)
+        G.DefineNodes({})
+        rggrf.utils.print_debug("---: %s %s" %(file,G.NLoops()), debug)
+        if G.NLoops()==n:
+            rggrf.utils.print_debug("calc: %s"%file, debug)
+            G.GenerateNickel()
+            G.LoadResults('eps')
+            G.FindSubgraphs()
+            D = rggrf.roperation.Delta(G)
+            G.__dict__["delta_gamma"] = rggrf.utils.SimpleSeries(D.Calculate('eps'),eps,0,phi3.target-G.NLoops())
+    #print G.__dict__["r1_dot_gamma"]
+    #print G.__dict__["delta_gamma"]
+    
+            G.__dict__["r1_gamma"] = rggrf.utils.SimpleSeries(-(G.r1_dot_gamma + G.delta_gamma)/G.NLoops()/eps*2, eps, 0, phi3.target-G.NLoops()-1)
+    
+            G.SaveResults(["r1_gamma","delta_gamma"])
 
-eps=sympy.var('eps')
-G.__dict__["r1_gamma"] = rggrf.utils.SimpleSeries(-(G.r1_dot_gamma + G.delta_gamma)/G.NLoops()/eps*2, eps, 0, phi3.target-G.NLoops()-1)
-
-G.SaveResults()
-
-print G.r1_gamma
--- rg-graph-static/src/green.py
+++ rg-graph-static/src/green.py
@@ -10,6 +10,12 @@ if "-target" in sys.argv:
     target = int(sys.argv[sys.argv.index('-target')+1])
 else:
     target = phi3.target
+    
+    
+if "-debug" in sys.argv:
+    debug = True
+else:
+    debug = False
 
 g_list = phi3.GraphList()
     
@@ -18,18 +24,23 @@ g = sympy.var('g')
 
 greens = dict()
 #print phi3.name
+max_nloop = 0
 for file in g_list:
-        print "--- %s"%file,
+        rggrf.utils.print_debug("---: %s"%file, debug)
         G = rggrf.Graph(phi3)
         G.Load(str_nickel=file)
         G.DefineNodes({})
         G.GenerateNickel()
         G.LoadResults('eps')
         if len(G.green)>0 and G.green in greens:
-            greens[G.green] = greens[G.green] + G.sym_coeff * G.r1_gamma * g**G.Nloops()
+            greens[G.green] = greens[G.green] + G.sym_coeff * G.r1_gamma * g**G.NLoops()
         else:
-            greens[G.green] = G.sym_coeff * G.r1_gamma * g**G.Nloops()  
+            greens[G.green] = G.sym_coeff * G.r1_gamma * g**G.NLoops()
+        if max_nloop < G.NLoops():
+            max_nloop = G.NLoops()  
             
             
-print greens
+for green in greens:
+    cur_series=rggrf.utils.SimpleSeries(greens[green], g, 0, max_nloop)
+    print "%s : %s"%(green,cur_series) 
 
