c961cb64e700e2953d39e95d5766fa81b76ef749
Mikhail Kompaniets <mkompan@gmail.com>
2010-04-08 14:57:04 +0400

    recalculate diagrams with specific number of loops

--- rg-graph-static/src/recalculate.py
+++ rg-graph-static/src/recalculate.py
@@ -89,92 +89,100 @@ if "-delta" in sys.argv:
     calc_delta = eval(sys.argv[sys.argv.index('-delta')+1])
 else:
     calc_delta = None
+    
+if "-nloops" in sys.argv:
+    nloops = eval(sys.argv[sys.argv.index('-nloops')+1])
+    if isinstance(nloops,int):
+        nloops = [nloops,]
+else:
+    nloops = range(1,model.target+1)    
 
 for nickel in g_list: 
     G = model.LoadGraph(nickel)
-    G.GenerateNickel()
-    G.LoadResults('eps')
-    if G.method in ["MCT_SVd", "MCO_SVd", "MCOR_SVd"]:
-        if calc_delta <> None:
-            G.delta = calc_delta
-        elif 'delta' in G.__dict__:
-            calc_delta = G.delta
-        rggrf.utils.print_debug("delta = %s"%calc_delta,debug)
- 
-    if not G.CheckAccuracy(absolute, relative):
-          
-        if npoints == 0:
-            if "npoints_r" in G.__dict__:
-                G.npoints = int(G.npoints_r)
+    if G.NLoops() in nloops:
+        G.GenerateNickel()
+        G.LoadResults('eps')
+        if G.method in ["MCT_SVd", "MCO_SVd", "MCOR_SVd"]:
+            if calc_delta <> None:
+                G.delta = calc_delta
+            elif 'delta' in G.__dict__:
+                calc_delta = G.delta
+            rggrf.utils.print_debug("delta = %s"%calc_delta,debug)
+     
+        if not G.CheckAccuracy(absolute, relative):
+              
+            if npoints == 0:
+                if "npoints_r" in G.__dict__:
+                    G.npoints = int(G.npoints_r)
+                else:
+                    G.npoints = 10000 
+            elif npoints ==None:
+                if "npoints_r" in G.__dict__:
+                    G.npoints = int(G.npoints_r)*10
+                else:
+                    G.npoints = 10000
             else:
-                G.npoints = 10000 
-        elif npoints ==None:
-            if "npoints_r" in G.__dict__:
-                G.npoints = int(G.npoints_r)*10
-            else:
-                G.npoints = 10000
-        else:
-            G.npoints = npoints
-        
-        G.WorkDir()
-    
-#        process = subprocess.Popen(["ls",], shell=False, 
-#                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#        rggrf.utils.print_debug("start process")
-#        exit_code = process.wait()
-#        rggrf.utils.print_debug("exit code: %s"%exit_code)
-
-#        (std_out,std_err) = process.communicate()
-           
-#        rggrf.utils.print_debug("comunicate()")
-
-        dirlist=os.listdir("./")
- 
-#        if exit_code <> 0 :
-#            print std_err
-#            raise Exception, "\"ls %s*\" returned error code %s"%(G.method,exit_code)
-    
-        t_exec_dict = FindExecutables(dirlist, G.method)
-#        if len(t_exec_dict)>1:
-#            raise ValueError, "found more then one set of executables: %s " %t_exec_dict.keys()
-        G.r1_dot_gamma = 0
-        err = 0
-        cnt=0
-        bar = progressbar.ProgressBar(maxval=len(t_exec_dict.keys()), 
-                                      term_width=70, 
-                                      widgets=["(%s/%s) %s  "%(g_list.index(nickel)+1, len(g_list),G.nickel), 
-                                               progressbar.Percentage(), " ", 
-                                               progressbar.Bar(), 
-                                               progressbar.ETA()]).start()
-        for idx in t_exec_dict:    
-            prog_names = t_exec_dict[idx]
-    
+                G.npoints = npoints
             
-            t_res = rggrf.integration.CalculateEpsilonSeries(prog_names, points=G.npoints,
-                                                              threads=nthreads, debug=debug, 
-                                                              calc_delta=calc_delta)
-            try:
-                (t_r1_dot_gamma, t_r1_dot_gamma_err) = ResultWithSd(t_res, G.reduced_nloops, 
-                                                                    G.model.target - G.NLoops())
-            except:
-                print "\nError: %s\n%s\n"%(prog_names[0],t_res)
-                err = sympy.core.numbers.Number(1000000.)
-                break
-            else:
-            #print t_r1_dot_gamma, t_r1_dot_gamma_err
-                G.r1_dot_gamma = G.r1_dot_gamma + t_r1_dot_gamma
+            G.WorkDir()
         
-                err = err + t_r1_dot_gamma_err**2
-            cnt=cnt+1
-            bar.update(cnt)
+    #        process = subprocess.Popen(["ls",], shell=False, 
+    #                                   stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    #        rggrf.utils.print_debug("start process")
+    #        exit_code = process.wait()
+    #        rggrf.utils.print_debug("exit code: %s"%exit_code)
+    
+    #        (std_out,std_err) = process.communicate()
+               
+    #        rggrf.utils.print_debug("comunicate()")
+    
+            dirlist=os.listdir("./")
+     
+    #        if exit_code <> 0 :
+    #            print std_err
+    #            raise Exception, "\"ls %s*\" returned error code %s"%(G.method,exit_code)
         
-        err=err**0.5
-        G.r1_dot_gamma_err = rggrf.utils.RelativeError(G.r1_dot_gamma, err, sympy.var('eps'))
+            t_exec_dict = FindExecutables(dirlist, G.method)
+    #        if len(t_exec_dict)>1:
+    #            raise ValueError, "found more then one set of executables: %s " %t_exec_dict.keys()
+            G.r1_dot_gamma = 0
+            err = 0
+            cnt=0
+            bar = progressbar.ProgressBar(maxval=len(t_exec_dict.keys()), 
+                                          term_width=70, 
+                                          widgets=["(%s/%s) %s  "%(g_list.index(nickel)+1, len(g_list),G.nickel), 
+                                                   progressbar.Percentage(), " ", 
+                                                   progressbar.Bar(), 
+                                                   progressbar.ETA()]).start()
+            for idx in t_exec_dict:    
+                prog_names = t_exec_dict[idx]
         
-        rggrf.utils.print_debug( G.r1_dot_gamma, debug)
-        
-        G.SaveResults(['r1_dot_gamma','r1_dot_gamma_err','npoints','method','delta'])
-        print G.r1_dot_gamma, G.r1_dot_gamma_err, "\n"
+                
+                t_res = rggrf.integration.CalculateEpsilonSeries(prog_names, points=G.npoints,
+                                                                  threads=nthreads, debug=debug, 
+                                                                  calc_delta=calc_delta)
+                try:
+                    (t_r1_dot_gamma, t_r1_dot_gamma_err) = ResultWithSd(t_res, G.reduced_nloops, 
+                                                                        G.model.target - G.NLoops())
+                except:
+                    print "\nError: %s\n%s\n"%(prog_names[0],t_res)
+                    err = sympy.core.numbers.Number(1000000.)
+                    break
+                else:
+                #print t_r1_dot_gamma, t_r1_dot_gamma_err
+                    G.r1_dot_gamma = G.r1_dot_gamma + t_r1_dot_gamma
+            
+                    err = err + t_r1_dot_gamma_err**2
+                cnt=cnt+1
+                bar.update(cnt)
+            
+            err=err**0.5
+            G.r1_dot_gamma_err = rggrf.utils.RelativeError(G.r1_dot_gamma, err, sympy.var('eps'))
+            
+            rggrf.utils.print_debug( G.r1_dot_gamma, debug)
+            
+            G.SaveResults(['r1_dot_gamma','r1_dot_gamma_err','npoints','method','delta'])
+            print G.r1_dot_gamma, G.r1_dot_gamma_err, "\n"
         
 
 #print "симметрийный коэффициент: %s" %(G.sym_coeff)
