99f6069ad4aedf78878f4f4418ac50f1e3987e89
Mikhail Kompaniets <mkompan@gmail.com>
2010-06-17 10:01:17 +0400

    set custom moment layout

---
+++ rg-graph-static/src/set_moment.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding:utf8
+
+model=None
+import sys
+import rggraph_static as rggrf
+
+def usage(progname):
+    return "%s -model phi3R [-overwrite] [-graph str_nickel] [-debug]"
+
+if "-model" in sys.argv:
+    model_module = sys.argv[sys.argv.index('-model')+1]
+    try:
+        exec('from %s import *'%model_module)
+    except:
+        print "Error while importing model!"
+        sys.exit(1)
+else:
+    print "Usage : %s " %usage(sys.argv[0])
+    sys.exit(1)
+
+
+if "-graph" in sys.argv:
+    g_list = [sys.argv[sys.argv.index('-graph')+1],]
+else:
+    g_list = model.GraphList()
+    
+if "-debug" in sys.argv:
+    debug = True
+else:
+    debug = False
+
+if "-overwrite" in sys.argv:
+    overwrite = True
+else:
+    overwrite = False
+
+
+for file in g_list:
+    rggrf.utils.print_debug(file, debug)
+    G = model.LoadGraph(file)
+    G.GenerateNickel()
+    G.FindSubgraphs()
+    G.WorkDir()
+
+    moments = rggrf.moments.Generate(G)
+    print moments
+    moments = {1: '+p', 2: '-p', 3: '-q1', 4: 'p+q1', 5: '-p-q1', 6: '+q2', 7: 'p+q1-q2'}
+    print moments
+    G._UpdateMoments(moments)
+
+    G.Save(overwrite=overwrite)
+    G.SaveAsPNG("graph.png")
+
+#print rggrf.nickel.Nickel(nickel=G.nickel.nickel).nickel
+#print rggrf.nickel.Nickel(nickel=G.nickel.nickel).edges
+#print G.sym_coeff
+
+    
