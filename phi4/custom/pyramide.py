449bfd2abac975e6b7faff01bcaa38fd4b305f10
Mikhail Kompaniets <mkompan@gmail.com>
2011-01-07 11:58:25 +0300

    lots of fixes in pyramide (not all)

--- phi4/custom/pyramide.py
+++ phi4/custom/pyramide.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python
 
 import sympy
-sympy.var('e k0 k1 k2 a1 a2 a3')
 
-expr=k0**(3.-e)*k1**(3.-e)*k2**(3.-e)*(1.-a1**2)**(1.-e/2.)*(1.-a2**2)**(1.-e/2.)*(1.-a3**2)**(0.5-e/2.)*((1+k0**2)**(2)*(1+k1**2)*(1+k2**2)*(1+k0**2+k1**2-2*k0*k1*a1)*(1+k1**2+k2**2-2*k1*k2*a3)*(1+k0**2+k2**2-2*k0*k2*a2))**(-1)
+def S(d):
+   return d*sympy.pi**(d/2.)/sympy.gamma(d/2.+1.)
+
+def S_(d):
+   return S(d)/((2*sympy.pi)**d)
+
+print float(S(2)), float(S(3)), float(S(4))
+print float(S_(2)), float(S_(3)), float(S_(4))
+
+sympy.var('e k0 k1 k2 a1 a2 a3 k0_ k1_ k2_')
+
+c3=a1*a2+(1-a1**2)**0.5*(1-a2**2)**0.5*a3
+
+expr=k0**(3.-e)*k1**(3.-e)*k2**(3.-e)*(1.-a1**2)**(1.-e/2.)*(1.-a2**2)**(1.-e/2.)*(1.-a3**2)**(0.5-e/2.)*((1+k0**2)**(2)*(1+k1**2)*(1+k2**2)*(1+k0**2+k1**2-2*k0*k1*a1)*(1+k1**2+k2**2-2*k1*k2*c3)*(1+k0**2+k2**2-2*k0*k2*a2))**(-1)
+expr=(expr.subs(k0,k0_/(1-k0_))*(1-k0_)**(-2)).subs(k0_,k0)
+expr=(expr.subs(k1,k1_/(1-k1_))*(1-k1_)**(-2)).subs(k1_,k1)
+expr=(expr.subs(k2,k2_/(1-k2_))*(1-k2_)**(-2)).subs(k2_,k2)
+d=4-e
+#expr=expr*S(d-2)/S(d)*S(d-1)/S(d)
+
+sympy.pretty_print(expr)
 
 
 for i in range(2):
