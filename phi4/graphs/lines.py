918629def87442d242b7d7b3c9cbb0797685d759
Mikhail Kompaniets <mkompan@gmail.com>
2011-07-18 11:45:29 +0400

    default values for type,momenta,modifiers in Line

--- phi4/graphs/lines.py
+++ phi4/graphs/lines.py
@@ -3,12 +3,15 @@
 
 class Line:
     """ Class represents lines
-        type=, momenta=, start=, end=, dots=
+        type=, momenta=, start=, end=, modifiers=
     """
-    def __init__(self,**kwargs):
-        required_fields = set(['type','momenta','start','end','modifiers'])
+    def __init__(self,type=None, momenta=None, modifiers=None, **kwargs):
+        required_fields = set(['start','end'])
         if not required_fields.issubset(set(kwargs.keys())):
              raise ValueError, "Lines required fields are %s"%required_fields
+        self.type=type
+        self.momenta=momenta
+        self.modifiers=modifiers
         for field in kwargs:
             self.__dict__[field] = kwargs[field]
 
