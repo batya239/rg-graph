badaa2298b6585a9bc25c6f6e64e44965fa0bfb8
Mikhail Kompaniets <mkompan@gmail.com>
2011-09-19 04:15:32 +0400

    chains of sauseges are equal to zero (ommitig)

--- phi4/graphs/parsegrc.py
+++ phi4/graphs/parsegrc.py
@@ -5,10 +5,16 @@ import utils
 from dummy_model import _phi3,_phi4
 import sys
 from graphs import Graph
+import subgraphs
 
 phi4=_phi4('dummy')
 
 for nickel in utils.LoadFromGRC(sys.argv[1], phi4):
     g=Graph(nickel)
     g.GenerateNickel()
-    print "%s %s"%(nickel, g.sym_coef())
+    phi4.SetTypes(g)
+    g.FindSubgraphs(phi4)
+    gas=g.asSubgraph()
+    to_remove=subgraphs.DetectSauseges([gas]+g._subgraphs)
+    if not gas in to_remove:
+        print "%s %s"%(nickel, g.sym_coef())
