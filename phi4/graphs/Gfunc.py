1e76b24c0af2d14ad66b956565df4fc6a3a26f05
Mikhail Kompaniets <mkompan@gmail.com>
2012-11-29 16:44:14 +0400

    G1,G2

--- phi4/graphs/Gfunc.py
+++ phi4/graphs/Gfunc.py
@@ -9,10 +9,23 @@ def G_(l1,l2):
    e=sympy.var('e')
    return gamma(l1+l2-lambd-1)*gamma(1+lambd-l1)*gamma(1+lambd-l2)/(gamma(l1)*gamma(l2)*gamma(2+2*lambd-l1-l2))
 
+def G1_(l1,l2):
+   return (-G_(l1,l2-1)+G_(l1-1,l2)+G_(l1,l2))/2
+
+def G2_(l1,l2):
+   return (-G_(l1,l2-1)-G_(l1-1,l2)+G_(l1,l2))/2
+
 
 def G(l1,l2):
    e=sympy.var('e')
    return G_(l1,l2)/e/G_(1,1)
+def G1(l1,l2):
+   e=sympy.var('e')
+   return G1_(l1,l2)/e/G_(1,1)
+
+def G2(l1,l2):
+   e=sympy.var('e')
+   return G2_(l1,l2)/e/G_(1,1)
 
 def S(d):
    return d*sympy.pi**(d/2)/gamma(d/2+1)
