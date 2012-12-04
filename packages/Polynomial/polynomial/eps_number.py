ea3a43048d559de81e24112573a7f0b7b9a44b0f
batya239 <devnull@localhost>
2012-12-04 13:05:13 +0400

    eps polynomials calculations

--- packages/Polynomial/polynomial/eps_number.py
+++ packages/Polynomial/polynomial/eps_number.py
@@ -94,30 +94,36 @@ def _reduceIntegers(epsNumberList):
 def _normalize(epsNumberList):
     resultList = []
     resultCoefficient = 1
+    resultShift = 0
     for _epsNumber in epsNumberList:
-        resultCoefficient *= _epsNumber.a
-        resultList.append(epsNumber((1, _epsNumber.b / _epsNumber.a)))
+        if _epsNumber.a == 0:
+            resultShift += 1
+            resultCoefficient *= _epsNumber.b
+        else:
+            resultCoefficient *= _epsNumber.a
+            resultList.append(epsNumber((1, float(_epsNumber.b) / float(_epsNumber.a))))
 
-    return resultCoefficient, resultList
+    return resultCoefficient, (resultList if resultList[0] <> 1 else resultList[1:]), resultShift
 
 
 def getCoefficients(epsNumberList):
-    c, reduced = _normalize(_reduceIntegers(epsNumberList))
+    c, reduced, shift = _normalize(_reduceIntegers(epsNumberList))
+    shifter = [0 for i in xrange(0, shift)]
     ord = len(reduced)
     if not ord:
-        return []
+        return shifter + []
     elif ord == 1 and reduced[0].isInt():
-        return c
+        return shifter + [c]
     else:
         result = []
         for i in xrange(0, ord + 1):
             if i == 0:
                 result.append(c)
             else:
-                result.append(c * reduce(lambda x, y: x * y, xPerm(map(lambda n: n.b, reduced), i)))
-        return result
+                result.append(c * reduce(lambda x, y: x + y, xPerm(map(lambda n: n.b, reduced), i)))
+        return shifter + result
 
 
 def xPerm(A, n):
-    for z in itertools.permutations(A, n):
+    for z in itertools.combinations(A, n):
         yield reduce(lambda x, y: x * y, z)
