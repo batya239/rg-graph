7eab40d8d682efd4f61c25f06de7c7e36dfb4151
Sergey Novikov <dr.snov@gmail.com>
2012-12-16 23:08:53 +0100

    Clean up.

--- phi4/graphs/comb_test.py
+++ phi4/graphs/comb_test.py
@@ -1,1 +1,31 @@
-../../rg-graph-static/src/rggraph_static/comb_test.py
+#!/usr/bin/python
+# -*- coding: utf8
+
+import comb
+import unittest
+
+
+class TestComb(unittest.TestCase):
+    def testCombinations(self):
+        xcomb = comb.xCombinations
+        self.assertEqual(list(xcomb([1, 2, 3], 4)), [])
+        self.assertEqual(list(xcomb(('1', 2), 1)), [('1',), (2,)])
+        self.assertEqual(list(xcomb([1, 2, 3], 2)),
+                         [[1, 2], [1, 3], [2, 1], [2, 3], [3, 1], [3, 2]])
+
+    def testPermutations(self):
+        xperm = comb.xPermutations
+        self.assertEqual(list(xperm([])), [[]])
+        self.assertEqual(list(xperm(('1',))), [('1',)])
+        self.assertEqual(list(xperm('ab')), ['ab', 'ba'])
+        perm3 = [[1, 2, 3], [1, 3, 2],
+                 [2, 1, 3], [2, 3, 1],
+                 [3, 1, 2], [3, 2, 1]]
+        self.assertEqual(list(xperm([1, 2, 3])), perm3)
+        for p in xperm([1, 2, 3]):
+            self.assertEqual(p, perm3.pop(0))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
