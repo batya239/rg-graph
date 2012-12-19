73ff476948f0cfd7d7b37a44c1ef027ab0904ab5
Mikhail Kompaniets <mkompan@gmail.com>
2012-12-20 03:33:27 +0400

    script for dynamics

--- phi4/graphs/dyn_1.py
+++ phi4/graphs/dyn_1.py
@@ -11,8 +11,12 @@ import nodes
 import conserv
 from methods import sd_tools
 
+from methods.feynman_tools import  conv_sub
+
 import subgraphs
 from dummy_model import _phi3_dyn
+
+import polynomial
 
 model = _phi3_dyn("phi3_dyn_test")
 
@@ -58,7 +62,7 @@ class DynGraph(graphs.Graph):
 
 gs = graph_state.GraphState.fromStr("e12-23-3-e-:0AaAaa-aAaa-aA-0a-:")
 #gs = graph_state.GraphState.fromStr("e12-e3-45-45-5--:0AaAaA-0aaA-aAaa-aaaa-aA--:")
-gs = graph_state.GraphState.fromStr("e12-e3-33--:0AaAaA-0aaa-aAaa--:")
+#gs = graph_state.GraphState.fromStr("e12-e3-33--:0AaAaA-0aaa-aAaa--:")
 print str(gs)
 
 dG = DynGraph(gs)
@@ -143,38 +147,72 @@ def EffectiveSubgraphDim(subgraph, tCuts, model):
 
 
 def genStatic_D_C(graph):
-    graph._eqsubgraphs = list()
     internalEdges = graph._internal_edges_dict()
     if len(graph.ExternalLines()) == 2:
         internalEdges[1000000] = [i.idx() for i in graph.ExternalNodes()] #Additional edge: suitable way to find F
         conservations = conserv.Conservations(internalEdges)
-#        equations = sd_tools.find_eq(conservations)
-#        conservations = sd_tools.apply_eq(conservations, equations)
-        equations=dict()
+        #        equations = sd_tools.find_eq(conservations)
+        #        conservations = sd_tools.apply_eq(conservations, equations)
+        equations = dict()
         graph._qi, graph._qi2l = sd_tools.qi_lambda(conservations, equations)
+        graph._eqsubgraphs = sd_tools.apply_eq_onsub(graph._qi2l, conv_sub(graph._subgraphs))
         graph_ = graph.Clone()
         graph_._cons = conservations
         C = sd_tools.gendet(graph_, N=graph.NLoops() + 1)
         internalEdges = graph._internal_edges_dict()
         conservation = conserv.Conservations(internalEdges)
-#        conservations = sd_tools.apply_eq(conservations, equations)
+        #        conservations = sd_tools.apply_eq(conservations, equations)
         graph._cons = conservations
+
 
     else:
-        C=None
+        C = None
         conservations = conserv.Conservations(internalEdges)
-#        equations = sd_tools.find_eq(conservations)
-        equations=dict()
-#        conservations = sd_tools.apply_eq(conservations, equations)
+        #        equations = sd_tools.find_eq(conservations)
+        equations = dict()
+        #        conservations = sd_tools.apply_eq(conservations, equations)
         graph._qi, graph._qi2l = sd_tools.qi_lambda(conservations, equations)
+        graph._eqsubgraphs = sd_tools.apply_eq_onsub(graph._qi2l, conv_sub(graph._subgraphs))
         graph._cons = conservations
 
-    D=sd_tools.gendet(graph, N=graph.NLoops())
-    return (D,C)
+    D = sd_tools.gendet(graph, N=graph.NLoops())
+    return (D, C)
 
-D,C= genStatic_D_C(dG)
+
+
+def relabel(lst, rules):
+    res = list()
+    for term in lst:
+        term_ = list()
+        for var in term:
+            if rules.has_key(var):
+                term_.append(rules[var])
+            else:
+                term_.append(var)
+        res.append(term_)
+    return res
+
+def genStaticT(graph):
+    res=list()
+    for var in graph._qi2l:
+        term=[var,]
+        for i in range(len(graph._subgraphs)):
+            sub=map(lambda x: x.idx(), graph._subgraphs[i].lines)
+            if var in sub:
+                term.append('a%s'%i)
+        res.append(term)
+    return res
+
+rules = {1000: 'a0', 1001: 'a1', 1002: 'a2', 1003: 'a3', 1004: 'a4', 1005: 'a5', 1006: 'a6', 1007: 'a7'}
+
+D, C = map(lambda x: relabel(x, rules), genStatic_D_C(dG))
+E = [(x,) for x in dG._qi2l]
+T= genStaticT(dG)
 print D
 print C
+print E
+print T
+
 print dG._qi
 print dG._qi2l
 
@@ -182,31 +220,38 @@ def dSubstitutions(graph, tCuts):
     """
     TODO: generalization required
     """
+
     def isPhiPhi(line):
-        return line.type==('a','a')
+        return line.type == ('a', 'a')
 
     def isPhi1Phi(line):
-        return (line.type==('A','a') or line.type==('a','A'))
+        return (line.type == ('A', 'a') or line.type == ('a', 'A'))
 
-    tCutShift=100
+    tCutShift = 100
 
-    res=dict()
+    res = dict()
     for var in graph._qi2l:
-        if len(graph._qi2l[var])==1:
-            line=graph._Line(graph._qi2l[var][0])
+        if len(graph._qi2l[var]) == 1:
+            line = graph._Line(graph._qi2l[var][0])
         else:
-            raise NotImplementedError, "all lines must have different Feynman parameters: %s"%graph._qi2l
-        res[var]=list()
+            raise NotImplementedError, "all lines must have different Feynman parameters: %s" % graph._qi2l
+        res[var] = list()
 
         if isPhiPhi(line):
-            res[var].append(var)
+            res[var].append((var,))
         for idx in range(len(tCuts)):
-            tCut=tCuts[idx]
+            tCut = tCuts[idx]
+
             if var in tCut:
-                res[var].append(idx+tCutShift)
+                tsubst = [idx + tCutShift, ]
+                for i in range(len(graph._subgraphs)):
+                    sub = graph._subgraphs[i]
+                    sub_ = set(map(lambda x: x.idx(), sub.lines))
+                    if len(set(tCut) & sub_) <> 0 and var not in sub_:
+                        tsubst.append('a%s' % i)
+                res[var].append(tsubst)
     return res
 
-D_ = polynomial.poly([(1, x) for x in D])
 
 for tVersion in TVersions(dG):
     tCuts = TCuts(dG, tVersion)
@@ -214,10 +259,12 @@ for tVersion in TVersions(dG):
     print tVersion, tCuts
     for sub in dG._subgraphs:
         print sub.Dim(model), EffectiveSubgraphDim(sub, tCuts, model), sub
-    D__=D_
-    substitutions=dSubstitutions(dG,tCuts)
+    Components=(D,C,E,T)
+    Components_= map(lambda y:polynomial.poly([(1, x) for x in y]), Components)
+    substitutions = dSubstitutions(dG, tCuts)
+    print substitutions
     for var in substitutions:
         subs = substitutions[var]
-        subs_ = polynomial.poly([(1,x) for x in subs])
-        D__=D__.changeVarToPolynomial(var,subs_)
-
+        subs_ = polynomial.poly([(1, x) for x in subs])
+        Components_=map(lambda x:x.changeVarToPolynomial(var, subs_), Components_)
+    print "D = %s\nC = %s\nE = %s\nT = %s\n"%tuple(Components_)
