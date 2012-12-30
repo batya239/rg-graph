0b477518fd78fd9db5dad7835112633faf66013e
Batya239 <devnull@localhost>
2012-12-30 23:13:16 +0400

    bogner code generation

---
+++ phi4/graphs/bw_code_generator_sample.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf8
+import bw_code_generator as bw
+
+bw.generate_code("e12-23-3-e-", "STRATEGY_A", 4, "/Users/dima/Downloads/sector_decomposition-1.1.0/checks/tbubble.cc")
