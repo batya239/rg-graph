c53d4b40511b9a6924c49dc88886fdf47e2135a0
Mikhail Kompaniets <mkompan@gmail.com>
2013-02-05 14:01:52 +0400

    fix for dynamics

--- phi4/graphs/dummy_model.py
+++ phi4/graphs/dummy_model.py
@@ -94,6 +94,21 @@ class _phi3_dyn(_generic_model):
         self.workdir='/home/mkompan/work/rg-graph/phi3_dyn/'
 
 
+class _phi4_dyn(_generic_model):
+    def __init__( self , name):
+        self.name = name
+        self.space_dim = 4
+        self.freq_dim = 2
+        self.space_dim_eff = self.space_dim + self.freq_dim
+
+        self.lines_dim = {('a','A'):-2,('A','a'):-2, ('a','a'):-4}
+        self.modifiers_dim = {'tau':-2,'p':-1}
+        self.lines_modifiers = {'default':['tau','p']}
+        self.nodes_modifiers = {None:[], 'default':['tau','p']}
+        self.nodes_dim = {1:0}
+        self.checktadpoles = False
+        self.target = 3
+        self.workdir = '/home/mkompan/work/rg-graph/phi4_dyn/'
 
 
 class _phi4(_generic_model):
