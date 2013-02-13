54ea50207c016b80d8bac0b38c562bc4f54daa7a
Mikhail Kompaniets <mkompan@gmail.com>
2013-02-13 12:03:50 +0400

    EpsNumber __neg__ test

--- packages/Polynomial/polynomial/test/eps_number_test.py
+++ packages/Polynomial/polynomial/test/eps_number_test.py
@@ -3,18 +3,23 @@
 import unittest
 from eps_number import EpsNumber, getCoefficients, epsNumber
 
+
 class EpsNumberTestCase(unittest.TestCase):
     def testIt(self):
-        sum =  EpsNumber(1, 2) + EpsNumber(3, 4)
+        sum = EpsNumber(1, 2) + EpsNumber(3, 4)
         self.assertEquals(sum.a, 4)
         self.assertEquals(sum.b, 6)
         sub = EpsNumber(1, 2) - EpsNumber(3, 4)
         self.assertEquals(sub.a, -2)
         self.assertEquals(sub.b, -2)
+        neg = - EpsNumber(1, 2)
+        self.assertEquals(neg.a, -1)
+        self.assertEquals(neg.b, -2)
 
     def testMultiply(self):
         self.assertEquals((3 * EpsNumber(1, 2)).a, 3)
         self.assertEquals((3 * EpsNumber(1, 2)).b, 6)
+
 
 class EpsNumberAuxFunctionsTestCase(unittest.TestCase):
     def testGetCoefficients(self):
