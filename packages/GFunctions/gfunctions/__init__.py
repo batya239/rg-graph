aa35af9d95138f1c4ce049bfb339bc8cba2ee286
Mikhail Kompaniets <mkompan@gmail.com>
2013-02-19 18:34:45 +0400

    refactoring bugs,

--- packages/GFunctions/gfunctions/__init__.py
+++ packages/GFunctions/gfunctions/__init__.py
@@ -3,6 +3,6 @@
 
 __author__ = 'dima'
 
-from packages.GFunctions.gfunctions.subgraph_processer import GGraphReducer
-from packages.GFunctions.gfunctions.momentum import graphFilter, oneIrreducible, connected, \
+from gfunctions.subgraph_processer import GGraphReducer
+from gfunctions.momentum import graphFilter, oneIrreducible, connected, \
     xPassExternalMomentum, xPickPassingExternalMomentum, passMomentOnGraph
