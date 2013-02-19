9750b75f90c5d1b6df20cf05e4721fa29c322e34
Dmitry Batkovich <batya239@gmail.com>
2013-02-19 17:22:37 +0400

    storing new values to storage

--- phi4/gfunctions/graph_storage.py
+++ phi4/gfunctions/graph_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf8
 from os import path
+import graph_state
 
 
 class GraphStorage(object):
@@ -14,6 +15,22 @@ class GraphStorage(object):
         return str(graphState) in self.underlying
 
 
+def put(graphState, value):
+    assert isinstance(graphState, graph_state.GraphState)
+    assert len(value) == 2
+    assert isinstance(value[0], str)
+    assert isinstance(value[1], tuple)
+    assert len(value[1]) == 2
+    if has(graphState):
+        return
+    graphStateAsString = str(graphState)
+    STORAGE.underlying[graphStateAsString] = value
+    storageFile = open(_getStoragePath(), "w")
+    storageFile.write("\n")
+    storageFile.write(str((graphStateAsString, value[0], value[1])))
+    storageFile.close()
+
+
 def get(graphState):
     return STORAGE.get(graphState)
 
@@ -22,8 +39,11 @@ def has(graphState):
     return STORAGE.has(graphState)
 
 
+def _getStoragePath():
+    return path.join(path.dirname(path.realpath(__file__)), "graph_storage.txt")
+
 def _initStorage():
-    for line in open(path.join(path.dirname(path.realpath(__file__)), "graph_storage.txt"), "r"):
+    for line in open(_getStoragePath(), "r"):
         k, v1, v2 = eval(line)
         STORAGE.underlying[k] = (v1, v2)
 
