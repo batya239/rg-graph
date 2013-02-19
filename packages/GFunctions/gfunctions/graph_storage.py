4c1893209a53febdc7aafe785b9067d4fa667936
Dmitry Batkovich <batya239@gmail.com>
2013-02-19 18:56:33 +0400

    local storage

--- packages/GFunctions/gfunctions/graph_storage.py
+++ packages/GFunctions/gfunctions/graph_storage.py
@@ -25,7 +25,7 @@ def put(graphState, value):
         return
     graphStateAsString = str(graphState)
     STORAGE.underlying[graphStateAsString] = value
-    storageFile = open(_getStoragePath(), "a")
+    storageFile = open(_STORAGE_FILE_NAME, "a")
     storageFile.write("\n")
     storageFile.write(str((graphStateAsString, value[0], value[1])))
     storageFile.close()
@@ -39,14 +39,24 @@ def has(graphState):
     return STORAGE.has(graphState)
 
 
-def _getStoragePath():
-    return path.join(path.dirname(path.realpath(__file__)), "graph_storage.txt")
-
+_STORAGE_FILE_NAME = "graph_storage.txt"
 
 def _initStorage():
-    for line in open(_getStoragePath(), "r"):
-        k, v1, v2 = eval(line)
-        STORAGE.underlying[k] = (v1, v2)
+    if path.exists(_STORAGE_FILE_NAME):
+        for line in open(_STORAGE_FILE_NAME, "r"):
+            k, v1, v2 = eval(line)
+            STORAGE.underlying[k] = (v1, v2)
+    else:
+        localStorageFile = open(_STORAGE_FILE_NAME, "a")
+        for line in open(path.join(path.dirname(path.realpath(__file__)), _STORAGE_FILE_NAME), "r"):
+            if not len(line):
+                continue
+            k, v1, v2 = eval(line)
+            STORAGE.underlying[k] = (v1, v2)
+            localStorageFile.write(str((k, v1, v2)))
+            localStorageFile.write("\n")
+        localStorageFile.close()
+
 
 STORAGE = GraphStorage()
 _initStorage()
