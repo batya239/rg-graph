d141328b19c8ed01fddb3d2b20e76234f3c4117e
Mikhail Kompaniets <mkompan@gmail.com>
2013-03-15 19:53:45 +0400

    feynmanSDdot_mpi

---
+++ phi4/graphs/methods/feynmanSDdot_mpi.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding:utf8
+import sys
+import calculate
+import sympy
+from methods import sd_tools
+import hashlib
+
+from feynman_tools import normalize
+from methods.feynman_tools import strech_indexes
+from methods.poly_tools import poly_exp
+from methods.sd_tools import _cnomenkl, debug, xTreeElement, decompose_expr, diff_subtraction, save_sectors
+
+from methods.feynmanSDdot import save_sd
+
+method_name = "feynmanSDdot_mpi"
+sd_tools.method_name = method_name
+Prepare = sd_tools.Prepare
+save = sd_tools.save
+compile = sd_tools.compile
+sd_tools.code_ = sd_tools.core_pvmpi_code
+code_ = sd_tools.core_pvmpi_code
+
+#mpi
+#compile = sd_tools.compile_mpi
+#sd_tools.code_=sd_tools.core_pvmpi_code
+
+
+#sd_tools.debug = True
+
+sd_tools.MaxSDLevel = -1
+sd_tools.MaxABranches = -1
+#sd_tools.MaxABranches=0
+sd_tools._CheckBadDecomposition = True
+#sd_tools._CheckBadDecomposition = False
+
+#sd_tools._ASym2=False
+#sd_tools._SSym=False
+
+sd_tools._ASectorsDots = True
+
+
+sd_tools.save_sd = save_sd
+
+
+def execute(name, model, points=10000, threads=4, calc_delta=0., neps=0):
+    return calculate.execute("%s/%s/" % (method_name,name), model, points=points, threads=threads, calc_delta=calc_delta, neps=neps)
+
+
+def result(model, method, **kwargs):
+    return calculate.result(model, method, **kwargs)
