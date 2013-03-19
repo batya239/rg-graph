466b16f7dcc2cf0704c77918e11c8fd8ddfd9369
mkompan <mkompan@v401.phys.cc.spbu.ru>
2013-03-20 01:05:34 +0400

    cluster scripts

---
+++ phi4/graphs/qsub-gen-sectors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+WORKDIR=~/workspace/rg-graph/phi4/graphs
+GRAPH=$1
+NAME="${1}_GS"
+NODESPPN=1
+#echo $PNAME
+
+#cat ~/tmp/qsub-integral.tmpl|sed "s/<PROG>/$PNAME/"|sed "s/<POINTS>/$POINTS/"|sed "s/<NODES>/$NODES/"| sed "s/<ITER>/$NITER/"| sed "s/<DELTA>/$DELTA/"
+cd $WORKDIR
+cat qsub-gen-sectors.tmpl|sed "s/<GRAPH>/$GRAPH/"| qsub  -N $NAME -l nodes=$NODESPPN -q long@pbs-vm.hpc.cc.spbu.ru -W group_list=vcluster 
