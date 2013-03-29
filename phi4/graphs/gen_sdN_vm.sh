2e7dbcd84ab85efc0f02b24196c7ca66af392a71
Mikhail Kompaniets <mkompan@gmail.com>
2013-03-30 02:53:03 +0400

    vmcluster scripts

---
+++ phi4/graphs/gen_sdN_vm.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd $PWD
+MODEL=`cat modelN.txt`
+for i in `cat _to_vmclusterN.txt`; do 
+TT=`qstat -a @pbs-vm.hpc.cc.spbu.ru|grep mkompan|wc -l`
+M=`cat NQ-vm.txt`
+while [ $TT -gt $M ]; do
+#echo $TT
+sleep 10
+M=`cat NQ-vm.txt`
+TT=`qstat -a @pbs-vm.hpc.cc.spbu.ru|grep mkompan|wc -l`
+done
+echo $i
+sh qsub-gen-sdN-vm.sh $i $MODEL
+done
