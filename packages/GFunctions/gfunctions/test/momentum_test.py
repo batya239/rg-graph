f8666d725ded9fb966ddfaeb6680bc37a7c60406
Dmitry Batkovich <batya239@gmail.com>
2013-06-01 12:50:32 +0400

    broken test

--- packages/GFunctions/gfunctions/test/momentum_test.py
+++ packages/GFunctions/gfunctions/test/momentum_test.py
@@ -16,13 +16,13 @@ class SubGraphReducerTestCase(unittest.TestCase):
     def testArbitraryPassMomentum(self):
         g = graphine.Graph.initEdgesColors(graphine.Graph(graph_state.GraphState.fromStr("ee12-e23-44-e44--::")))
         for x in momentum.xArbitrarilyPassMomentum(g):
-            self.assertFalse(str(x[0]).startswith("ee"))
+            self.assertFalse(str(x).startswith("ee"))
 
     def doTestArbitrarilyPassMomentum(self, graphStateStr, graphsNumber):
         g = graphine.Graph(graph_state.GraphState.fromStr(graphStateStr))
         graphsWithPassedMomentum = [x for x in momentum.xArbitrarilyPassMomentum(g)]
         for _g in graphsWithPassedMomentum:
-            self.assertEquals(len(_g[0].edges(_g.externalVertex)), 2)
+            self.assertEquals(len(_g.edges(_g.externalVertex)), 2)
         self.assertEquals(len(graphsWithPassedMomentum), graphsNumber)
 
 
