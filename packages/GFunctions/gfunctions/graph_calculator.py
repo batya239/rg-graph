482b8c8e979ff6a6742bf58c69c94c131717d0de
Dmitry Batkovich <batya239@gmail.com>
2013-06-24 22:52:09 +0400

    graph calculator

---
+++ packages/GFunctions/gfunctions/graph_calculator.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf8
+
+__author__ = 'daddy-bear'
+
+
+class AbstractGraphCalculator(object):
+    def getLabel(self):
+        raise NotImplementedError
+
+    def calculate(self, graph):
+        raise NotImplementedError
+
+    def isApplicable(self, graph):
+        raise NotImplementedError
+
+
+class MincerGraphCalculator(AbstractGraphCalculator):
+    pass
+
+
+_calculators = list()
+
+
+def addCalculator(graphCalculator):
+    if isinstance(graphCalculator, AbstractGraphCalculator):
+        _calculators.append(graphCalculator)
+    else:
+        raise ValueError("unsupported calculator")
+
+
+def tryCalculate(graph):
+    for c in _calculators:
+        if c.isApplicable(graph):
+            res = c.calculate(graph)
+            if res is not None:
+                return res, c.getLabel()
+    return None
+
+
+
+
