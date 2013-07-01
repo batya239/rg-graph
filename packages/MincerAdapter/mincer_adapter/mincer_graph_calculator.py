7e37908b3d94ac3ec934722e46cfec2486449eb4
Dmitry Batkovich <batya239@gmail.com>
2013-07-01 23:29:41 +0400

    condition

--- packages/MincerAdapter/mincer_adapter/mincer_graph_calculator.py
+++ packages/MincerAdapter/mincer_adapter/mincer_graph_calculator.py
@@ -6,7 +6,13 @@ import mincer
 
 class MincerGraphCalculator(gfunctions.graph_calculator.AbstractGraphCalculator):
     def isApplicable(self, graph):
-        return graph.calculateLoopsCount() <= 3
+        if graph.calculateLoopsCount() > 3:
+            return False
+        for e in graph.allEdges():
+            c = e.colors
+            if c is None or len(c) != 2:
+                return False
+            return c[1] == 0 and c[0] == int(c[0])
 
     def getLabel(self):
         return "mincer graph calculator"
