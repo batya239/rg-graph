f533dd67300878b39f99bdf0fffdc2ebe714456b
Mikhail Kompaniets <mkompan@gmail.com>
2013-07-06 23:47:34 +0400

    mincer test script

--- packages/MincerAdapter/run_tests.sh
+++ packages/MincerAdapter/run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export PYTHONPATH=`pwd`/mincer
+export PYTHONPATH=`pwd`/mincer_adapter
 
-for i in `ls mincer/test/*.py`; do
+for i in `ls mincer_adapter/test/*.py`; do
     echo $i
     python $i 1>/dev/null || exit 1
 done
