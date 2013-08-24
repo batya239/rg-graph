2163fa4b2162ff89c72d6cdb6fd389b5dc5c64c7
Mikhail Kompaniets <mkompan@gmail.com>
2013-08-24 17:53:31 +0400

    install script

---
+++ packages/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PWD1=`pwd`
+echo $PWD1
+for i in `ls -d GraphState Graphine GFunctions RgGraphUtil Polynomial MincerAdapter`; do
+   cd $PWD1
+   cd $i
+   echo intstalling $i
+   A=`python setup.py install --user`
+   echo $A
+   echo
+done;
