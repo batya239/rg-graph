52cdbf0bedac3b0ae8ba51fc06ab06aa88362a9c
Mikhail Kompaniets <mkompan@gmail.com>
2013-08-26 22:22:50 +0400

    VL

--- phi4/graphs/gen_vl.py
+++ phi4/graphs/gen_vl.py
@@ -2,14 +2,22 @@
 
 import topology
 import sys
+import graphine
+import graphine.graph_operations
 
-valence=int(sys.argv[1])
-nvertex=int(sys.argv[2])
 
-topologies = [t for t in topology.GetTopologies({valence:nvertex})]
+def isGraphVertexIrreducible(graph):
+    return graphine.graph_operations.isGraphVertexIrreducible(graph.allEdges(), graph, graph.allEdges())
+
+
+
+valence = int(sys.argv[1])
+nvertex = int(sys.argv[2])
+
+topologies = [t for t in topology.GetTopologies({valence: nvertex})]
 
 for t in topologies:
-    print t
+    print t, isGraphVertexIrreducible(graphine.Graph.fromStr(t))
 
 print
 print len(topologies)
