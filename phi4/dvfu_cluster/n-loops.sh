5c46ee89c49d37502957ac6d1aa33588926bbddc
Yury Kirienko <yury.kirienko@gmail.com>
2013-08-30 05:58:55 +1100

    small bugfixes to dvfu_cluster/n-loops.sh

--- phi4/dvfu_cluster/n-loops.sh
+++ phi4/dvfu_cluster/n-loops.sh
@@ -7,7 +7,7 @@ WORKDIR=$HOME'/work/rg-graph/phi_4_d2_s2/feynmanSDdotSF_mpi/'
 rm -rf $WORKDIR/e*
 
 cd '../graphs/'
-for diag in $(for i  in `ls phi4/e?-[123]*`; do awk '{print $1}' $i; done)
+for diag in $(for i  in `ls phi4/e?-[1234]*`; do awk '!/S/{print $1}' $i; done)
     do
         python gen_sectorsN.py $diag methods.feynmanSDdotSF_mpi _phi4_d2_s2
         python gen_sdN_mpi.py  $diag methods.feynmanSDdotSF_mpi _phi4_d2_s2
@@ -17,7 +17,7 @@ for d in `ls -d */`
     do
         cd $d
         scons -f ../SConstruct
-        ./cuba.run 3 10000 1e-4 1e-8 > out_${PWD##*/}_10k_e-4_e-8_cuhre
+        ./cuba.run 3 100000 1e-4 1e-8 > out_${PWD##*/}_100k_e-4_e-8_cuhre
         cd ..
     done
 cd $CUR_DIR
