06fbe912eb498e323c5347dd4510cdbd27af64e9
Dmitry Batkovich <batya239@gmail.com>
2013-09-13 07:57:41 +0400

    s

--- packages/Phi4/phi4/__init__.py
+++ packages/Phi4/phi4/__init__.py
@@ -4,10 +4,8 @@ from rggraphutil.env import graph_calculator
 
 __author__ = 'dima'
 
+import lambda_number
 import symbolic_functions
-from gfunctions.gfun_calculator import GGraphReducer
-from graphine.momentum import oneIrreducible, connected, \
-    xPassExternalMomentum, xPickPassingExternalMomentum, passMomentOnGraph, xArbitrarilyPassMomentum
-from graphine.filters import graphFilter
-from gfunctions.r import KR1
+from gfun_calculator import GGraphReducer
+from r import KR1
 from common import AbstractKOperation, MSKOperation, defaultGraphHasNotIRDivergenceFilter, defaultSubgraphUVFilter, CannotBeCalculatedError
