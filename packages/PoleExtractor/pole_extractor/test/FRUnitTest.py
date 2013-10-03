73262b0a2429fb70b9fc92bf5fb4c26bb2db7409
Gleb Dovzhenko <dovjenko.g@gmail.com>
2013-10-03 21:41:55 +0400

    PoleExtractor is a package now

---
+++ packages/PoleExtractor/pole_extractor/test/FRUnitTest.py
@@ -0,0 +1,15 @@
+__author__ = 'gleb'
+
+import unittest
+
+
+class TestIntegrand(unittest.TestCase):
+    def det_test(self):
+        # TODO: this is bad style, I use methods that are supposed to be private & calculate same values twice.
+
+        return
+
+
+class TestCoefficients(unittest.TestCase):
+    def c_test(self):
+        return
