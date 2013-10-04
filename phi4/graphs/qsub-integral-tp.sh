1b4aef92e29bf5d759f28dd7d5ef0f891140b40e
Mikhail Kompaniets <mkompan@gmail.com>
2013-10-04 19:18:46 +0400

    qsub LIB from export

--- phi4/graphs/qsub-integral-tp.sh
+++ phi4/graphs/qsub-integral-tp.sh
@@ -8,11 +8,18 @@ NITER=$4
 DELTA=$5
 NODESPPN=$6
 DIR=$7
-SCRIPTDIR='/home/mkompan/workspace/rg-graph/phi4/graphs'
+if [ -z "$LIB" ]; then
+    LIB=$HOME/lib/pvegas
+fi
+if [ -z "$SCRIPTDIR" ]; then
+   SCRIPTDIR='/home/mkompan/workspace/rg-graph/phi4/graphs'
+fi
+
+
 
 PNAME=`echo $DIR/$NAME|sed 's/\//\\\\\//g'`
 DIRNAMR=`echo $DIR|sed 's/\//\\\\\//g'`
 #echo $PNAME
 
-cat $SCRIPTDIR/qsub-integral.tmpl|sed "s/<DIR>/$DIRNAME/"|sed "s/<PROG>/$PNAME/"|sed "s/<POINTS>/$POINTS/"|sed "s/<NODES>/$NODES/"| sed "s/<ITER>/$NITER/"| sed "s/<DELTA>/$DELTA/"| qsub  -N $NAME -l nodes=$NODESPPN  -q long@pbs-tp.hpc.cc.spbu.ru 
+cat $SCRIPTDIR/qsub-integral.tmpl|sed "s/<DIR>/$DIRNAME/"|sed "s/<PROG>/$PNAME/"|sed "s/<POINTS>/$POINTS/"|sed "s/<NODES>/$NODES/"| sed "s/<ITER>/$NITER/"| sed "s/<DELTA>/$DELTA/"| sed "s/<LIB>/$LIB/"| qsub  -N $NAME -l nodes=$NODESPPN  -q long@pbs-tp.hpc.cc.spbu.ru 
 #-W group_list=tpcluster
