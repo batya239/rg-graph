20a362e1e9dca58a8b44453a99bc6b007bc96b26
Mikhail Kompaniets <mkompan@gmail.com>
2013-10-10 22:11:20 +0400

    gen_compile_script ppn fix

--- phi4/spbu_cluster/sample_settings.py
+++ phi4/spbu_cluster/sample_settings.py
@@ -5,8 +5,9 @@ __author__ = 'mkompan'
 workdir = "akkuratova/rg-graph/phi4/graphs/phi4_dyn/simpleSDT"
 libdir = "libs/pvegas"
 mpidir = "/usr/lib64/openmpi/1.4-gcc"
-nodes = "16"
-nodesppn = "%s:ppn=4" % nodes
+nodes = 16
+ppn = 4
+nodesppn = "%s:ppn=%s" % (nodes/ppn, ppn)
 iterations = 2
 delta = 0.
 points = 100000
