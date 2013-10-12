29f33a5ad2e687f5b2e6154b53203a43cc67fea6
Mikhail Kompaniets <mkompan@gmail.com>
2013-10-12 18:16:21 +0400

    SA settings

---
+++ phi4/spbu_cluster/sa_phi4_dyn_settings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf8
+__author__ = 'mkompan'
+
+workdir = "rg-graph/phi4/graphs/phi4_dyn/simpleSDT"
+libdir = "libs/pvegas"
+mpidir = "/usr/lib64/openmpi/1.4-gcc"
+nodes = 16
+ppn = 4
+iterations = 2
+delta = 0.
+points = 100000
