485f22cbeb3464a9aa19899ef350a54e2ea7ca36
Mikhail Kompaniets <mkompan@gmail.com>
2013-10-18 00:54:24 +0400

    phi3 VL

--- phi4/graphs/phi3_vl3.py
+++ phi4/graphs/phi3_vl3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf8
+import os
 
 import conserv
 import dynamics
@@ -20,7 +21,7 @@ graph = graphine.Graph.fromStr(sys.argv[1])
 graph._subgraphs_as_line_ids = []
 internal_edges = sd_tools.internal_edges_dict(graph)
 cons = conserv.Conservations(internal_edges)
-if use_eqs :
+if use_eqs:
     eqs = sd_tools.find_eq(cons)
 else:
     eqs = dict()
@@ -31,27 +32,26 @@ graph._qi, graph._qi2l = sd_tools.qi_lambda(cons, eqs)
 print graph._qi
 D = sd_tools.gendet(graph)
 
-
-
 D_ = sd_tools.gendet(graph)
 
-D = polynomial.poly(map(lambda x:(1,x), D_), degree=(-3,1)).toPolyProd()
-print D
-delta = polynomial.poly(map(lambda x: (1,(x,)), D.getVarsIndexes()))
-print delta
+D = polynomial.poly(map(lambda x: (1, x), D_), degree=(-3, 1)).toPolyProd()
+#print D
+delta = polynomial.poly(map(lambda x: (1, (x,)), D.getVarsIndexes()))
+#print delta
 
 tree = sd_tools.gen_speer_tree(graph)
 
-for sector in dynamics.xTreeElement2(tree):
-    print sector
-    expr = sd_lib.sectorDiagram(D, sector, delta_arg=delta)[0][0].simplify()
-    print expr
+dir = os.path.join("sample_extraction/", str(graph))
+try:
+    os.makedirs(dir)
+except OSError:
+    pass
 
-    print isinstance(expr, PolynomialProduct)
-    extracted = pole_extractor.extract_poles_and_eps_series(expr, 1)
-    formatted_dict = formatter.formatPoleExtracting(extracted)
-    print formatted_dict.keys()
-    for i in formatted_dict.keys():
-        print i, len(formatted_dict[i]), type(formatted_dict[i])
-        if i == -2:
-            print formatted_dict[i]
+for functions_file in sd_tools.generate_func_files(tree, lambda x: sd_lib.sectorDiagram(D, x, delta_arg=delta)[0][0].simplify()):
+    print os.path.join(dir, "%s.c" % functions_file.get_file_name(str(graph))), functions_file.file_info, len(functions_file.functions), functions_file.functions_count
+    f = open(os.path.join(dir, "%s.c" % functions_file.get_file_name(str(graph))), "w")
+    f.write(functions_file.get_c_file())
+    f.close()
+    f = open(os.path.join(dir, "%s.h" % functions_file.get_file_name(str(graph))), "w")
+    f.write(functions_file.get_h_file())
+    f.close()
