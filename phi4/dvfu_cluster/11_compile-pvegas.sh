975a97f67d5a1a7d66e708becb865951632bcf85
Yury Kirienko <yury.kirienko@gmail.com>
2013-10-18 09:00:41 +0400

    SConstruct.pvegas draft

--- phi4/dvfu_cluster/11_compile-pvegas.sh
+++ phi4/dvfu_cluster/11_compile-pvegas.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+sh $HOME/python_env.sh
 NUM_THREADS=2
 CUR_DIR=`pwd`
 WORKDIR=$HOME'/work/rg-graph/phi_4_d2_s2/feynmanSDdotSF_mpi/'
 #if !`ls $WORKDIR | grep SConstruct`
     cp $CUR_DIR/SConstruct.pvegas $WORKDIR
 #if !`ls $WORKDIR | grep cubaCodeTemplate.py`
-    cp $CUR_DIR/cubaCodeTemplate.py $WORKDIR
-METHOD='cuhre'
+#    cp $CUR_DIR/cubaCodeTemplate.py $WORKDIR
+#METHOD='cuhre'
 cd $WORKDIR
 for d in `ls -d */`
     do
         cd $d
-        scons -j $NUM_THREADS -f ../SConstruct.pvegas
+        scons -j 8 -f ../SConstruct
         cd ..
     done
 cd $CUR_DIR
+#./20_run-cuba.sh
