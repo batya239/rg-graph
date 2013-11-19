873ed1ace512c869aa3f66dd74ff22be8708009a
Dmitry Batkovich <batya239@gmail.com>
2013-11-19 18:55:40 +0400

    jrules_parser

--- packages/Reduction/reduction/jrules_parser.py
+++ packages/Reduction/reduction/jrules_parser.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf8
+import rggraphutil
+
 __author__ = ['mkompan', 'dima']
 
 import sys
@@ -17,19 +19,20 @@ _DEBUG = False
 
 def _read_raw_zero_sectors(zero_sectors_string, j_suffix):
     _zero_sectors_string = zero_sectors_string[1: -2]
-    return set(map(lambda s: s.replace("js[%s, " % j_suffix, "Sector(").replace("]", ")"), _JS_REGEXP.findall(_zero_sectors_string)))
+    return set(map(lambda s: s.replace("js[%s, " % j_suffix, "Sector(").replace("]", ")"),
+                   _JS_REGEXP.findall(_zero_sectors_string)))
 
 
 def _create_zero_rules(raw_zero_sectors):
-    result = list()
+    result = set()
     for rs in raw_zero_sectors:
         propagators = list()
         for e in eval(rs[6:]):
             if e == 1:
-                propagators.append(sector.SectorRule.PROPAGATOR_CONDITION_POSITIVE)
+                propagators.append(sector.SectorRuleKey.PROPAGATOR_CONDITION_POSITIVE)
             else:
-                propagators.append(sector.SectorRule.PROPAGATOR_CONDITION_NOT_POSITIVE)
-        result.append(sector.SectorRule.create_zero_rule(propagators))
+                propagators.append(sector.SectorRuleKey.PROPAGATOR_CONDITION_NOT_POSITIVE)
+        result.add(sector.SectorRuleKey(propagators))
     return result
 
 
@@ -80,9 +83,9 @@ def _convert_sector_conditions(sector_condition_string):
             n, value = regex_result.groups()
             value = value.rstrip()
             if value == 'Positive':
-                result.append(sector.SectorRule.PROPAGATOR_CONDITION_POSITIVE)
+                result.append(sector.SectorRuleKey.PROPAGATOR_CONDITION_POSITIVE)
             elif value == 'NonPositive':
-                result.append(sector.SectorRule.PROPAGATOR_CONDITION_NOT_POSITIVE)
+                result.append(sector.SectorRuleKey.PROPAGATOR_CONDITION_NOT_POSITIVE)
             else:
                 raise AssertionError("%s is incorrect sector condition" % value)
     return result
@@ -101,15 +104,13 @@ def _replace_n(string):
 def _parse_rule(rule_string, j_suffix, raw_zero_sectors):
     regex_result = _SECTOR_REGEXP.match(rule_string)
     if regex_result:
-        return sector.SectorRule(_convert_sector_conditions(regex_result.groups()[0]),
-                                 _parse_additional_condition(regex_result.groups()[1]),
+        return sector.SectorRuleKey(_convert_sector_conditions(regex_result.groups()[0])), \
+               sector.SectorRule(_parse_additional_condition(regex_result.groups()[1]),
                                  _convert_rule(regex_result.groups()[2], j_suffix).replace("^", "**"))
     if "Expand" in rule_string:
         rule = _parse_strange_rule(rule_string, j_suffix, raw_zero_sectors)
-        return sector.SectorRule(_convert_sector_conditions(rule_string[:rule_string.index("] :>")]),
-                                 None,
-                                 rule[0].replace("^", "**"),
-                                 expection_condition=rule[1])
+        return sector.SectorRuleKey(_convert_sector_conditions(rule_string[:rule_string.index("] :>")])), \
+               sector.SectorRule(None, rule[0].replace("^", "**"), expection_condition=rule[1])
     raise ValueError("invalid rule: %s" % rule_string)
 
 
@@ -122,9 +123,7 @@ def read_raw_zero_sectors(file_path, j_suffix):
 def x_parse_rules(file_path, j_suffix, raw_zero_sectors):
     j_rules_string = ''.join(map(lambda x: x.rstrip(), open(file_path).readlines()))[2:-1].split(', j')
     for item in j_rules_string:
-        rule = _parse_rule(item, j_suffix, raw_zero_sectors)
-        if rule is not None:
-            yield rule
+        yield _parse_rule(item, j_suffix, raw_zero_sectors)
 
 
 def parse_propagators(file_path, loops_count):
