15876e411c09047e1a0c7f8623d192bcb3102792
Dmitry Batkovich <batya239@gmail.com>
2013-11-22 02:41:00 +0400

    example

--- packages/Reduction/reduction/example.py
+++ packages/Reduction/reduction/example.py
@@ -6,11 +6,4 @@ __author__ = 'dima'
 import reductor
 import sector
 
-result = reductor.THREE_LOOP_REDUCTOR.evaluate_sector(sector.Sector(0, 0, 0, 3, 1, 1, 1, 0, 1))
-
-print "d=5"
-print result.subs(sector.d == 6)
-print "d=10"
-print result.subs(sector.d == 10)
-print "d=15"
-print result.subs(sector.d == 15)
+result = reductor.THREE_LOOP_REDUCTOR.evaluate_sector(sector.Sector(0, 1, -2, 0, 0, 1, 1, 1, 1))
