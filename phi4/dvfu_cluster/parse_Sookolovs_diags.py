500974d61e90a05b68950c6b0a85921111d6d6e2
Yury Kirienko <yurry.kirienko@gmail.com>
2013-12-10 19:29:37 +1100

    parse_Sookolovs_diags.py

--- phi4/dvfu_cluster/parse_Sookolovs_diags.py
+++ phi4/dvfu_cluster/parse_Sookolovs_diags.py
@@ -3,7 +3,7 @@
 from mmap import mmap,ACCESS_READ
 from xlrd import open_workbook
 
-aString = open('sokolov_diags.xls','rb').read()
+aString = open('sokolov_diags_1.xls','rb').read()
 wb = open_workbook(file_contents=aString)
 
 diags = []
@@ -14,5 +14,11 @@ for s in wb.sheets():
         for col in range(s.ncols):
             values.append(s.cell(row,col).value)
             print ','.join(map(str,values))
-            print
+        diags += [values]
+
+print diags
+
+f_mkompan = open("phi4_d2_s2-5loop-e4-100M-6loop-e2-1M.py")
+data2 = eval(f_mkompan.read())
+f_mkompan.close()
 
