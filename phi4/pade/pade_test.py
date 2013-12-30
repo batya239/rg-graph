105155c53addde695cf63680a92e83b607366c92
Mikhail Kompaniets <mkompan@gmail.com>
2013-12-31 01:15:13 +0400

    pade

--- phi4/pade/pade_test.py
+++ phi4/pade/pade_test.py
@@ -3,13 +3,11 @@
 __author__ = 'mkompan'
 
 import sympy
-import FuncDesigner
-import openopt
 
 
-def pade(L, M, tau):
+def pade_aproximant(L, M, tau):
     numerator = 0
-    for i in range(1, L + 1):
+    for i in range(0, L + 1):
         coeff = sympy.var('a%s' % i)
         numerator += coeff * tau ** (i )
     denominator = 1
@@ -17,74 +15,95 @@ def pade(L, M, tau):
         coeff = sympy.var('b%s' % i)
         denominator += coeff * tau ** (i )
 
-    return numerator / denominator
+    return numerator, denominator
 
 
-def solvePadeOO(padePoly, seriesDict, N, tau):
+
+def dict2poly(series_dict, var):
+    res = 0
+    for power, coeff in series_dict.iteritems():
+        res += var**power * coeff
+    return res
+
+
+def eqs2matrix(eqs, vars):
+    matrix_list=list()
+    for eq in eqs:
+        row = list()
+        const = eq
+        for var in vars:
+            row.append(eq.coeff(var, 1))
+            const = const.subs(var, 0)
+#        print "const", const
+        row.append(-const)
+        matrix_list.append(row)
+    return sympy.Matrix(matrix_list)
+
+
+def solve_pade_sympy(pade_num, pade_denom, series_dict, n, tau):
     vars = list()
-    for atom in padePoly.atoms():
-        if isinstance(atom, sympy.Symbol) and atom <> tau:
-        #            vars.append(FuncDesigner.oovar(str(atom)))
-            exec ('%s=FuncDesigner.oovar("%s")\nvars.append(%s)' % (str(atom), str(atom), str(atom)))
-    print vars
-    expansion = padePoly.series(tau, 0, N + 1)
-    equations = list()
-    for power in sorted(seriesDict.keys()):
-        if power <= N:
-            print "%s==%s" % (str(expansion.coeff(tau, power).evalf()), seriesDict[power])
-            equations.append(eval("%s==%s" % (str(expansion.coeff(tau, power).evalf()), seriesDict[power])))
-    print equations
-    start_point = dict([(x, 0) for x in vars])
-    print start_point
-    p = openopt.SNLE(tuple(equations), start_point)
-    res = p.solve('nssolve')
-    return dict(zip(vars, res(*vars)))
-
-#    print sympy.solve_linear(equations[0][0], equations[0][1], vars)
-#    xxx = sympy.solve_linear(equations[0][0], equations[0][1], vars)
-#    print equations[1].subs(x)
-
-
-def solvePadeSP(padePoly, seriesDict, N, tau):
-    vars = list()
-    for atom in padePoly.atoms():
+    atoms = set()
+    for poly in (pade_num, pade_denom):
+        try:
+            atoms = atoms | poly.atoms()
+        except:
+            pass
+    for atom in atoms:
         if isinstance(atom, sympy.Symbol) and atom <> tau:
             vars.append(atom)
-    print vars
-    expansion = padePoly.series(tau, 0, N + 1)
-    equations = list()
-    for power in sorted(seriesDict.keys()):
-        if power <= N:
-            print "%s==%s" % (str(expansion.coeff(tau, power).evalf()), seriesDict[power])
-            equations.append(expansion.coeff(tau, power).evalf()-seriesDict[power])
-    print equations
+    series_poly = dict2poly(series_dict, tau)
+    eq = (pade_num-pade_denom*series_poly).expand()
+    eqs = list()
+    for i in range(n+1):
+        eqs.append(eq.coeff(tau, i))
+    return sympy.solve_linear_system(eqs2matrix(eqs, vars), *vars)
 
 
+def borel_transform(series_dict, b=0):
+    return dict(map(lambda x: (x, series_dict[x]/sympy.gamma(x+b+1).evalf()), series_dict))
 
-tau = sympy.var('tau')
+
+def resummation_pade(L,M, series_dict):
+    tau = sympy.var('tau')
+    padeNum, padeDenom = pade_aproximant(L, M, tau)
+    padeFunc = padeNum/padeDenom
+    res = solve_pade_sympy(padeNum, padeDenom, series_dict, L + M, tau)
+#    print res
+    padeFunc_ = padeFunc
+    for var, value in res.iteritems():
+        var_ = sympy.var(str(var))
+        padeFunc_ = padeFunc_.subs(var_, value)
+
+    return padeFunc_.subs(tau,1)
 
 gStar = {1: 1, 2: 0.716173621, 3: 0.095042867, 4: 0.086080396, 5: -0.204139}
 
-b = 0
+gamma_minus = {0: 1, 1: -1./3, 2: -0.113701246, 3: 0.024940678, 4: -0.039896059, 5: 0.0645212}
 
-gStarBorel = dict(map(lambda x: (x,gStar[x]/sympy.gamma(x+b+1).evalf()), gStar))
-print gStarBorel
+nu_minus = {0: 2, 1: -2./3, 2: -0.2613686, 3: 0.0145746, 4: -0.0913127, 5: 0.118121}
 
-L = 1
-M = 3
+#b = 0
+#gStarBorel = borel_transform(gStar, b=0)
+#print gStarBorel
 
-padeFunc = pade(L, M, tau)
-padePoly = padeFunc.series(tau, 0, L + M + 1).removeO()
+N = 5
+print "\ngStar\n"
+for M in range(0, N):
+    for L in range(1, N-M+1):
+#        print M, L,
+        print "%10.4f" % (resummation_pade(L, M, gStar)),
+    print
 
-res = solvePadeOO(padePoly, gStar, L + M, tau)
-#res = solvePadeOO(padePoly, gStarBorel, L + M, tau)
-print res
-#print solvePadeSP(padePoly, gStar, L + M, tau)
+print "\ngamma^-1\n"
+for M in range(0, N+1):
+    for L in range(0, N-M+1):
+#        print M, L,
+        print "%10.4f" % (1/resummation_pade(L, M, gamma_minus)),
+    print
 
-
-padeFunc_ = padeFunc
-for var, value in res.iteritems():
-    var_= sympy.var(str(var))
-    padeFunc_ = padeFunc_.subs(var_, value)
-
-print padeFunc_.subs(tau,1)
+print "\nnu^-1\n"
+for M in range(0, N+1):
+    for L in range(0, N-M+1):
+#        print M, L,
+        print "%10.4f" % (1/resummation_pade(L, M, nu_minus)),
+    print
