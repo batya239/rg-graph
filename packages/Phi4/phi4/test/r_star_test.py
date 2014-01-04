93238b17cf9d067d8fb32c9d471bd58da42a7f88
Dmitry Batkovich <batya239@gmail.com>
2014-01-05 01:55:13 +0400

    test uncommented

--- packages/Phi4/phi4/test/r_star_test.py
+++ packages/Phi4/phi4/test/r_star_test.py
@@ -48,87 +48,70 @@ class RStarTestCase(base_test_case.GraphStorageAwareTestCase):
     def test_e1123_e23___(self):
         self._do_test_krstar("e1123|e23|||", "1/(3*e**3)-2/(3*e**2)+1/(3*e)")
 
-    #def test_e112_e3_334_4__(self):
-    #    self._do_test_krstar("e112-e3-334-4--",
-    #                         "-5/(24*e**4)+5/(12*e**3)+1/(24*e**2)-1/(4*e)",
-    #                         use_graph_calculator=True)
-    #
-    #def test_e1123_44_4_4_e_(self):
-    #    self._do_test_krstar("e1123-44-4-4-e-", "-1/(6*e**4)+1/(3*e**3)+1/(3*e**2)-1/e+zeta(3)/e")
-    #
-    #def test_e122_e33_4_44__(self):
-    #    self._do_test_krstar("e112-e3-e44-e44--", "(0.5-zeta(3))/2/e+1./2/2/e**2+2./2/2/2/e**3-4./2/2/2/2/e**4")
-    #
-    #def test_e123_224_4_4_e_(self):
-    #    self._do_test_krstar("e123-224-4-4-e-", "-1/(12*e**4)+1/(3*e**3)-5/(12*e**2)-1/(2*e)+zeta(3)/e",
-    #                         use_graph_calculator=True)
-    #
-    #def test_e114_22_33_e4__(self):
-    #    self._do_test_krstar("e114-22-33-e4--", "-1/(4*e**4)+1/(4*e**3)+1/(4*e**2)+1/(4*e)-zeta(3)/(2*e)")
-    #
-    #def test_e123_e23_33__(self):
-    #    self._do_test_krstar("ee12-e34-334-4-e-",
-    #                         "(5./2-2*zeta(3))/2/e-19./6/2/2/e**2+2./2/2/2/e**3-2./3/2/2/2/2/e**4",
-    #                         use_graph_calculator=True)
-    #
-    #def test_ee12_233_34_4_ee_(self):
-    #    self._do_test_krstar("ee12-233-34-4-ee-",
-    #                         "-1/2*e**(-1)*(-1.8333333333333332593+zeta(3))-(1.0833333333333332593)*e**(-2)+(0.41666666666666668517)*e**(-3)-(0.08333333333333332871)*e**(-4)",
-    #                         use_graph_calculator=True)
-    #
-    #def test_e112_23_e4_e45_55_e_(self):
-    #    self._do_test_krstar("e112-23-e4-e45-55-e-",
-    #                         "(8./30)/e-(8./60)/e/e+34./120/e/e/e-16./80/e/e/e/e+8./160/e/e/e/e/e",
-    #                         use_graph_calculator=True)
-    #
-    #def test_112_e23_e4_45_55__(self):
-    #    self._do_test_krstar("e112-23-e4-e45-55-e-",
-    #                         "(8./30)/e-(8./60)/e/e+34./120/e/e/e-16./80/e/e/e/e+8./160/e/e/e/e/e",
-    #                         use_graph_calculator=True)
-    #
-    #def test_ee12_23_33__(self):
-    #    self._do_test_ir_coperation("ee12-23-33-", "2/3*e**(-1)-1/2*e**(-2)+1/6*e**(-3)")
-    #
-    #def test_ee12_223_4_e45_55_e_(self):
-    #    self._do_test_krstar("ee12-223-4-e45-55-e-",
-    #                         "-(0.53333333333333332593)*e**(-1)+(0.35833333333333333703)*e**(-3)+(0.11666666666666666852)*e**(-2)-(0.28333333333333332593)*e**(-4)+(0.074999999999999997224)*e**(-5)")
-    #
-    #def test_ee12_223_4_e45_55_e_2(self):
-    #    self._do_test_krstar("e112-23-e4-445-5--",
-    #                         "-(0.53333333333333332593)*e**(-1)+(0.35833333333333333703)*e**(-3)+(0.11666666666666666852)*e**(-2)-(0.28333333333333332593)*e**(-4)+(0.074999999999999997224)*e**(-5)")
-    #
-    #def test_e1123_e34_445__5___(self):
-    #    self._do_test_krstar("e1123-e34-445--5--",
-    #                         "-2./15/2/e+(16./15)/2/2/e/e+8./5/2/2/2/e/e/e-32./5/2/2/2/2/e/e/e/e+64./15/2/2/2/2/2/e/e/e/e/e")
-    #
-    #def test_ee12_334_355_5_ee5__(self):
-    #    self._do_test_krstar("ee12-334-355-5-ee5--",
-    #                         "-(3./10*zeta(4)-11./5*zeta(3)+14./5)/2/e-(2./5*zeta(3)-29./15)/2/2/e/e+34./15/2/2/2/e/e/e-52./15/2/2/2/2/e/e/e/e+8./5/2/2/2/2/2/e/e/e/e/e", use_graph_calculator=True)
-    #
-    #def test_ee12_334_345_e_55_e_(self):
-    #    self._do_test_krstar("ee12-334-345-e-55-e-",
-    #                         "-(4./3)/2/e-(2./15)/2/2/e/e+11./5/2/2/2/e/e/e-32./15/2/2/2/2/e/e/e/e+4./5/2/2/2/2/2/e/e/e/e/e")
-    #
-    #def test_ee12_234_35_45_e5_e_(self):
-    #    self._do_test_krstar("e112-34-e35-45-e5-e-",
-    #                         "-1/378*(Pi**6-(75.6000000000000042)*zeta(3)**2-1890*zeta(5))*e**(-1)-zeta(5)*e**(-2)", use_graph_calculator=True)
-    #
-    ##TODO
-    #def _test_e123_e24_55_e45_e5__(self):
-    #    self._do_test_krstar("e123-e24-55-e45-e5--",
-    #                         "-1/378*(Pi**6+(831.60000000000006715)*zeta(3)**2-1890*zeta(5))*e**(-1)-zeta(5)*e**(-2)", use_graph_calculator=True)
-    #
-    #def test_e123_e45_e45_445__e_(self):
-    #    self._do_test_krstar("e123-e45-e45-445--e-",
-    #                         "-zeta(5)*e**(-2)-1/378*(Pi**6+(1285.1999999999999664)*zeta(3)**2-1890*zeta(5))*e**(-1)", use_graph_calculator=True)
-    #
-    #def test_e112_34_e35_45_e5_e_(self):
-    #    self._do_test_krstar("e112-34-e35-45-e5-e-",
-    #                         "-1/378*(Pi**6-(75.6000000000000042)*zeta(3)**2-1890*zeta(5))*e**(-1)-zeta(5)*e**(-2)", use_graph_calculator=True)
-    #
-    #def test_e112_34_e35_45_e5_e_(self):
-    #    self._do_test_krstar("e112-34-e35-45-e5-e-",
-    #                         "-1/378*(Pi**6-(75.6000000000000042)*zeta(3)**2-1890*zeta(5))*e**(-1)-zeta(5)*e**(-2)", use_graph_calculator=True)
+    def test_e112_e3_334_4__(self):
+        self._do_test_krstar("e112|e3|334|4||",
+                             "-5/(24*e**4)+5/(12*e**3)+1/(24*e**2)-1/(4*e)",
+                             use_graph_calculator=True)
+
+    def test_e1123_44_4_4_e_(self):
+        self._do_test_krstar("e1123|44|4|4|e|", "-1/(6*e**4)+1/(3*e**3)+1/(3*e**2)-1/e+zeta(3)/e")
+
+    def test_e122_e33_4_44__(self):
+        self._do_test_krstar("e112|e3|e44|e44||", "(0.5-zeta(3))/2/e+1./2/2/e**2+2./2/2/2/e**3-4./2/2/2/2/e**4")
+
+    def test_e123_224_4_4_e_(self):
+        self._do_test_krstar("e123|224|4|4|e|", "-1/(12*e**4)+1/(3*e**3)-5/(12*e**2)-1/(2*e)+zeta(3)/e",
+                             use_graph_calculator=True)
+
+    def test_e114_22_33_e4__(self):
+        self._do_test_krstar("e114|22|33|e4||", "-1/(4*e**4)+1/(4*e**3)+1/(4*e**2)+1/(4*e)-zeta(3)/(2*e)")
+
+    def test_e123_e23_33__(self):
+        self._do_test_krstar("ee12|e34|334|4|e|",
+                             "(5./2-2*zeta(3))/2/e-19./6/2/2/e**2+2./2/2/2/e**3-2./3/2/2/2/2/e**4",
+                             use_graph_calculator=True)
+
+    def test_ee12_233_34_4_ee_(self):
+        self._do_test_krstar("ee12|233|34|4|ee|",
+                             "-1/2*e**(-1)*(-1.8333333333333332593+zeta(3))-(1.0833333333333332593)*e**(-2)+(0.41666666666666668517)*e**(-3)-(0.08333333333333332871)*e**(-4)",
+                             use_graph_calculator=True)
+
+    def test_e112_23_e4_e45_55_e_(self):
+        self._do_test_krstar("e112|23|e4|e45|55|e|",
+                             "(8./30)/e-(8./60)/e/e+34./120/e/e/e-16./80/e/e/e/e+8./160/e/e/e/e/e",
+                             use_graph_calculator=True)
+
+    def test_112_e23_e4_45_55__(self):
+        self._do_test_krstar("e112|23|e4|e45|55|e|",
+                             "(8./30)/e-(8./60)/e/e+34./120/e/e/e-16./80/e/e/e/e+8./160/e/e/e/e/e",
+                             use_graph_calculator=True)
+
+    def test_ee12_23_33__(self):
+        self._do_test_ir_coperation("ee12|23|33|", "2/3*e**(-1)-1/2*e**(-2)+1/6*e**(-3)")
+
+    def test_ee12_223_4_e45_55_e_(self):
+        self._do_test_krstar("ee12|223|4|e45|55|e|",
+                             "-(0.53333333333333332593)*e**(-1)+(0.35833333333333333703)*e**(-3)+(0.11666666666666666852)*e**(-2)-(0.28333333333333332593)*e**(-4)+(0.074999999999999997224)*e**(-5)")
+
+    def test_ee12_223_4_e45_55_e_2(self):
+        self._do_test_krstar("e112|23|e4|445|5||",
+                             "-(0.53333333333333332593)*e**(-1)+(0.35833333333333333703)*e**(-3)+(0.11666666666666666852)*e**(-2)-(0.28333333333333332593)*e**(-4)+(0.074999999999999997224)*e**(-5)")
+
+    def test_e1123_e34_445__5___(self):
+        self._do_test_krstar("e1123|e34|445||5||",
+                             "-2./15/2/e+(16./15)/2/2/e/e+8./5/2/2/2/e/e/e-32./5/2/2/2/2/e/e/e/e+64./15/2/2/2/2/2/e/e/e/e/e")
+
+    def test_ee12_334_355_5_ee5__(self):
+        self._do_test_krstar("ee12|334|355|5|ee5||",
+                             "-(3./10*zeta(4)-11./5*zeta(3)+14./5)/2/e-(2./5*zeta(3)-29./15)/2/2/e/e+34./15/2/2/2/e/e/e-52./15/2/2/2/2/e/e/e/e+8./5/2/2/2/2/2/e/e/e/e/e", use_graph_calculator=True)
+
+    def test_ee12_334_345_e_55_e_(self):
+        self._do_test_krstar("ee12|334|345|e|55|e|",
+                             "-(4./3)/2/e-(2./15)/2/2/e/e+11./5/2/2/2/e/e/e-32./15/2/2/2/2/e/e/e/e+4./5/2/2/2/2/2/e/e/e/e/e")
+
+    def test_ee12_234_35_45_e5_e_(self):
+        self._do_test_krstar("e112|34|e35|45|e5|e|",
+                             "-1/378*(Pi**6-(75.6000000000000042)*zeta(3)**2-1890*zeta(5))*e**(-1)-zeta(5)*e**(-2)", use_graph_calculator=True)
 
     def _do_test_krstar(self, graph_state_as_string, expected_result_as_string, use_graph_calculator=False):
         try:
