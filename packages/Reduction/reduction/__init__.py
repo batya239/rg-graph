d547d891c63ac6f6377d8cb4c6729b875c1f4311
Dmitry Batkovich <batya239@gmail.com>
2014-01-06 02:52:34 +0400

    cleanup

--- packages/Reduction/reduction/__init__.py
+++ packages/Reduction/reduction/__init__.py
@@ -5,4 +5,3 @@ __author__ = 'dima'
 
 from reductor import TwoAndThreeReductionCalculator, ScalarProductTwoAndThreeLoopsGraphCalculator, THREE_LOOP_REDUCTOR, TWO_LOOP_REDUCTOR
 from scalar_product import ScalarProduct
-from scalar_product_util import enumerated_graph_scalar_product_extractor
