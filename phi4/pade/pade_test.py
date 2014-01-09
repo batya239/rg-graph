6b6f9755d116430d3443354d57f235c1210b7eae
Mikhail Kompaniets <mkompan@gmail.com>
2014-01-09 16:57:41 +0400

    pade refactor

--- phi4/pade/pade_test.py
+++ phi4/pade/pade_test.py
@@ -9,6 +9,11 @@ import scipy.integrate as integrate
 import math
 
 
+from collections import namedtuple
+
+results2013 = namedtuple('results2013', ('gamma', 'gamma_minus', 'nu', 'nu_minus', 'eta'))
+
+
 def pade_aproximant(L, M, tau):
     numerator = 0
     for i in range(0, L + 1):
@@ -52,7 +57,7 @@ def solve_pade_sympy(pade_num, pade_denom, series_dict, n, tau):
         except:
             pass
     for atom in atoms:
-        if isinstance(atom, sympy.Symbol) and atom <> tau:
+        if isinstance(atom, sympy.Symbol) and tau != atom:
             vars.append(atom)
     series_poly = dict2poly(series_dict, tau)
     eq = (pade_num - pade_denom * series_poly).expand()
@@ -64,15 +69,20 @@ def solve_pade_sympy(pade_num, pade_denom, series_dict, n, tau):
     return sympy.solve_linear_system(eqs2matrix(eqs, vars), *vars)
 
 
-def solve_pade_sympy_lob(pade_num, pade_denom, series_dict, n, tau, a=0):
-    return solve_pade_sympy(pade_num, pade_denom*(1+tau*a), series_dict, n, tau)
+def solve_pade_sympy_lob(pade_num, pade_denom, series_dict, n, tau, a=0, g_star=None):
+    """
+    g_star must depend on tau (only for lob in pseudo-esp expansion)
+    """
+    if g_star is None:
+        g_star = tau
+    return solve_pade_sympy(pade_num, pade_denom*(1+g_star*a), series_dict, n, tau)
 
 
 def borel_transform(series_dict, b=0):
     return dict(map(lambda x: (x, series_dict[x] / sympy.gamma(x + b + 1).evalf()), series_dict))
 
 
-def resummation_pade(L, M, series_dict):
+def resummation_pade(L, M, series_dict, series_param_value=1):
     tau = sympy.var('tau')
     padeNum, padeDenom = pade_aproximant(L, M, tau)
     padeFunc = padeNum / padeDenom
@@ -83,18 +93,18 @@ def resummation_pade(L, M, series_dict):
         var_ = sympy.var(str(var))
         padeFunc_ = padeFunc_.subs(var_, value)
 
-    return padeFunc_.subs(tau, 1)
+    return padeFunc_.subs(tau, series_param_value)
 
 #FIXME : b!=0 !!!!
 func_template = """
-def func(x,b):
+def func(x,b,g):
     tau = x/(1-x)
     res = tau**b*math.exp(-tau) * ({pade})/(1-x)**2
     return res
 """
 
 
-def resummation_pade_borel(L, M, series_dict, a=0, b=0):
+def resummation_pade_borel(L, M, series_dict, a=0, b=0, series_param_value=1):
     tau = sympy.var('tau')
     borel_dict = borel_transform(series_dict, b=b)
     padeNum, padeDenom = pade_aproximant(L, M, tau)
@@ -110,7 +120,7 @@ def resummation_pade_borel(L, M, series_dict, a=0, b=0):
 #    print func_template.format(pade=padeFunc_)
     exec(func_template.format(pade=padeFunc_))
     try:
-        output = integrate.quad(func, 0., 1., args=(b,), full_output=1, limit=100)
+        output = integrate.quad(func, 0., 1., args=(b, series_param_value), full_output=1, limit=100)
         result = output[0]
         if len(output)==4:
             warn = output[3]
@@ -121,43 +131,6 @@ def resummation_pade_borel(L, M, series_dict, a=0, b=0):
         return None, None
     #flag, result, error = integrate.qagiu(gfunc, 0, 1e-12, 1e-12, 100000, w)
     #return result
-
-
-gStar_05 = {1: 1, 2: 0.716173621, 3: 0.095042867, 4: 0.086080396, 5: -0.204139}
-gamma_minus_05 = {0: 1, 1: -1. / 3, 2: -0.113701246, 3: 0.024940678, 4: -0.039896059, 5: 0.0645212}
-nu_minus_05 = {0: 2, 1: -2. / 3, 2: -0.2613686, 3: 0.0145746, 4: -0.0913127, 5: 0.118121}
-
-from collections import namedtuple
-
-results2013 = namedtuple('results2013', ('gamma', 'gamma_minus', 'nu', 'nu_minus', 'eta'))
-#13
-#n=1
-n1 = results2013({0: 1, 1: 1. / 3, 2: 0.224812357, 3: 0.087897190, 4: 0.086443008, 5: -0.0180209},
-                 {0: 1, 1: -1. / 3, 2: -0.113701246, 3: 0.024940678, 4: -0.039896059, 5: 0.0645210},
-                 {0: 1./2, 1: 1. / 6, 2: 0.120897626, 3: 0.0584361287, 4: 0.056891652, 5: 0.00379868},
-                 {0: 2., 1: -2. / 3, 2: -0.261368281, 3: 0.0145750797, 4: -0.091312521, 5: 0.118121},
-                 {0: 0., 1: 0., 2: 0.0339661470, 3: 0.0466287623, 4: 0.030925471, 5: 0.0256843})
-
-
-#n=0
-n0 = results2013({0: 1, 1: 1. / 4, 2: 0.143242270, 3: 0.018272597, 4: 0.035251118, 5: -0.0634415},
-                 {0: 1, 1: -1. / 4, 2: -0.080742270, 3: 0.037723538, 4: -0.028548147, 5: 0.0754631},
-                 {0: 1./2, 1: 1. / 8, 2: 0.0787857831, 3: 0.0211750671, 4: 0.028101050, 5: -0.0222040},
-                 {0: 2., 1: -1. / 2, 2: -0.190143132, 3: 0.0416216976, 4: -0.071673308, 5: 0.136330},
-                 {0: 0., 1: 0., 2: 0.0286589366, 3: 0.0409908542, 4: 0.027138940, 5: 0.0236106})
-
-#n=-1
-nm1 = results2013({0: 1, 1: 1. / 7, 2: 0.060380873, 3: -0.023532210, 4: 0.012034268, 5: -0.0638772},
-                  {0: 1, 1: -1. / 7, 2: -0.039972710, 3: 0.03786436, 4: -0.018392201, 5: 0.0649966},
-                  {0: 1./2, 1: 1. / 14, 2: 0.0348693698, 3: -0.00424514372, 4: 0.011608435, 5: -0.0268913},
-                  {0: 2., 1: -2. / 7, 2: -0.0986611527, 3: 0.0510003794, 4: -0.049264800, 5: 0.116842},
-                  {0: 0., 1: 0., 2: 0.0187160402, 3: 0.0274103364, 4: 0.017144702, 5: 0.0159901})
-
-
-#b = 0
-#gStarBorel = borel_transform(gStar, b=0)
-#print gStarBorel
-
 
 
 
@@ -175,7 +148,7 @@ def print_pade(series_dict, N, m0=0, l0=0):
     print "Pade"
     print " "*10,
     for i in range(l0, N + 1):
-        print "%10d" % (i),
+        print "%10d" % i,
     print
     for M in range(m0, N + 1):
         if l0 < N - M + 1:
@@ -183,6 +156,7 @@ def print_pade(series_dict, N, m0=0, l0=0):
         for L in range(l0, N - M + 1):
             print "%10.4f" % (resummation_pade(L, M, series_dict)),
         print
+
 
 def print_pade_minus(series_dict, N, m0=0, l0=0):
     print "Pade"
@@ -217,8 +191,6 @@ def print_pade_borel(series_dict, N, m0=0, l0=0, inverse=False):
         if l0 < N - M + 1:
             print "%10d" % M,
         for L in range(l0, N - M + 1):
-        #        print M, L,
-            #FIXME : unknown exception
             try:
                 res, warn = resummation_pade_borel(L, M, series_dict)
             except:
@@ -282,6 +254,43 @@ def calculate2013(result, N, a=0, b=0):
 
 
 if __name__ == "__main__":
+
+
+    gStar_05 = {1: 1, 2: 0.716173621, 3: 0.095042867, 4: 0.086080396, 5: -0.204139}
+    gamma_minus_05 = {0: 1, 1: -1. / 3, 2: -0.113701246, 3: 0.024940678, 4: -0.039896059, 5: 0.0645212}
+    nu_minus_05 = {0: 2, 1: -2. / 3, 2: -0.2613686, 3: 0.0145746, 4: -0.0913127, 5: 0.118121}
+
+    #13
+    #n=1
+    n1 = results2013({0: 1, 1: 1. / 3, 2: 0.224812357, 3: 0.087897190, 4: 0.086443008, 5: -0.0180209},
+                     {0: 1, 1: -1. / 3, 2: -0.113701246, 3: 0.024940678, 4: -0.039896059, 5: 0.0645210},
+                     {0: 1./2, 1: 1. / 6, 2: 0.120897626, 3: 0.0584361287, 4: 0.056891652, 5: 0.00379868},
+                     {0: 2., 1: -2. / 3, 2: -0.261368281, 3: 0.0145750797, 4: -0.091312521, 5: 0.118121},
+                     {0: 0., 1: 0., 2: 0.0339661470, 3: 0.0466287623, 4: 0.030925471, 5: 0.0256843})
+
+
+    #n=0
+    n0 = results2013({0: 1, 1: 1. / 4, 2: 0.143242270, 3: 0.018272597, 4: 0.035251118, 5: -0.0634415},
+                     {0: 1, 1: -1. / 4, 2: -0.080742270, 3: 0.037723538, 4: -0.028548147, 5: 0.0754631},
+                     {0: 1./2, 1: 1. / 8, 2: 0.0787857831, 3: 0.0211750671, 4: 0.028101050, 5: -0.0222040},
+                     {0: 2., 1: -1. / 2, 2: -0.190143132, 3: 0.0416216976, 4: -0.071673308, 5: 0.136330},
+                     {0: 0., 1: 0., 2: 0.0286589366, 3: 0.0409908542, 4: 0.027138940, 5: 0.0236106})
+
+    #n=-1
+    nm1 = results2013({0: 1, 1: 1. / 7, 2: 0.060380873, 3: -0.023532210, 4: 0.012034268, 5: -0.0638772},
+                      {0: 1, 1: -1. / 7, 2: -0.039972710, 3: 0.03786436, 4: -0.018392201, 5: 0.0649966},
+                      {0: 1./2, 1: 1. / 14, 2: 0.0348693698, 3: -0.00424514372, 4: 0.011608435, 5: -0.0268913},
+                      {0: 2., 1: -2. / 7, 2: -0.0986611527, 3: 0.0510003794, 4: -0.049264800, 5: 0.116842},
+                      {0: 0., 1: 0., 2: 0.0187160402, 3: 0.0274103364, 4: 0.017144702, 5: 0.0159901})
+
+
+    #b = 0
+    #gStarBorel = borel_transform(gStar, b=0)
+    #print gStarBorel
+
+
+
+
     N = 5
 
     print "\ngStar\n"
