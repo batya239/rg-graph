22c3743ddbbfeff3e6f9508338c37a838a6b4596
Dmitry Batkovich <batya239@gmail.com>
2014-01-10 23:10:00 +0400

    edges automatically become external

--- packages/Phi4/examples/gexample.py
+++ packages/Phi4/examples/gexample.py
@@ -2,26 +2,11 @@
 # -*- coding: utf8
 __author__ = 'mkompan'
 
-import sys
-
-import gfunctions
-import graphine
 import graph_state
 
 
-gfunctions.graph_storage.initStorage(withFunctions=True)
-gfunctions.rprime_storage.initStorage()
+gs_builder = graph_state.PropertiesConfig.create(PropertyKey(name="fields",
+                                    is_directed=True,
+                                    externalizer=Fields.externalizer()))
 
-gs = graph_state.GraphState.fromStr('e112-23-e4-e55-e55--')
-graph = graphine.Graph.initEdgesColors(graphine.Graph(gs))
-
-gfunctions.GGraphReducer.setDebug(True)
-reducer = gfunctions.GGraphReducer(graph)
-
-reducer.calculate()
-print "result =", reducer.calculate()
-
-gfunctions.rprime_storage.closeStorage(revert=False, doCommit=True, commitMessage="example")
-gfunctions.graph_calculator.dispose()
-
-
+print gs_builder.graph_state_from_str("e11|ee|")
