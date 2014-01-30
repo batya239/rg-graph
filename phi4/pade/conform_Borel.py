83778549bdbb0cce18edaf1d54834c390c831d60
Yury Kirienko <yury.kirienko@gmail.com>
2014-01-31 00:29:14 +0400

    conform_Borel.py --  bugfixes

--- phi4/pade/conform_Borel.py
+++ phi4/pade/conform_Borel.py
@@ -12,25 +12,23 @@ eps = 0.5  # d = 3
 
 def func(t,a,b,k, eps):
     u = (sqrt(1+a*eps*t)-1)/(sqrt(1+a*eps*t)+1)
-    res = t**b*exp(-t) * u**k
+    res = t**b * exp(-t) * u**k
     return res
 
-def conformBorel(coeffs):
+def conformBorel(coeffs, eps):
     A = coeffs
-    n = len(A)
     #a, b = 0.238659217, 3.5 # -- for d = 2
     a, b = 0.14777422, 2.5 # -- for d = 3
-    B = [aa/gamma(k+b+1) for k, aa in enumerate(A)] ## образ Бореля-Лероя
+    B = [A[k]/gamma(k+b+1) for k in range(len(A))] ## образ Бореля-Лероя
     U = [B[0]] + \
-        [sum([b*(4/a)**(m+1)*binomial(k+m,k-m+1) for m,b in enumerate(B[1:k+1])]) \
-            for k in range(1,len(B))]
+        [sum([B[m] * (4/a)**m * binomial(k+m-1,k-m) for m in range(1,k+1)]) for k in range(1,len(B))]
     print "U =",U
     for k in range(len(U)):
-        print "%f ± %e"%(integrate.quad(func, 0., inf, args=(a, b, k, eps), limit=100))
+        print "k = %d:"%k,"%f ± %e"%(integrate.quad(func, 0., inf, args=(a, b, k, eps), limit=100))
     return [U[k]*integrate.quad(func, 0., inf, args=(a, b, k, eps), limit=100)[0] for k in range(len(U)) ]
 
 from Vladimirov_et_all_1984 import eta_n1
 
-A = conformBorel(eta_n1)
+A = conformBorel(eta_n1, eps)
 print "A(ϵ) =", " + ".join(map(lambda (k,x): str(x)+"×ϵ^"+str(k), enumerate(A)))
 print "eta =",sum([a*eps**k for k,a in enumerate(A)])
