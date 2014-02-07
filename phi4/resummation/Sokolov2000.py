381dbe4f2b5a89929ef95ffbbe2a6587cf93b476
Yury Kirienko <yury.kirienko@gmail.com>
2014-02-07 18:31:54 +0400

    Sokolov2000.py added -- data from Orlov & Sokolov paper (FTT, 5 loops in d=2)

---
+++ phi4/resummation/Sokolov2000.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf8
+
+__author__ = 'kirienko'
+
+""" Данные из работы Орлова и Соколова 2000 года:
+@article{Orlov2000,
+  title={Critical thermodynamics of two-dimensional systems in the five-loop renormalization-group approximation},
+  author={Orlov, EV and Sokolov, AI},
+  journal={Physics of the Solid State},
+  volume={42},
+  number={11},
+  pages={2151--2158},
+  year={2000},
+  publisher={Springer}
+}
+"""
+
+from sympy import symbols, poly
+
+n, g = symbols('n g')
+
+
+def beta_half(k):
+    _beta = -g + g ** 2 - g ** 3 / (n + 8) ** 2 * (10.33501055 * n + 47.67505273) \
+            + g ** 4 / (n + 8) ** 3 * (5.000275928 * n ** 2 + 149.1518586 * n + 524.3766023) \
+            - g ** 5 / (n + 8) ** 4 * (0.088842906 * n ** 3 + 179.6975910 * n ** 2 + 2611.154798 * n + 7591.108694) \
+            + g ** 6 / (n + 8) ** 5 * (
+    -0.00407946 * n ** 4 + 80.3096 * n ** 3 + 5253.56 * n ** 2 + 53218.6 * n + 133972)
+    beta_k = poly(_beta.subs(n, k).expand()).all_coeffs()
+    beta_k.reverse()
+    return beta_k
+
+
+def eta(k):
+    _eta = g ** 2 / (n + 8) ** 2 * (n + 2) * 0.9170859698 - g ** 3 / (n + 8) ** 2 * (n + 2) * 0.05460897758 \
+           + g ** 4 / (n + 8) ** 4 * (-0.0926844583 * n ** 3 + 4.05641051 * n ** 2 + 29.2511668 * n + 41.5352155) \
+           - g ** 5 / (n + 8) ** 5 * (0.0709196 * n ** 4 + 1.05240 * n ** 3 + 57.7615 * n ** 2 + 325.329 * n + 426.896)
+    eta_k = poly(_eta.subs(n, k).expand()).all_coeffs()
+    eta_k.reverse()
+    return eta_k
+
+
+if __name__ == "__main__":
+    print "for n = 1"
+    print "β(g)/2 =", beta_half(1)
+    print "η(g)/2 =", eta(1)
+
--- phi4/resummation/conform_Borel.py
+++ phi4/resummation/conform_Borel.py
@@ -7,6 +7,7 @@ from sympy import gamma, binomial
 from math import sqrt, exp
 from numpy import inf
 import scipy.integrate as integrate
+import os
 
 eps = 0.5  # d = 3
 #eps = 1.0  # d = 2
@@ -18,8 +19,8 @@ def func(t,a,b,k, eps):
 
 def conformBorel(coeffs, eps):
     A = coeffs
-    #a, b = 0.238659217, 3.5 # -- for d = 2
-    a, b = 0.14777422, 3.5 # -- for d = 3
+    a, b = 0.238659217, 3.5 # -- for d = 2
+    #a, b = 0.14777422, 3.5 # -- for d = 3
     B = [A[k]/gamma(k+b+1) for k in range(len(A))] ## образ Бореля-Лероя
     U = [B[0]] + \
         [sum([B[m] * (4/a)**m * binomial(k+m-1,k-m) for m in range(1,k+1)]) for k in range(1,len(B))]
@@ -28,10 +29,36 @@ def conformBorel(coeffs, eps):
     #    print "k = %d:"%k,"%f ± %e"%(integrate.quad(func, 0., inf, args=(a, b, k, eps), limit=100))
     return [U[k]*integrate.quad(func, 0., inf, args=(a, b, k, eps), limit=100)[0] for k in range(len(U)) ]
 
-import Vladimirov_et_all_1984, Kleinert_book
+
+if __name__ == "__main__":
+    import Vladimirov_et_all_1984, Kleinert_book
 
 
-print "Vladimirov:\teta =", sum(conformBorel(Vladimirov_et_all_1984.eta(1), eps))
-print "Kleinert:\teta =", sum(conformBorel(Kleinert_book.eta(1), eps))
-#print conformBorel(Kleinert_book.eta(1), eps)
-#print "\nTest (Kleinert 17.16):\teta =", sum(conformBorel([0, 0, 0.0185, 0.0187, -0.0083, 0.0257], eps * 2))
+    print "Vladimirov:\teta =", sum(conformBorel(Vladimirov_et_all_1984.eta(1), eps))
+    print "Kleinert:\teta =", sum(conformBorel(Kleinert_book.eta(1), eps))
+    #print conformBorel(Kleinert_book.eta(1), eps)
+    #print "\nTest (Kleinert 17.16):\teta =", sum(conformBorel([0, 0, 0.0185, 0.0187, -0.0083, 0.0257], eps * 2))
+
+    L = 6
+    beta = [0, -1.0, 1.0, -0.71617362, + 0.930764, -1.582398, 3.260219]
+    eta_g = [0., 0., 0.033966148, -0.00202253, 0.01139321, -0.0137366, 0.028233, -1.466e-05]#, 0.000202430]
+    beta = beta[:L + 1]
+    eta_g = eta_g[:L + 2]
+    print "beta =", beta
+    gStar = 1.75
+    delta = 0.01
+    for i in range(1000):
+        g1 = sum(conformBorel(beta, gStar - delta))
+        g2 = sum(conformBorel(beta, gStar + delta))
+        print "β(%.2f) = %.4f, β(%.2f) = %.4f" % (gStar - delta, g1, gStar + delta, g2)
+        if g1 - g2 > 0:
+            gStar -= delta
+        else:
+            gStar += delta
+        if g1 * g2 < 0:
+            break
+    print "g* =", gStar
+
+    print "eta(g*) =", sum(conformBorel(eta_g, gStar))
+    print len(eta_g), "eta(g) =", eta_g
+    print len(beta), "beta =", beta
