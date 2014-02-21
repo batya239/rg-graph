a14967225ec301443a265c1794ebf8d0742016d7
Mikhail Kompaniets <mkompan@gmail.com>
2014-02-21 17:21:17 +0400

    sd6l

--- phi4/graphs/sd6loop_results.py
+++ phi4/graphs/sd6loop_results.py
@@ -4,16 +4,43 @@ import os
 import re
 
 from rggraphenv.symbolic_functions import e, tgamma, series
-
+from graph_state_builder_static import gs_builder
 import graphine
 import sys
 
-graph = graphine.Graph.fromStr(sys.argv[1])
+
+def C4(lambdas_, n):
+    if isinstance(lambdas_, int):
+        lambdas = [1]*lambdas_
+    else:
+        lambdas = lambdas_
+    res = tgamma(sum(lambdas) - 2 * n + n * e) * tgamma(2 - 2 * e) ** n / tgamma(e) ** n / tgamma(1 - e) ** (2 * n) / e ** n
+    for lambd in lambdas:
+        if lambd != 0:
+            res = res / tgamma(lambd)
+    return res
+
+
+def C6(lambdas_, n):
+    if isinstance(lambdas_, int):
+        lambdas = [1]*lambdas_
+    else:
+        lambdas = lambdas_
+
+    res = tgamma(sum(lambdas) - 3 * n + n * e) * tgamma(4 - 2 * e) ** n / tgamma(e - 1) ** n / tgamma(2 - e) ** (2 * n) / (-6 * e) ** n
+    for lambd in lambdas:
+        if lambd != 0:
+            res = res / tgamma(lambd)
+    return res
+
+Cn = C4
+
+graph = graphine.Graph(gs_builder.graph_state_from_str(sys.argv[1]))
 
 loops = graph.getLoopsCount()
 alpha = len(graph.internalEdges())
 
-graph_dir = "sample_extraction/%s" % graph
+graph_dir = "sd6loop/%s" % graph
 
 _FILENAME_REGEXP = re.compile(".*_(\d+)_V(\d+)_E(.+)\.run.res")
 _EXPR_REGEXP = re.compile(".* =(.*)$")
@@ -39,23 +66,17 @@ for result_file in result_files:
 
 print result
 #print error
-g11 = tgamma(e) * tgamma(-e + 1) ** 2 / tgamma(-2 * e + 2)
+
 expr = reduce(lambda x, y: x + y, map(lambda x: x[1] * e ** x[0], result.items()))
 #print series(expr*tgamma(alpha - loops*(1-e))/(e*g11)**loops,e,0,max_eps_power).evalf()
 if alpha - loops * 2 <= 0:
     max_eps_power -= 1
 print (alpha - loops * (2 - e)).expand()
-final = series(expr * tgamma(alpha - loops * (2 - e)) / (e * g11) ** loops, e, 0, max_eps_power + 1).evalf()
+final = series(expr * Cn(alpha,loops), e, 0, max_eps_power + 1).evalf()
 print final
 
 results = {
-    'e11-e-::': 1 / e,
-    'e112|2|e|::': 0.5 * e ** (-2) + 0.5 * e ** (-1) + 1.5 + 0.8938292905212171433 * e + 5.0233747388210952802 * e ** 2 + 3.0355504618527611193 * e ** 3 + 19.881646498182317079 * e ** 4+12.096918018576493531*e**5+ 79.53720161336304073*e**6,
-    'e112-e2--::': (-0.5)*e**(-2)+0.5*e**(-1)+(-0.5000000000000000001)+2.1061707094787828563*e+(-3.2357161577786609941)*e**2+7.0111990157894294494*e**3+(-13.8105455744767948445)*e**4+27.666374977788140557*e**5+(-55.34336557621005371)*e**6,
-    'e12-23-3-e-::': 7.2123414189575657126 - (4.683773734514887159) * e - (21.549990225248099193) * e ** 3 + (24.069147509221048373) * e ** 2,
-    'e12-223-3-e-::': 3.276265548078106665+0.3333333333333333333/e**3+0.3333333333333333333/e**2+0.3333333333333333333/e-9.14747181594392599*e+38.35479100795094758* e**2-121.8709485789786307*e**3,
-    'e12-e23-33--::': (-0.033333333333333333335)*e**(-2)+(-0.10555555555555555556)*e**(-1)+(-0.08888888888888888884)+0.084855825076363301824*e+0.26377745322552894935*e**2+0.29878072202252724067*e**3+0.21433389572441654735*e**4+0.10929928599756573176*e**5+0.03901335072699244663*e**6,
-    'e12-23-34-4-e-::': 20.7385551028674 - 37.866830505981674 * e
+
 }
 if str(graph) in results:
     print
