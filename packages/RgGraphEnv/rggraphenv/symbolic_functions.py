c4d5443bca403a6e3c05ac59f410bff6009bd64b
dima <dima@Dimas-MacBook-Air.local>
2014-03-08 13:12:41 +0400

    cleanup

--- packages/RgGraphEnv/rggraphenv/symbolic_functions.py
+++ packages/RgGraphEnv/rggraphenv/symbolic_functions.py
@@ -45,7 +45,8 @@ log = swiginac.log
 Order = swiginac.Order
 O = Order
 
-D = swiginac.numeric("4") - swiginac.numeric("2") * e
+d_phi4 = cln(4) - cln(2) * e
+d_percolation = cln(4) - e
 
 p2 = p ** 2
 
@@ -56,7 +57,7 @@ CLN_ONE = swiginac.numeric("1")
 CLN_MINUS_ONE = swiginac.numeric("-1")
 CLN_TWO = swiginac.numeric("2")
 
-l = D / CLN_TWO - CLN_ONE
+l = d_phi4 / CLN_TWO - CLN_ONE
 
 def series(expression, x, x0, n, remove_order=False):
     res = expression.series(x == x0, n)
@@ -97,19 +98,19 @@ def pole_part(expr):
 
 
 #noinspection PyPep8Naming
-def G(alpha, beta, d=D):
+def G(alpha, beta, d=d_phi4):
     if alpha == 1 and beta == 1:
         return (CLN_ONE / e) * _get_raw_g_pole(d=d)
     return _raw_g(alpha, beta, d=d) / (_g11(d=d)) * _get_raw_g_pole(d=d)
 
 
 #noinspection PyPep8Naming
-def G1(alpha, beta, d=D):
+def G1(alpha, beta, d=d_phi4):
     return (G(alpha, beta, d=d) + G(alpha - CLN_ONE, beta, d=d) - G(alpha, beta - CLN_ONE, d=d))/CLN_TWO
 
 
 #noinspection PyPep8Naming
-def G2(alpha, beta, d=D):
+def G2(alpha, beta, d=d_phi4):
     return (G(alpha, beta, d=d) - G(alpha - CLN_ONE, beta, d=d) - G(alpha, beta - CLN_ONE, d=d))/CLN_TWO
 
 
@@ -128,7 +129,7 @@ class IsEqualWrapper(object):
         return self.underlying.is_equal(other.underlying)
 
 
-def _get_raw_g_pole(d=D):
+def _get_raw_g_pole(d=d_phi4):
     wrapper = IsEqualWrapper(d)
     pole = _RAW_G_POLE.get(wrapper)
     if pole is None:
@@ -137,7 +138,7 @@ def _get_raw_g_pole(d=D):
     return pole
 
 
-def _raw_g(alpha, beta, d=D):
+def _raw_g(alpha, beta, d=d_phi4):
     #noinspection PyUnresolvedReferences
     if (alpha + zero).is_equal(zero) or (beta + zero).is_equal(zero) \
         or (CLN_TWO * (d/CLN_TWO - CLN_ONE) + CLN_TWO - alpha - beta + zero).is_equal(zero):
@@ -146,7 +147,7 @@ def _raw_g(alpha, beta, d=D):
            / (tgamma(alpha) * tgamma(beta) * tgamma(d - alpha - beta))
 
 
-def _g11(d=D):
+def _g11(d=d_phi4):
     wrapper = IsEqualWrapper(d)
     g11 = _G_11.get(wrapper, None)
     if g11 is None:
