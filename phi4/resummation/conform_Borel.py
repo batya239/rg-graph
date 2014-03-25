b2e5788d1d4114e5be209a6e3db84b8678806236
Yury Kirienko <yury.kirienko@gmail.com>
2014-03-25 22:32:26 +0400

    some improvements in conformBorel

--- phi4/resummation/conform_Borel.py
+++ phi4/resummation/conform_Borel.py
@@ -18,10 +18,10 @@ def func(t,a,b,k, eps):
     """
     Образ Бореля
     @param t:
-    @param a:
-    @param b:
-    @param k:
-    @param eps:
+    @param a: из АВП
+    @param b: из АВП + что-то
+    @param k: номер члена
+    @param eps: заряд!
     @return:
     """
     u = (sqrt(1+a*eps*t)-1)/(sqrt(1+a*eps*t)+1)
@@ -29,39 +29,38 @@ def func(t,a,b,k, eps):
     return res
 
 
-def conformBorel(coeffs, b = 2, loops = 6, dim = 2):
+def conformBorel(coeffs, eps, b = 2, loops = 6, n = 1, dim = 2):
     A = coeffs
-    print "Initial coeffs:", A
+    # print "Initial coeffs:", A
     if dim is 2:
         a, b = 0.238659217,b # -- for d = 2
-        eps = 1.0  # d = 2
     elif dim is 3:
         a, b = 0.14777422, b # -- for d = 3
-        eps = 0.5  # d = 3
     else:
-        raise "Dimension must be either 2 or 3"
+        raise Exception("Dimension must be either 2 or 3")
 
-    n = 1 ## <-- какую степень заряда выносить
-    L = loops+1
-    A = A[n:L]
+    nulls = 0 ## <-- количество начальных нулевых коэффициентов
+    for i in A:
+        if i == 0: nulls += 1
+        else: break
+    n = n ## <-- какую степень заряда выносить
+    L = loops + nulls - n - 1
+    A = A[n:]
 
-    print "A =", A, " len(A)=%d, L=%d"%(len(A),L)
-    B = [A[k]/gamma(k+b+1) for k in range(len(A))] ## образ Бореля-Лероя
-    # B = [A[k]/gamma(k+b+1) for k in range(L)] ## образ Бореля-Лероя
-    print "B =",B, " len(B)=%d, L=%d"%(len(B),L)
-    # U = [sum([B[m] * (4/a)**m * binomial(k+m-1,k-m) for m in range(1,k+1)]) for k in range(1,len(B))]
-    U = [sum([B[m] * (4/a)**m * binomial(k+m-1,k-m) for m in range(len(B))]) for k in range(len(B))]
-    print "U =",U, " len(U)=%d, L=%d"%(len(U),L)
-    # return [U[k]*integrate.quad(func, 0., np.inf, args=(a, b, k, eps), limit=100)[0] for k in range(len(U)) ]
-    return [eps**n*U[k]*integrate.quad(func, 0., np.inf, args=(a, b, k, eps), limit=100)[0] for k in range(len(U)) ]
+    # print "A =", A, " len(A)=%d, L=%d"%(len(A),L)
+    B = [A[k]/gamma(k+b+1) for k in range(L)] ## образ Бореля-Лероя
+    # print "B =",B, " len(B)=%d, L=%d"%(len(B),L)
+    U = [sum([B[m] * (4/a)**m * binomial(k+m-1,k-m) for m in range(L)]) for k in range(L)]
+    # print "U =",U, " len(U)=%d, L=%d"%(len(U),L)
+    return [eps**n*U[k]*integrate.quad(func, 0., np.inf, args=(a, b, k, eps), limit=100)[0] for k in range(L) ]
 
 def findZero(beta_half, gStar = 1.75, delta = 0.01):
     _gStar = gStar
-    print "β/2 =", beta_half
+    # print "β/2 =", beta_half
     for i in range(1000):
-        g1 = sum(conformBorel(beta_half, _gStar - delta))
-        g2 = sum(conformBorel(beta_half, _gStar + delta))
-        print "β(%.2f) = %.4f, β(%.2f) = %.4f" % (_gStar - delta, g1, _gStar + delta, g2)
+        g1 = sum(conformBorel(beta_half, _gStar - delta, loops = len(beta_half)-1, n = 1))
+        g2 = sum(conformBorel(beta_half, _gStar + delta, loops = len(beta_half)-1, n = 1))
+        # print "β(%.2f) = %.4f, β(%.2f) = %.4f" % (_gStar - delta, g1, _gStar + delta, g2)
         if abs(g1) > abs(g2):
             _gStar += delta
         else:
@@ -160,14 +159,14 @@ if __name__ == "__main__":
     beta = map(lambda x: x.n, beta.gSeries.values())
     eta_g= map(lambda x: x.n,eta_g.gSeries.values())
     #beta_half = [0, -1.0, 1.0, -0.71617362, + 0.930764, -1.582398, 3.260219] ## NB: in fact it's beta/2
-    #eta_g = [0., 0., 0.033966148, -0.00202253, 0.01139321, -0.0137366, 0.028233] = [0, -1.0, 1.0, -0.71617362, + 0.930764, -1.582398, 3.260219] ## NB: in fact it's beta/2
+    # eta_g = [0., 0., 0.033966148, -0.00202253, 0.01139321, -0.0137366, 0.028233]
     #Z2 = [1, 0, -0.0084915370, -0.005323936, -0.002340342, -0.00135597, -0.0003502]
     #Z3 = [1, 1.0, 0.624930113, 0.4470878, 0.1735522, 0.283165]
 
     beta_half = [b/2 for b in beta[:L4+2]]
     eta_g = eta_g[:L2 + 1]
-    # gStar = findZero(beta_half)
-    # print "g* =", gStar
+    gStar = findZero(beta_half)
+    print "g* =", gStar
 
     print "η(g*):\n", sum(conformBorel(eta_g, 1.88))
     # print "η(g*) =", sum(conformBorel(eta_g, gStar))
--- phi4/resummation/zinn.py
+++ phi4/resummation/zinn.py
@@ -3,10 +3,7 @@
 
 __author__ = 'kirienko'
 
-from sympy import gamma, binomial
-from math import sqrt
 import numpy as np # inf, exp, array, arange
-import scipy.integrate as integrate
 from scipy.optimize import curve_fit
 from uncertSeries import Series
 import matplotlib.pyplot as plt
@@ -14,20 +11,6 @@ from conform_Borel import conformBorel, findZero
 
 # eps = 0.5  # d = 3
 eps = 1.0  # d = 2
-
-def func(t,a,b,k, eps):
-    """
-    Образ Бореля
-    @param t:
-    @param a:
-    @param b:
-    @param k:
-    @param eps:
-    @return:
-    """
-    u = (sqrt(1+a*eps*t)-1)/(sqrt(1+a*eps*t)+1)
-    res = t ** b * np.exp(-t) * u ** (k)
-    return res
 
 def fit_exp(x, a, b, c):
     #print "a = %f, b = %f, c = %f, x_0 = %f" %(a,b,c, x_0)
@@ -50,7 +33,6 @@ def plot(coeffs, beta_half, name, fileName):
     plt.clf()
     b_0 = 0
     n = len(coeffs)
-    print "XXXX"
     L = range(2,n)
     coeffs_by_loops = [coeffs[:k+1] for k in L]
     plots = []
@@ -59,20 +41,20 @@ def plot(coeffs, beta_half, name, fileName):
         gStar_by_loops.append(gStar_by_loops[-1])
 
     for i in range(4): ## loop over b: b=b_0+i
-        points = [sum(conformBorel(c,gStar_by_loops[j],b=b_0+i)) for j,c in enumerate(coeffs_by_loops)]
-        #points = [sum(conformBorel(c,1.75,b=b_0+i)) for j,c in enumerate(coeffs_by_loops)]
-        print "i = %d, g* = %f, \t"%(i,gStar),points
-        #plots.append(plt.plot(L, points, 'o-', label = 'b = %.1f'%(b_0+i)))
+        points = [sum(conformBorel(c,gStar_by_loops[j],loops = len(c)-1,b=b_0+i)) for j,c in enumerate(coeffs_by_loops)]
+        # points = [sum(conformBorel(c,1.88,loops = len(c)-1,b=b_0+i)) for j,c in enumerate(coeffs_by_loops)]
+        print "i = %d, g* = %f, \t"%(i,1.88),points
+        plots.append(plt.plot(L, points, 'o-', label = 'b = %.1f'%(b_0+i)))
         xn, yn = np.array(L),np.array(points, dtype = 'float32')
         x = np.arange(2,10,0.1)
-        popt, pcov = curve_fit(fit_exp, xn, yn)
-        print "approximation: %f*exp(-%f*(x+(%f))) + %f"%(popt[0],popt[1],popt[3],popt[2])
-        plt.plot(x, fit_exp(x, *popt), 'r-', label="Fitted Curve")
+        # popt, pcov = curve_fit(fit_exp, xn, yn)
+        # print "approximation: %f*exp(-%f*x) + %f"%(popt[0],popt[1],popt[2])
+        # plt.plot(x, f1it_exp(x, *popt), 'r-', label="Fitted Curve")
         #plots.append(plt.plot(L, points, 'o-', label = 'g* = %.1f'%(gStar+0.1*i)))
     title = name
     plt.title(title, fontdict = font)
-    #plt.legend(loc = "upper left")
-    plt.text(L[-2], points[0],'g* =%.2f'%gStar, fontdict = font)
+    plt.legend(loc = "upper left")
+    # plt.text(L[-2], points[0],'g* =%.2f'%gStar, fontdict = font)
     plt.grid(True)
     plt.xticks(L)
     plt.xlabel('Number of loops')
@@ -115,12 +97,6 @@ def plotBeta(beta_half, name, fileName):
 
 
 if __name__ == "__main__":
-    #import Vladimirov_et_all_1984, Kleinert_book
-    #print "Vladimirov:\teta =", sum(conformBorel(Vladimirov_et_all_1984.eta(1), eps))
-    #print "Kleinert:\teta =", sum(conformBorel(Kleinert_book.eta(1), eps))
-    #print conformBorel(Kleinert_book.eta(1), eps)
-    #print "\nTest (Kleinert 17.16):\teta =", sum(conformBorel([0, 0, 0.0185, 0.0187, -0.0083, 0.0257], eps * 2))
-
     L2, L4 = 6, 5
     N = 1 #-1, 0, 1
     Z2   = eval(open('Z2.txt').read())
@@ -130,20 +106,21 @@ if __name__ == "__main__":
 
     beta = map(lambda x: x.n, beta.gSeries.values())
     eta_g= map(lambda x: x.n,eta_g.gSeries.values())
-    #beta_half = [0, -1.0, 1.0, -0.71617362, + 0.930764, -1.582398, 3.260219] ## NB: in fact it's beta/2
-    #eta_g = [0., 0., 0.033966148, -0.00202253, 0.01139321, -0.0137366, 0.028233] = [0, -1.0, 1.0, -0.71617362, + 0.930764, -1.582398, 3.260219] ## NB: in fact it's beta/2
+    beta_half = [0, -1.0, 1.0, -0.71617362, + 0.930764, -1.582398, 3.260219] ## NB: in fact it's beta/2
+    eta_g = [0., 0., 0.033966148, -0.00202253, 0.01139321, -0.0137366, 0.028233]
+
     ### For d=3, from Nickel, 1978
-    # beta_half = [0.,-1., 1., -0.4224965707, 0.3510695978, -0.3765268283, 0.49554795, -0.749689]
-    # eta_g = [0.,0., 0.0109739369, 0.0009142223, 0.0017962229, -0.00065370, 0.00138781, -0.0016977]
+    # beta_half = [0.,-1., 1., -0.4224965707, 0.3510695978, -0.3765268283, 0.49554795, -0.749689] ## d = 3
+    # eta_g = [0.,0., 0.0109739369, 0.0009142223, 0.0017962229, -0.00065370, 0.00138781, -0.0016977] ## d = 3
     #Z2 = [1, 0, -0.0084915370, -0.005323936, -0.002340342, -0.00135597, -0.0003502]
     #Z3 = [1, 1.0, 0.624930113, 0.4470878, 0.1735522, 0.283165]
 
-    beta_half = [b/2 for b in beta[:L4+2]]
-    gStar = findZero(beta_half)
-    print "g* =", gStar
+    # beta_half = [b/2 for b in beta[:L4+2]]
+    # gStar = findZero(beta_half)
+    # print "g* =", gStar
 
-    eta_g = eta_g[:L2 + 1]
-    print "η(g*) =", sum(conformBorel(eta_g, gStar,b=2,loops=L2))
+    # eta_g = eta_g[:L2 + 1]
+    # print "η(g*) =", sum(conformBorel(eta_g, gStar,b=2,loops=L2))
     # print "η(g*) =", [sum(conformBorel(eta_g, 1.4,b=0,loops=l)) for l in [1,2,3,4,5,6]]
     #print len(beta_half), "β(g)/2 =", beta_half
     #print len(eta_g), "η(g)/2 =", eta_g
