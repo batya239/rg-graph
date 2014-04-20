79513555c859862bcc0ba94d9efc6dd81843c28e
Gleb Dovzhenko <dovjenko.g@gmail.com>
2014-04-20 12:02:16 +0400

    trying to get more precision in 4 loops

--- packages/PoleExtractor/c4l_02.py
+++ packages/PoleExtractor/c4l_02.py
@@ -2,14 +2,14 @@ __author__ = 'Gleb Dovzhenko <dovjenko.g@gmail.com>'
 
 from pole_extractor import diagram_calculator, utils
 
-need_p2 = utils.get_diagrams(tails=2, loops=4)
+need_p0 = utils.get_diagrams(tails=3, loops=4)
 
-for i, (d, c) in reversed(list(enumerate(need_p2))):
-    v = diagram_calculator.get_expansion(d, False, True)[-1]
-    if abs(v.s) / abs(v.n) > 1E-3:
-        print '(' + str(i + 1) + '/' + str(len(need_p2)) + ')'
+for i, (d, c) in list(enumerate(need_p0))[1:-1:2]:
+    v = diagram_calculator.get_expansion(d, False, False)[-1]
+    if abs(v.s) / abs(v.n) > 1E-4:
+        print '(' + str(i + 1) + '/' + str(len(need_p0)) + ')'
         diagram_calculator.calculate_diagram(label=d,
                                              theory=3,
                                              max_eps=-1,
-                                             zero_momenta=False,
+                                             zero_momenta=True,
                                              force_update=False)
--- packages/PoleExtractor/calc_4_loops.py
+++ packages/PoleExtractor/calc_4_loops.py
@@ -8,12 +8,13 @@ from pole_extractor import diagram_calculator, utils
 # so there must be a check that we have every diagram we need for R'
 
 need_p2 = utils.get_diagrams(tails=2, loops=4)
-need_p0 = need_p2 + utils.get_diagrams(tails=3, loops=4)
-"""
-for i, (d, c) in enumerate(need_p0):
-    #v = diagram_calculator.get_expansion(d, False, False)[-1]
-    #if abs(v.s) / abs(v.n) > 1E-3:
-    if not diagram_calculator.is_present(d, rprime=False, momentum_derivative=False):
+#need_p0 = need_p2 + utils.get_diagrams(tails=3, loops=4)
+need_p0 = utils.get_diagrams(tails=3, loops=4)
+
+for i, (d, c) in reversed(list(enumerate(need_p0))[:-1]):
+    v = diagram_calculator.get_expansion(d, False, False)[-1]
+    if abs(v.s) / abs(v.n) > 1E-4:
+    #if not diagram_calculator.is_present(d, rprime=False, momentum_derivative=False):
         print '(' + str(i + 1) + '/' + str(len(need_p0)) + ')'
         diagram_calculator.calculate_diagram(label=d,
                                              theory=3,
@@ -33,7 +34,7 @@ for i, (d, c) in enumerate(need_p2):
                                              zero_momenta=False,
                                              force_update=False)
         #print (d, diagram_calculator.get_expansion(d, rprime=False, momentum_derivative=True).to_base_dict())
-
+"""
 for diag in need_p0:
     diagram_calculator.calculate_rprime(diag[0],
                                         PHI_EXPONENT=3,
