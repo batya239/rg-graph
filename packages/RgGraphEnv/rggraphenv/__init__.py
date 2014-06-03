ac6fd9527993294a556bc52c0d4ec173b738107c
Dmitry Batkovich <batya239@gmail.com>
2014-06-03 23:57:36 +0400

    mongo storage added to __init__.py

--- packages/RgGraphEnv/rggraphenv/__init__.py
+++ packages/RgGraphEnv/rggraphenv/__init__.py
@@ -11,6 +11,10 @@ from storage import StorageSettings, StoragesHolder
 from graph_calculator import GraphCalculatorManager
 from abstract_graph_calculator import AbstractGraphCalculator
 from g_graph_calculator import GLoopCalculator
+try:
+    from mongo_storage import MongoClientWrapper, GraphIdExtractor, StrGraphIdExtractor
+except ImportError:
+    pass
 
 __author__ = 'daddy-bear'
 
