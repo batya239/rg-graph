7b1fe58bdfd47c3d73f562ba681c951b0c3bc306
Yury Kirienko <yury.kirienko@gmail.com>
2014-07-16 01:30:42 +0400

    uncertSeries package (stub)

---
+++ packages/UncertSeries/README.txt
@@ -0,0 +1,10 @@
+
+Description
+===========
+
+
+
+Examples
+========
+
+
---
+++ packages/UncertSeries/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf8
+from distutils.core import setup
+
+setup(
+    name='UncertSeries',
+    version='0.0.1',
+    author='Kirienko',
+    author_email='yury.kirienko@gmail.com',
+    packages=[],
+    url='',
+    license='LICENSE.txt',
+    description='Series with uncertain coefficients',
+    long_description=open('README.txt').read(),
+    requires=['uncertainties (>= 2.4.0)', 'sympy (>= 0.7.5)', 'graphine', 'graph_state']
+)
