42051c95393afffacb79ff4a25a255530abdbbd9
Dmitry Batkovich <batya239@gmail.com>
2014-10-20 19:48:43 +0400

    rstar package setup

--- packages/RStar/setup.py
+++ packages/RStar/setup.py
@@ -7,10 +7,10 @@ setup(
     version='0.0.1',
     author='D. Batkovich',
     author_email='batya239@gmail.com',
-    packages=['rstar'],
-    url='',
+    packages=['rstar', 'rstar.test'],
+    url='http://pypi.python.org/pypi/RStar/',
     license='LICENSE.txt',
-    description='RStar',
+    description='Calculating diagrams using R* operation',
     long_description=open('README.txt').read(),
-    requires=['RgGraphUtil (>= 0.0.1)']
+#    requires=['GraphState (>= 0.0.3), Graphine (>= 0.0.1), RgGraphUtil(>= 0.0.1)', sympy]
 )
