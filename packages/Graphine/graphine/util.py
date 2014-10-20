51734eb27a15c6409411a0431d47bdbddcdb3fd4
Dmitry Batkovich <batya239@gmail.com>
2014-10-20 19:46:18 +0400

    bfs improved to arbitrary given lines

--- packages/Graphine/graphine/util.py
+++ packages/Graphine/graphine/util.py
@@ -4,11 +4,10 @@ import graph_state
 import graph
 
 
-def find_shortest_momentum_flow(graph):
-    assert graph.external_edges_count == 2
+def find_shortest_momentum_flow(graph, _from=None, to=None):
     external_edges = graph.edges(graph.external_vertex)
-    start_vertex = external_edges[0].internal_nodes[0]
-    target_vertex = external_edges[1].internal_nodes[0]
+    start_vertex = external_edges[0].internal_nodes[0] if _from is None else _from.internal_node
+    target_vertex = external_edges[1].internal_nodes[0] if to is None else to.internal_node
     queue = [start_vertex]
     distances = {start_vertex: ()}
     while len(queue):
@@ -16,7 +15,7 @@ def find_shortest_momentum_flow(graph):
         del queue[0]
         dist = distances.get(vertex)
         for e in graph.edges(vertex):
-            if len(e.internal_nodes) == 1:
+            if e.is_external():
                 continue
             v = e.nodes[1 if e.nodes[0] == vertex else 0]
             e_dist = distances.get(v)
