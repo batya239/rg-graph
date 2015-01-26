d06c3fb5119c035535a49620c89e66267aa88847
Yury Kirienko <yury.kirienko@gmail.com>
2015-01-26 14:19:32 +0400

    phi3/d_to_infty

---
+++ phi3/d_to_infty/spine.py
@@ -0,0 +1,116 @@
+#! /usr/bin/python
+#! encoding: utf8
+
+__author__ = "kirienko"
+
+#from graph_state_builder_with_fields import gs_builder
+
+import graph_state as gs
+import networkx as nx
+import itertools as it
+#import pygraphviz as pgv
+import matplotlib.pyplot as plt
+from copy import deepcopy
+
+def draw_nx_graph(G,spines,nom):
+    """
+    Buggy in case of MultiGraph()
+    """
+    #plt.xkcd() ## WOW!!!!
+    n = len(spines)
+    region = 230
+    pos = None
+    for i in range(n):
+        region += 1
+        plt.subplot(region)
+        plt.title("number %d"%(i))
+        new_G = nx.MultiGraph()
+        new_G.add_path(spines[i][0],color = 'blue', weight = 2)
+        new_G.add_path(spines[i][1],color = 'red',  weight = 2)
+        rest_edges = [e for e in G.edges() if e not in new_G.edges()]
+        new_G.add_edges_from(rest_edges,color = 'black',weight=1)
+        edges = new_G.edges()
+        colors = [new_G[u][v][0]['color'] for u,v in edges]
+        weights = [new_G[u][v][0]['weight'] for u,v in edges]
+        if not pos:
+            pos=nx.spring_layout(new_G)
+        nx.draw(new_G,pos,node_size=1, edge_color=colors, width=weights)
+    #plt.show()
+    plt.savefig('%s.png'%nom)
+    plt.close('all')
+
+def draw_Agraph(G,spines,nom):
+    n = len(spines)
+    region = 230
+    pos = None
+    for i in range(n):
+        new_G = nx.MultiGraph()
+        new_G.add_path(spines[i][0],color = 'blue', weight = 2)
+        new_G.add_path(spines[i][1],color = 'red',  weight = 2)
+        rest_edges = [e for e in G.edges() if e not in new_G.edges()]
+        new_G.add_edges_from(rest_edges,color = 'black',weight=1)
+        a = nx.to_agraph(new_G)
+        a.layout(prog='circo')
+        #a.node_attr.update(shape="circle")
+        a.draw('%s_%d.png'%(nom,i))
+
+def nx_graph_from_str(nickel_str):
+    """
+    Returns networkx MultiGraph object from Nickel string
+    """
+    gs_diag = static_diag(nickel_str)
+    edges = [e.nodes for e in gs_diag.edges]
+    ext = [e for e in edges if -1 in e]
+    ext_index = lambda x: 1-x.index(-1) 
+
+    for i,e in enumerate(ext):
+        edges.remove(e)
+        edges.append((e[ext_index(e)],'e'+str(i)))
+    #print "edges", edges
+
+    g = nx.MultiGraph()
+    g.add_edges_from(edges)
+    #nx.write_dot(g,'multi.dot')
+    return g
+
+def static_diag(diag_from_str):
+    sc = gs.PropertiesConfig.create()
+    return sc.graph_state_from_str(diag_from_str)
+
+def spine(G,source,sink):
+    """
+    Returns spine pairs array for certain pair (source,sink)
+        in networkx graph G
+    """
+    spine_pairs = []
+    all_paths = [p for p in nx.all_simple_paths(G,source,sink)]
+    for s in it.combinations(all_paths,2):
+        if set(s[0][1:-1]).intersection(set(s[1][1:-1])) == set():
+            spine_pairs.append(s)
+    return spine_pairs
+
+if  __name__ == "__main__":
+    with open("e2-1loop.txt.gs") as fd:
+        diags = [d.strip() for d in fd.readlines()]
+    for d in diags:
+        print "\n",d
+        G = nx_graph_from_str(d)
+        source = 0
+        sink = max([g for g in G.edges() if type(g[1]) == str])[0]
+        spine_pairs = spine(G,source,sink)
+        #print "Spine pairs:",spine_pairs
+        #draw_nx_graph(G,spine_pairs,d.replace('|','-'))
+        #draw_Agraph(G,spine_pairs,d.replace('|','-'))
+        spine,half_spine = spine_pairs[0]
+        print "Spine pairs",spine_pairs[0]
+        internal_nodes = [g for g in G.nodes() if type(g) != str]
+        NN = []
+        for v in internal_nodes:
+            print "Node",v
+            print "\tNeighbours:",G[v].keys()
+            for u in G[v].keys():
+                if sorted([v,u]) not in spine:
+                    nG = deepcopy(G)
+                    nG[v][u]['dot'] = v
+                    NN.append(nG)
+        print "len(NN)",len(NN)
