52f174453cb36eb3f0db7f958f3b4831b3a6e17c
Dmitry Batkovich <batya239@gmail.com>
2015-01-26 21:24:43 +0300

    some

---
+++ packages/Graphine/examples/percolation_generator_example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf8
+
+__author__ = 'dima'
+
+import dynamic_diagram_generator
+import time
+
+t = time.time()
+for g in dynamic_diagram_generator.generate("e12|e3|33||", possible_fields=["aA"], possible_external_fields="Aa", possible_vertices=["aaA", "aAA"]):
+    print g
+print time.time() - t
