1f84b385df29b5a604e60cc184567e93b806b805
Dmitry Batkovich <batya239@gmail.com>
2015-01-26 13:08:59 +0300

    added split_edges_for_node function in operations_lib

--- packages/GraphState/graph_state/operations_lib.py
+++ packages/GraphState/graph_state/operations_lib.py
@@ -32,6 +32,17 @@ def edges_for_node(edges, node):
     return filter(lambda e: node in e.nodes, edges)
 
 @_graph_state_to_edges_implicit_conversion
+def split_edges_for_node(edges, node):
+    """
+    :return: only edges that contain node
+    """
+    with_node = list()
+    without_node = list()
+    for e in edges:
+        (with_node if node in e.nodese else without_node).append(e)
+    return with_node, without_node
+
+@_graph_state_to_edges_implicit_conversion
 def get_external_node(edges):
     """
     Method does not check that all edges have same external node and returns :func:`external_node`
