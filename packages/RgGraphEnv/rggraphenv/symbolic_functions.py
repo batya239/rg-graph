045a349be7a6e38aefcde6c8beaeb5bb58248c42
Dmitry Batkovich <batya239@gmail.com>
2015-09-22 15:13:47 +0300

    use ginac real symbols for symbolic functions

--- packages/RgGraphEnv/rggraphenv/symbolic_functions.py
+++ packages/RgGraphEnv/rggraphenv/symbolic_functions.py
@@ -21,7 +21,7 @@ def var(names):
         assert len(n) != 0
         v = _vars.get(n, None)
         if v is None:
-            v = swiginac.symbol(n)
+            v = swiginac.realsymbol(n)
             _vars[n] = v
         n_vars.append(v)
     return n_vars if len(n_vars) > 1 else n_vars[0]
