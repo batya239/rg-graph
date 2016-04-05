3d39b89e00871b4b7d3cde7c029804ae53fecabb
kirienko <yury.kirienko@gmail.com>
2016-04-05 12:38:11 +0300

    maple interface: 1. assumptions for stretching parameters 2. always use temporary files 3. do not remove `simplify` manually after unsuccessful simplification

--- phi3/d_to_infty/maple.py
+++ phi3/d_to_infty/maple.py
@@ -19,10 +19,13 @@ def maple(expr, Digits=10):
         'screenwidth=infinity',
         'errorcursor=false']
     MW = 'maple -t -c "interface({})"'.format(','.join(__maple_iface_opts))
-    assumption = "assume(%s):" % ", ".join(["k%s>1" % i for i in xrange(5)])
+    k_s = ", ".join(["k%s>1" % i for i in xrange(5)])
+    a_s = ", ".join(["a%s>0" % i for i in xrange(5)])
+    assumption = "assume(%s, %s):" % (k_s, a_s)
 
     ###
     # If expression is huge, put it in a temporary file
+    """
     if len(expr) > 16380:
         fd_source = tempfile.NamedTemporaryFile(prefix='tmp_rggraph_maple', delete=False)
         fd_dest = tempfile.NamedTemporaryFile(prefix='tmp_rggraph_maple', delete=False)
@@ -38,29 +41,29 @@ def maple(expr, Digits=10):
         os.remove(fd_source.name)
         os.remove(fd_dest.name)
         # print "sent/received symbols: %d / %d" % (len(expr), len(out))
-
     else:
-        child = pexpect.spawn(MW)
+    """""
+    child = pexpect.spawn(MW)
+    child.expect('#-->')
+    child.sendline(assumption)
+    child.expect('#-->')
+    if Digits != 10:
+        child.sendline('Digits = %d:' % Digits)
         child.expect('#-->')
-        child.sendline(assumption)
-        child.expect('#-->')
-        if Digits != 10:
-            child.sendline('Digits = %d:' % Digits)
-            child.expect('#-->')
-        sended = child.sendline(expr)
-        child.expect('#-->', timeout=None)
-        out = child.before
-        if len(out.split(';')) > 1:
-            out = out[out.find(';') + 1:].strip()
-            out = ''.join(out.split('\r\n'))
-            # print "sent/received symbols: %d / %d" % (sended, len(out))
-        elif len(out.split(';')) == 1 and expr[:9] == 'simplify(':
-            print "\t\t*** Simplification E R R O R ***\nExpression:\n"+expr+"\n*** END OF ERROR LOG"
-        else:
-            return "\n\t\t*** M A P L E ()  E R R O R ***\nExpression:\n"+expr+"\nResult:\n"+out+"\n*** END OF ERROR LOG"
+    sended = child.sendline(expr)
+    child.expect('#-->', timeout=None)
+    out = child.before
+    if len(out.split(';')) > 1:
+        out = out[out.find(';') + 1:].strip()
+        out = ''.join(out.split('\r\n'))
+        # print "sent/received symbols: %d / %d" % (sended, len(out))
+    elif len(out.split(';')) == 1 and expr[:9] == 'simplify(':
+        print "\t\t*** Simplification E R R O R ***\nExpression:\n"+expr+"\n*** END OF ERROR LOG"
+    else:
+        return "\n\t\t*** M A P L E ()  E R R O R ***\nExpression:\n"+expr+"\nResult:\n"+out+"\n*** END OF ERROR LOG"
 
-    if len(expr) - 10 < len(out) and expr[:9] == 'simplify(':
-        out = expr[8:-1]
+    #if len(expr) - 10 < len(out) and expr[:9] == 'simplify(':
+    #    out = expr[8:-1]
     return out
 
 if __name__ == "__main__":
